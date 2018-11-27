e24566b72f205ed1c40d97b625c269dea3c2bb7c
mvangog <martijn.van.gog@amsterdam.nl>
2018-11-27 12:23:40 +0100

    test with viewer.cs

--- Unity/Assets/Viewer.cs
+++ Unity/Assets/Viewer.cs
@@ -7,7 +7,7 @@ public class Viewer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //test
     }
 
     // Update is called once per frame
