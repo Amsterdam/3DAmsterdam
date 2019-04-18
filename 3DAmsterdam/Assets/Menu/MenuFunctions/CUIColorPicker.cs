b891792c051e1d0fc29f72742bcb7e4cd508ebe6
Dylan <dyhayg@gmail.com>
2019-04-18 14:22:11 +0200

    Color picker updated (kan nu kleuren opslaan)

--- 3DAmsterdam/Assets/Menu/MenuFunctions/CUIColorPicker.cs
+++ 3DAmsterdam/Assets/Menu/MenuFunctions/CUIColorPicker.cs
@@ -1,12 +1,14 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
 public class CUIColorPicker : MonoBehaviour
 {
-    public Color Color { get { return _color; } set { Setup( value ); } }
-    public void SetOnValueChangeCallback( Action<Color> onValueChange )
+    public Color Color { get { return _color; } set { Setup(value); } }
+    public void SetOnValueChangeCallback(Action<Color> onValueChange)
     {
         _onValueChange = onValueChange;
     }
@@ -16,55 +18,62 @@ public class CUIColorPicker : MonoBehaviour
 
     public TextMeshProUGUI textR, textB, textG, textA;
     public Slider sliderR, sliderB, sliderG, sliderA;
+    public List<Button> savedColorsButtons;
 
-    public GameObject cube;
+    private Queue savedColors = new Queue();
+    private int queueSize = 3;
+    int counter = 0;
 
-    private static void RGBToHSV( Color color, out float h, out float s, out float v )
+
+
+    public Material materialBuildings;
+
+    private static void RGBToHSV(Color color, out float h, out float s, out float v)
     {
-        var cmin = Mathf.Min( color.r, color.g, color.b );
-        var cmax = Mathf.Max( color.r, color.g, color.b );
+        var cmin = Mathf.Min(color.r, color.g, color.b);
+        var cmax = Mathf.Max(color.r, color.g, color.b);
         var d = cmax - cmin;
-        if ( d == 0 ) {
+        if (d == 0) {
             h = 0;
-        } else if ( cmax == color.r ) {
-            h = Mathf.Repeat( ( color.g - color.b ) / d, 6 );
-        } else if ( cmax == color.g ) {
-            h = ( color.b - color.r ) / d + 2;
+        } else if (cmax == color.r) {
+            h = Mathf.Repeat((color.g - color.b) / d, 6);
+        } else if (cmax == color.g) {
+            h = (color.b - color.r) / d + 2;
         } else {
-            h = ( color.r - color.g ) / d + 4;
+            h = (color.r - color.g) / d + 4;
         }
         s = cmax == 0 ? 0 : d / cmax;
         v = cmax;
     }
 
-    private static bool GetLocalMouse( GameObject go, out Vector2 result ) 
+    private static bool GetLocalMouse(GameObject go, out Vector2 result)
     {
-        var rt = ( RectTransform )go.transform;
-        var mp = rt.InverseTransformPoint( Input.mousePosition );
-        result.x = Mathf.Clamp( mp.x, rt.rect.min.x, rt.rect.max.x );
-        result.y = Mathf.Clamp( mp.y, rt.rect.min.y, rt.rect.max.y );
-        return rt.rect.Contains( mp );
+        var rt = (RectTransform)go.transform;
+        var mp = rt.InverseTransformPoint(Input.mousePosition);
+        result.x = Mathf.Clamp(mp.x, rt.rect.min.x, rt.rect.max.x);
+        result.y = Mathf.Clamp(mp.y, rt.rect.min.y, rt.rect.max.y);
+        return rt.rect.Contains(mp);
     }
 
-    private static Vector2 GetWidgetSize( GameObject go ) 
+    private static Vector2 GetWidgetSize(GameObject go)
     {
-        var rt = ( RectTransform )go.transform;
+        var rt = (RectTransform)go.transform;
         return rt.rect.size;
     }
 
-    private GameObject GO( string name )
+    private GameObject GO(string name)
     {
-        return transform.Find( name ).gameObject;
+        return transform.Find(name).gameObject;
     }
 
-    private void Setup( Color inputColor )
+    private void Setup(Color inputColor)
     {
-        var satvalGO = GO( "SaturationValue" );
-        var satvalKnob = GO( "SaturationValue/Knob" );
-        var hueGO = GO( "Hue" );
-        var hueKnob = GO( "Hue/Knob" );
-        var result = GO( "Result" );
-        var hueColors = new Color [] {
+        var satvalGO = GO("SaturationValue");
+        var satvalKnob = GO("SaturationValue/Knob");
+        var hueGO = GO("Hue");
+        var hueKnob = GO("Hue/Knob");
+        var result = GO("Result");
+        var hueColors = new Color[] {
             Color.red,
             Color.yellow,
             Color.green,
@@ -72,42 +81,42 @@ public class CUIColorPicker : MonoBehaviour
             Color.blue,
             Color.magenta,
         };
-        var satvalColors = new Color [] {
+        var satvalColors = new Color[] {
             new Color( 0, 0, 0 ),
             new Color( 0, 0, 0 ),
             new Color( 1, 1, 1 ),
             hueColors[0],
         };
-        var hueTex = new Texture2D( 1, 7 );
-        for ( int i = 0; i < 7; i++ ) {
-            hueTex.SetPixel( 0, i, hueColors[i % 6] );
+        var hueTex = new Texture2D(1, 7);
+        for (int i = 0; i < 7; i++) {
+            hueTex.SetPixel(0, i, hueColors[i % 6]);
         }
         hueTex.Apply();
-        hueGO.GetComponent<Image>().sprite = Sprite.Create( hueTex, new Rect( 0, 0.5f, 1, 6 ), new Vector2( 0.5f, 0.5f ) );
-        var hueSz = GetWidgetSize( hueGO );
-        var satvalTex = new Texture2D(2,2);
-        satvalGO.GetComponent<Image>().sprite = Sprite.Create( satvalTex, new Rect( 0.5f, 0.5f, 1, 1 ), new Vector2( 0.5f, 0.5f ) );
+        hueGO.GetComponent<Image>().sprite = Sprite.Create(hueTex, new Rect(0, 0.5f, 1, 6), new Vector2(0.5f, 0.5f));
+        var hueSz = GetWidgetSize(hueGO);
+        var satvalTex = new Texture2D(2, 2);
+        satvalGO.GetComponent<Image>().sprite = Sprite.Create(satvalTex, new Rect(0.5f, 0.5f, 1, 1), new Vector2(0.5f, 0.5f));
         Action resetSatValTexture = () => {
-            for ( int j = 0; j < 2; j++ ) {
-                for ( int i = 0; i < 2; i++ ) {
-                    satvalTex.SetPixel( i, j, satvalColors[i + j * 2] );
+            for (int j = 0; j < 2; j++) {
+                for (int i = 0; i < 2; i++) {
+                    satvalTex.SetPixel(i, j, satvalColors[i + j * 2]);
                 }
             }
             satvalTex.Apply();
         };
-        var satvalSz = GetWidgetSize( satvalGO );
+        var satvalSz = GetWidgetSize(satvalGO);
         float Hue, Saturation, Value;
-        RGBToHSV( inputColor, out Hue, out Saturation, out Value );
+        RGBToHSV(inputColor, out Hue, out Saturation, out Value);
         Action applyHue = () => {
-            var i0 = Mathf.Clamp( ( int )Hue, 0, 5 );
-            var i1 = ( i0 + 1 ) % 6;
-            var resultColor = Color.Lerp( hueColors[i0], hueColors[i1], Hue - i0 );
+            var i0 = Mathf.Clamp((int)Hue, 0, 5);
+            var i1 = (i0 + 1) % 6;
+            var resultColor = Color.Lerp(hueColors[i0], hueColors[i1], Hue - i0);
             satvalColors[3] = resultColor;
             resetSatValTexture();
         };
         Action applySaturationValue = () => {
-            var sv = new Vector2( Saturation, Value );
-            var isv = new Vector2( 1 - sv.x, 1 - sv.y );
+            var sv = new Vector2(Saturation, Value);
+            var isv = new Vector2(1 - sv.x, 1 - sv.y);
             var c0 = isv.x * isv.y * satvalColors[0];
             var c1 = sv.x * isv.y * satvalColors[1];
             var c2 = isv.x * sv.y * satvalColors[2];
@@ -115,48 +124,48 @@ public class CUIColorPicker : MonoBehaviour
             var resultColor = c0 + c1 + c2 + c3;
             var resImg = result.GetComponent<Image>();
             resImg.color = resultColor;
-            if ( _color != resultColor ) {
-                if ( _onValueChange != null ) {
-                    _onValueChange( resultColor );
+            if (_color != resultColor) {
+                if (_onValueChange != null) {
+                    _onValueChange(resultColor);
                 }
                 _color = resultColor;
             }
         };
         applyHue();
         applySaturationValue();
-        satvalKnob.transform.localPosition = new Vector2( Saturation * satvalSz.x, Value * satvalSz.y );
-        hueKnob.transform.localPosition = new Vector2( hueKnob.transform.localPosition.x, Hue / 6 * satvalSz.y );
+        satvalKnob.transform.localPosition = new Vector2(Saturation * satvalSz.x, Value * satvalSz.y);
+        hueKnob.transform.localPosition = new Vector2(hueKnob.transform.localPosition.x, Hue / 6 * satvalSz.y);
         Action dragH = null;
         Action dragSV = null;
         Action idle = () => {
-            if ( Input.GetMouseButtonDown( 0 ) ) {
+            if (Input.GetMouseButtonDown(0)) {
                 Vector2 mp;
-                if ( GetLocalMouse( hueGO, out mp ) ) {
+                if (GetLocalMouse(hueGO, out mp)) {
                     _update = dragH;
-                } else if ( GetLocalMouse( satvalGO, out mp ) ) {
+                } else if (GetLocalMouse(satvalGO, out mp)) {
                     _update = dragSV;
                 }
             }
         };
         dragH = () => {
             Vector2 mp;
-            GetLocalMouse( hueGO, out mp );
+            GetLocalMouse(hueGO, out mp);
             Hue = mp.y / hueSz.y * 6;
             applyHue();
             applySaturationValue();
-            hueKnob.transform.localPosition = new Vector2( hueKnob.transform.localPosition.x, mp.y );
-            if ( Input.GetMouseButtonUp( 0 ) ) {
+            hueKnob.transform.localPosition = new Vector2(hueKnob.transform.localPosition.x, mp.y);
+            if (Input.GetMouseButtonUp(0)) {
                 _update = idle;
             }
         };
         dragSV = () => {
             Vector2 mp;
-            GetLocalMouse( satvalGO, out mp );
+            GetLocalMouse(satvalGO, out mp);
             Saturation = mp.x / satvalSz.x;
             Value = mp.y / satvalSz.y;
             applySaturationValue();
             satvalKnob.transform.localPosition = mp;
-            if ( Input.GetMouseButtonUp( 0 ) ) {
+            if (Input.GetMouseButtonUp(0)) {
                 _update = idle;
             }
         };
@@ -166,10 +175,10 @@ public class CUIColorPicker : MonoBehaviour
     public void SetRandomColor()
     {
         var rng = new System.Random();
-        var r = ( rng.Next() % 1000 ) / 1000.0f;
-        var g = ( rng.Next() % 1000 ) / 1000.0f;
-        var b = ( rng.Next() % 1000 ) / 1000.0f;
-        Color = new Color( r, g, b );
+        var r = (rng.Next() % 1000) / 1000.0f;
+        var g = (rng.Next() % 1000) / 1000.0f;
+        var b = (rng.Next() % 1000) / 1000.0f;
+        Color = new Color(r, g, b);
     }
 
     public void SetRGB()
@@ -180,6 +189,26 @@ public class CUIColorPicker : MonoBehaviour
         textG.GetComponent<TextMeshProUGUI>().text = ((int)(sliderG.GetComponent<Slider>().value * 255f)).ToString();
         textB.GetComponent<TextMeshProUGUI>().text = ((int)(sliderB.GetComponent<Slider>().value * 255f)).ToString();
         textA.GetComponent<TextMeshProUGUI>().text = ((int)(sliderA.GetComponent<Slider>().value * 255f)).ToString();
+    }
+
+    public void SaveColor()
+    {
+        savedColors.Enqueue(Color);
+
+        if (savedColors.Count == (queueSize + 1))
+        {
+            savedColors.Dequeue();
+        }
+
+        object[] queueItems = savedColors.ToArray();
+
+        counter = savedColors.Count - 1;
+
+        for (int i = 0; i < savedColors.Count; i++)
+        {
+            savedColorsButtons[counter].image.color = (Color)queueItems[i];
+            counter--;
+        }
     }
 
     void Awake()
@@ -199,8 +228,7 @@ public class CUIColorPicker : MonoBehaviour
 
     void Update()
     {
-        //cube.GetComponent<Renderer>().material.color = Color;
-        cube.GetComponent<Renderer>().material.color = new Color(Color.r, Color.g, Color.b, sliderA.GetComponent<Slider>().value);
+        materialBuildings.color = new Color(Color.r, Color.g, Color.b, sliderA.GetComponent<Slider>().value);
 
         _update();
     }
---
+++ 3DAmsterdam/Assets/Menu/MenuFunctions/SetColor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetColor : MonoBehaviour
+{
+    private CUIColorPicker colorPicker;
+
+    void Start()
+    {
+        colorPicker = GameObject.Find("CUIColorPicker").GetComponent<CUIColorPicker>();
+
+        gameObject.GetComponent<Button>().onClick.AddListener(ButtonClicked);
+    }
+
+    void ButtonClicked()
+    {
+        colorPicker.Color = gameObject.GetComponent<Image>().color;
+    }
+}
