a2f679f06ae30ea8453f85ba29dc5a8fed86f16d
Rick van Kersbergen <Rickvkersbergen@gmail.com>
2019-05-13 13:06:32 +0200

    You can now upload an .obj file with a file explorer

--- 3DAmsterdam/Assets/OBJImport/Samples/ObjFromFile.cs
+++ 3DAmsterdam/Assets/OBJImport/Samples/ObjFromFile.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using SimpleFileBrowser;
 
 public class ObjFromFile : MonoBehaviour
 {
@@ -17,40 +18,44 @@ public class ObjFromFile : MonoBehaviour
         PlaceUpload();
     }
 
-    void OnGUI() {
-        objPath = GUI.TextField(new Rect(0, 0, 256, 32), objPath);
+    //void OnGUI()
+    //{
+    //    objPath = GUI.TextField(new Rect(0, 0, 256, 32), objPath);
 
-        GUI.Label(new Rect(0, 0, 256, 32), "Obj Path:");
-        if(GUI.Button(new Rect(256, 32, 64, 32), "Load File"))
-        {
-            //file path
-            if (!File.Exists(objPath))
-            {
-                error = "File doesn't exist.";
-            }else{
-                if(loadedObject != null)            
-                    Destroy(loadedObject);
-                
-                loadedObject = new OBJLoader().Load(objPath);
-                loadedObject.layer = LayerMask.NameToLayer("Ignore Raycast");
+    //    GUI.Label(new Rect(0, 0, 256, 32), "Obj Path:");
+    //    if (GUI.Button(new Rect(256, 32, 64, 32), "Load File"))
+    //    {
+    //        //file path
+    //        if (!File.Exists(FileBrowser.Result))
+    //        {
+    //            error = "File doesn't exist.";
 
-                placeUpload = true;
+    //        }
+    //        else
+    //        {
+    //            if (loadedObject != null)
+    //                Destroy(loadedObject);
 
-                error = string.Empty;
-            }
-        }
+    //            loadedObject = new OBJLoader().Load(objPath);
+    //            loadedObject.layer = LayerMask.NameToLayer("Ignore Raycast");
 
-        if(!string.IsNullOrWhiteSpace(error))
-        {
-            GUI.color = Color.red;
-            GUI.Box(new Rect(0, 64, 256 + 64, 32), error);
-            GUI.color = Color.white;
-        }
-    }
+    //            placeUpload = true;
+
+    //            error = string.Empty;
+    //        }
+    //    }
+
+    //    if (!string.IsNullOrWhiteSpace(error))
+    //    {
+    //        GUI.color = Color.red;
+    //        GUI.Box(new Rect(0, 64, 256 + 64, 32), error);
+    //        GUI.color = Color.white;
+    //    }
+    //}
 
     public void PlaceUpload()
     {
-        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+      ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
         if (placeUpload)
         {
@@ -67,5 +72,27 @@ public class ObjFromFile : MonoBehaviour
                 }
             }
         }     
+    }
+
+    public void LoadUpload()
+    {
+
+        //file path
+        if (!File.Exists(FileBrowser.Result))
+        {
+            error = "File doesn't exist.";
+        }
+        else
+        {
+            if (loadedObject != null)
+                Destroy(loadedObject);
+
+            loadedObject = new OBJLoader().Load(FileBrowser.Result);
+            loadedObject.layer = LayerMask.NameToLayer("Ignore Raycast");
+
+            placeUpload = true;
+
+            error = string.Empty;
+        }
     }
 }
