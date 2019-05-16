bcd81b4f281bd94d2b36c674b2cce30e6576b58c
Dylan <dyhayg@gmail.com>
2019-05-16 09:30:56 +0200

    pijlenprefab scaled mee met positie van gebouw en grootte van gebouw (prefab blijft symmetrisch)

--- 3DAmsterdam/Assets/Gebouwplaatser/PijlenPrefab.cs
+++ 3DAmsterdam/Assets/Gebouwplaatser/PijlenPrefab.cs
@@ -11,15 +11,19 @@ public class PijlenPrefab : MonoBehaviour
     private RaycastHit hit;
     private Ray ray;
 
-    private List<Vector3> rendererSizes;
+    private List<Transform> rendererOptions;
     private Vector3 largestSize = Vector3.zero;
+    private Transform rendChild;
 
     private float scalingXZ, scalingY, scaleFactor = 2f;
 
     private void Start()
     {
+        rendererOptions = new List<Transform>();
+
         FindRenderer();
-        Debug.Log("fdafds");
+        AddCollider();
+
         pijlenPrefabMesh = (GameObject) Resources.Load("PijlenPrefab");
     }
 
@@ -29,6 +33,11 @@ public class PijlenPrefab : MonoBehaviour
         {
             pijlenPrefab = Instantiate(pijlenPrefabMesh, transform.position, Quaternion.identity);
             pijlenPrefab.transform.parent = gameObject.transform;
+
+            var groundToObject = Vector3.Distance(new Vector3(transform.position.x, 0, transform.position.z), transform.position);
+
+            pijlenPrefab.transform.position = new Vector3(pijlenPrefab.transform.position.x, groundToObject + (largestSize.y / 3),
+                                                          pijlenPrefab.transform.position.z);
         }
 
         if (Input.GetMouseButtonUp(0))
@@ -72,8 +81,15 @@ public class PijlenPrefab : MonoBehaviour
     {
         if (transform.hasChanged && pijlenPrefab != null)
         {
-            scalingXZ = Mathf.Max(transform.localScale.x, transform.localScale.z);
-            scalingY = pijlenPrefab.transform.localScale.y / scaleFactor;
+            if (transform.localScale != new Vector3(1, 1, 1))
+            {
+                scalingXZ = Mathf.Max(transform.localScale.x, transform.localScale.z);
+                scalingY = pijlenPrefab.transform.localScale.y / scaleFactor;
+            } else
+            {
+                scalingXZ = Mathf.Max(largestSize.x, largestSize.z);
+                scalingY = pijlenPrefab.transform.localScale.y / scaleFactor;
+            }
 
             pijlenPrefab.transform.parent = null;
             pijlenPrefab.transform.localScale = new Vector3(scalingXZ, scalingY, scalingXZ) * scaleFactor;
@@ -83,26 +99,40 @@ public class PijlenPrefab : MonoBehaviour
 
     private void FindRenderer()
     {
-        if (transform.GetComponent<Renderer>() != null) rendererSizes.Add(transform.GetComponent<Renderer>().bounds.size);
+        rendererOptions.Add(this.gameObject.transform);
 
-        foreach(Transform child in transform)
+        if (transform.childCount != 0)
         {
-            Debug.Log(child);
-
-            if (child.GetComponent<Renderer>() != null)
+            foreach (Transform child in transform)
             {
-                rendererSizes.Add(child.GetComponent<Renderer>().bounds.size);
+                rendererOptions.Add(child);
             }
         }
 
-        foreach(Vector3 size in rendererSizes)
+        foreach (Transform option in rendererOptions)
         {
-            Debug.Log(size);
-
-            if (size.magnitude > largestSize.magnitude)
+            if (option.GetComponent<Renderer>() != null)
             {
-                largestSize = size;
+                if (option.GetComponent<Renderer>().bounds.size.magnitude > largestSize.magnitude)
+                {
+                    largestSize = option.GetComponent<Renderer>().bounds.size;
+                    rendChild = option;
+                }
             }
+        }
+    }
+
+    private void AddCollider()
+    {
+        if (gameObject.GetComponent<Collider>() == null)
+        {
+            gameObject.AddComponent<BoxCollider>();
+
+            var boxCollider = rendChild.gameObject.AddComponent<BoxCollider>();
+            Destroy(rendChild.gameObject.GetComponent<BoxCollider>());
+
+            gameObject.GetComponent<BoxCollider>().size = boxCollider.size;
+            gameObject.GetComponent<BoxCollider>().center = new Vector3(0f, boxCollider.center.y, 0f);
         }
     }
 }
