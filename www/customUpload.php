6765ac263592b7e59d441beabb91885cb41ece1e
barkn <bart.knuiman@geodan.nl>
2019-06-10 13:53:21 +0200

    Upload script

--- www/customUpload.php
+++ www/customUpload.php
@@ -1,31 +1,47 @@
 
 <?php
-if(isset($_POST["secret"]) && isset($_POST["name"]) && isset($_POST["objData"]))
-{ 
-    if ( $_POST["secret"] == "87ajdf898##@@jjKJA" )
-    {
-        $name = $_POST["name"];
-        $handle = fopen("uploadedFiles/" . $name, "w");
-        if ( isset($handle) )
-        {
-            // Write data to the file
-            $data = $_POST["objData"];
-            fwrite($handle, $data);
-            fflush($handle);
-            fclose($handle);
-        }
-        else
-        {
-            echo "failed to write file";
-        }
-    }
-    else
-    {
-        echo "invalid secret";
-    }
-}
-else 
+
+if(!isset($_POST["secret"]))
 {
-    echo "not all parameters set";
+    echo "Error, no secret provided.";
+    exit;
 }
+
+if ( $_POST["secret"] !== "87ajdf898##@@jjKJA" )
+{
+    echo "Error secret does not match.";
+    exit;
+}
+
+// For backwards compatability
+if(!isset($_FILES) && isset($HTTP_POST_FILES))
+{
+    $_FILES = $HTTP_POST_FILES;
+}
+
+if ($_FILES['file']['error'] !== UPLOAD_ERR_OK)
+{
+    echo "Upload error set " . $_FILES['file']['error'];
+    exit;
+}
+
+/*
+if ($_FILES['file']['type'] !== "application/zip")
+{
+    echo "Mime type mismatch";
+    exit;
+}*/
+
+// Check if file already exists
+$uploadfile =  'uploadedFiles/' . $_FILES['file']['name'];
+if (file_exists($uploadfile)) 
+{
+    echo "File already exists.";
+    exit;
+}
+
+move_uploaded_file($_FILES['file']['tmp_name'], $uploadfile); 
+
+echo "Upload of " . $uploadfile . " succesful.";
+
 ?>
