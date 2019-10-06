240ccf7b9ea53c1fa8543dde5a218366b55d55c3
Mvangog <mvangog@casema.nl>
2019-10-06 13:32:30 +0200

    ipv shader worden gehighlighte panden in een submesh geplaatst

--- 3DAmsterdam/Assets/Stadsmodel/ObjectMapping.cs
+++ 3DAmsterdam/Assets/Stadsmodel/ObjectMapping.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class ObjectMapping : MonoBehaviour
 {
     public Dictionary<string, float> Objectenlijst = new Dictionary<string, float>();
@@ -10,6 +11,7 @@ public class ObjectMapping : MonoBehaviour
     public Material DefaultMaterial;
 
     private SubObject Totaalmesh;
+    private SubObject Tempmesh;
 
     private List<SubObject> HighlightObjecten = new List<SubObject>();
 
@@ -19,111 +21,104 @@ public class ObjectMapping : MonoBehaviour
         Totaalmesh.verts = totaalmesh.vertices;
         Totaalmesh.triangles = totaalmesh.triangles;
         Totaalmesh.uv2 = totaalmesh.uv2;
+        Tempmesh = new SubObject();
+        Tempmesh.verts = totaalmesh.vertices;
+        Tempmesh.triangles = totaalmesh.triangles;
+        Tempmesh.uv2 = totaalmesh.uv2;
     }
 
     public void SetHighlight(List<string> HighlightObjectenstring)
     {
 
-        for (int i = 0; i < HighlightObjectenstring.Count; i++)
+        HighlightObjecten = new List<SubObject>();
+        Totaalmesh.triangles = Tempmesh.triangles;
+        for (int i = 0; i < 1; i++)
         {
             if (Objectenlijst.ContainsKey(HighlightObjectenstring[i]))
             {
-                bool IsalApart = false;
-                for (int j = 0; i < HighlightObjecten.Count; i++)
-                {
-                    if(HighlightObjecten[j].ObjectID == HighlightObjectenstring[i]) { IsalApart = true; }
-                }
-                if (IsalApart == false)
-                {
+
                     ExtractSubObject(HighlightObjectenstring[i]);
-                    SetupMesh();
-                }
+
             }
         }
+        SetupMesh();
+    }
+
+    void ReturnSubobject(SubObject SO)
+    {
+        List<int> verts = new List<int>();
+        for (int i = 0; i < Totaalmesh.triangles.Length; i++)
+        {
+            verts.Add(Totaalmesh.triangles[i]);
+        }
+        for (int i = 0; i < SO.triangles.Length; i++)
+        {
+            verts.Add(SO.triangles[i]);
+        }
+        Totaalmesh.triangles = verts.ToArray();
+
     }
     void ExtractSubObject(string ObjectID)
     {
+        
         SubObject SO = new SubObject(); //nieuw subobject
         SubObject TO = new SubObject(); // nieuw totaalobject zonder subobject
         SO.ObjectID = ObjectID;
-        Dictionary<int, int> indexmappingSO = new Dictionary<int, int>();
-        Dictionary<int, int> indexmappingTO = new Dictionary<int, int>();
-        List<Vector3> VertsSO = new List<Vector3>();
-        List<Vector3> VertsTO = new List<Vector3>();
-        List<Vector2> uv2SO = new List<Vector2>();
-        List<Vector2> uv2TO = new List<Vector2>();
+        
         float uv2Waarde = Objectenlijst[ObjectID];
-        Vector2[] TotaalmeshUVs = Totaalmesh.uv2;
-
-        //// verts splitsen in behorend bij suboject en behorend bij Totaalobject
-        for (int uvnr = 0; uvnr < TotaalmeshUVs.Length; uvnr++)
-        {
-            if (TotaalmeshUVs[uvnr].x == uv2Waarde)
-            {
-                VertsSO.Add(Totaalmesh.verts[uvnr]);
-                indexmappingSO.Add(uvnr, VertsSO.Count - 1);
-                uv2SO.Add(TotaalmeshUVs[uvnr]);
-            }
-            else
-            {
-                VertsTO.Add(Totaalmesh.verts[uvnr]);
-                indexmappingTO.Add(uvnr, VertsTO.Count - 1);
-                uv2TO.Add(TotaalmeshUVs[uvnr]);
-            }
-        }
-        SO.verts = VertsSO.ToArray();
-        TO.verts = VertsTO.ToArray();
-        SO.uv2 = uv2SO.ToArray();
-        TO.uv2 = uv2TO.ToArray();
-        //triangles splitsen en verwijzing naar vertices updaten
+        
+        //triangles splitsen
         List<int> trianglesSO = new List<int>();
         List<int> trianglesTO = new List<int>();
-        for (int TriangledIndex = 0; TriangledIndex < Totaalmesh.triangles.Length/3; TriangledIndex+=3)
+        for (int TriangledIndex = 0; TriangledIndex < (Tempmesh.triangles.Length); TriangledIndex+=3)
         {
             
-            int vertIndex = Totaalmesh.triangles[TriangledIndex];
-            if (Totaalmesh.uv2[vertIndex].x==uv2Waarde)
+            int vertIndex = Tempmesh.triangles[TriangledIndex];
+            if (Mathf.Abs(Tempmesh.uv2[vertIndex].x - uv2Waarde) < 0.5)
             {
                 
-                trianglesSO.Add(indexmappingSO[Totaalmesh.triangles[TriangledIndex]]);
-                trianglesSO.Add(indexmappingSO[Totaalmesh.triangles[TriangledIndex+1]]);
-                trianglesSO.Add(indexmappingSO[Totaalmesh.triangles[TriangledIndex+2]]);
+                trianglesSO.Add(Tempmesh.triangles[TriangledIndex]);
+                trianglesSO.Add(Tempmesh.triangles[TriangledIndex+1]);
+                trianglesSO.Add(Tempmesh.triangles[TriangledIndex+2]);
             }
             else
             {
-                trianglesTO.Add(indexmappingTO[Totaalmesh.triangles[TriangledIndex]]);
-                trianglesTO.Add(indexmappingTO[Totaalmesh.triangles[TriangledIndex + 1]]);
-                trianglesTO.Add(indexmappingTO[Totaalmesh.triangles[TriangledIndex + 2]]);
+                trianglesTO.Add(Tempmesh.triangles[TriangledIndex]);
+                trianglesTO.Add(Tempmesh.triangles[TriangledIndex + 1]);
+                trianglesTO.Add(Tempmesh.triangles[TriangledIndex + 2]);
             }
         }
         SO.triangles = trianglesSO.ToArray();
         TO.triangles = trianglesTO.ToArray();
         HighlightObjecten.Add(SO);
-        Totaalmesh = TO;
+        Totaalmesh.triangles = TO.triangles;
     }
 
     void SetupMesh()
     {
-        Mesh ms = transform.GetComponent<MeshFilter>().sharedMesh;
+        Mesh ms = new Mesh();
         List<Material> mats = new List<Material>();
         mats.Add(DefaultMaterial);
-        ms.Clear();
-        ms.subMeshCount = HighlightObjecten.Count+1;
-        ms.vertices = Totaalmesh.verts;
-        ms.uv2 = Totaalmesh.uv2;
-        //ms.SetTriangles( Totaalmesh.triangles,0);
-        ms.SetIndices(Totaalmesh.triangles, MeshTopology.Triangles, 0);
         for (int i = 0; i < HighlightObjecten.Count; i++)
         {
-            
             mats.Add(HighlightMaterial);
-            ms.SetIndices(HighlightObjecten[i].triangles, MeshTopology.Triangles, i);
-            //ms.SetUVs(1, HighlightObjecten[i].uv2);
-            ms.SetTriangles(HighlightObjecten[i].triangles, i+1,false,0);
+        }
+        transform.GetComponent<MeshRenderer>().sharedMaterials = mats.ToArray();
+        
+        ms.subMeshCount = HighlightObjecten.Count+1;
+        ms.vertices = Tempmesh.verts;
+        ms.uv2 = Tempmesh.uv2;
+        //ms.SetTriangles( Totaalmesh.triangles,0);
+        ms.SetTriangles(Totaalmesh.triangles,  0);
+        for (int i = 0; i < HighlightObjecten.Count; i++)
+        {
+  
+         ms.SetTriangles(HighlightObjecten[i].triangles, i+1);
             
         }
+        ms.RecalculateNormals();
         transform.GetComponent<MeshFilter>().sharedMesh = ms;
-        transform.GetComponent<MeshRenderer>().sharedMaterials = mats.ToArray();
+        
         
     }
 }
