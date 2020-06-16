63ed6dd829734047fe148f7edb59ebc1c755cb13
Sam Baas <samuelpetrus@gmail.com>
2020-06-16 12:52:03 +0200

    changed layers button to toggle, and added swipe in and out toggle animation

---
+++ 3DAmsterdam/Assets/Amsterdam3D/Scripts/Interface/Layers/InterfaceLayers.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Amsterdam3d.Interface
+{
+    public class InterfaceLayers : MonoBehaviour
+    {
+        private Animator animator;
+        private bool toggledVisible = false;
+
+        void Awake()
+        {
+            animator = GetComponent<Animator>();
+        }
+        
+        public void ToggleVisibility(bool visible)
+        {
+            toggledVisible = visible;
+            animator.SetBool("AnimateIn", toggledVisible);
+        }
+    }
+}
