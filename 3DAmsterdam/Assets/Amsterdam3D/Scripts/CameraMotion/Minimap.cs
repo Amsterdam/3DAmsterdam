a3ba397ee95ef9d7f16b4e2a4b484b5d6c350e93
Sam Baas <samuelpetrus@gmail.com>
2020-06-17 16:40:27 +0200

    added clicking and dragging to minimap allowing positioning of camera

--- 3DAmsterdam/Assets/Amsterdam3D/Scripts/CameraMotion/Minimap.cs
+++ 3DAmsterdam/Assets/Amsterdam3D/Scripts/CameraMotion/Minimap.cs
@@ -3,26 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using ConvertCoordinates;
+using UnityEngine.EventSystems;
 
 namespace Amsterdam3D.Interface
 {
-    public class Minimap : MonoBehaviour
+    public class Minimap : MonoBehaviour, IPointerClickHandler, IDragHandler
     {
         private Vector3 direction;
 
-        Vector3 bottomLeft, topRight, centerPosition, startPosition;
+        Vector3 bottomLeftUnityCoordinates, topRightUnityCoordinates, centerPosition, startPosition;
+       
+        [SerializeField]
+        private RectTransform minimapPointer;
 
-        private RectTransform rectTransform;
+        [SerializeField]
+        private RectTransform mapImage;
 
         private const float bottomLeftLat = 52.261480f;
         private const float bottomLeftLong = 4.727386f;
         private const float topRightLat = 52.454227f;
         private const float topRightLong = 5.108260f;
-
-        private void Awake()
-        {
-            rectTransform = GetComponent<RectTransform>();
-        }
 
         private void Start()
         {
@@ -32,24 +32,70 @@ namespace Amsterdam3D.Interface
 
         private void CalculateMapCoordinates()
         {
-            bottomLeft = CoordConvert.WGS84toUnity(bottomLeftLong, bottomLeftLat);
-            topRight = CoordConvert.WGS84toUnity(topRightLong, topRightLat);
+            bottomLeftUnityCoordinates = CoordConvert.WGS84toUnity(bottomLeftLong, bottomLeftLat);
+            topRightUnityCoordinates = CoordConvert.WGS84toUnity(topRightLong, topRightLat);
         }
 
         void LateUpdate()
         {
-            var posX = Mathf.InverseLerp(bottomLeft.x,topRight.x,Camera.main.transform.position.x);
-            var posY = Mathf.InverseLerp(bottomLeft.z, topRight.z, Camera.main.transform.position.z);
-
-            rectTransform.anchorMin = rectTransform.anchorMax = new Vector3(posX, posY, 0);
-
-            ChangeImageForwardToCameraForward();
+            AlignPointerToCamera();
         }
 
-        private void ChangeImageForwardToCameraForward()
+        private void AlignPointerToCamera()
+        {
+            PositionPointer();
+            RotatePointerByCameraDirection();
+        }
+
+        private void PositionPointer()
+        {
+            var posX = Mathf.InverseLerp(bottomLeftUnityCoordinates.x, topRightUnityCoordinates.x, Camera.main.transform.position.x);
+            var posY = Mathf.InverseLerp(bottomLeftUnityCoordinates.z, topRightUnityCoordinates.z, Camera.main.transform.position.z);
+            minimapPointer.anchorMin = minimapPointer.anchorMax = new Vector3(posX, posY, 0);
+        }
+
+        private void RotatePointerByCameraDirection()
         {
             direction.z = Camera.main.transform.eulerAngles.y;
-            transform.localEulerAngles = direction * -1.0f;
+            minimapPointer.localEulerAngles = direction * -1.0f;
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            ConvertClickToMovePosition(eventData);
+        }
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            ConvertClickToMovePosition(eventData);
+        }
+
+        /// <summary>
+        /// Convert the position we clicked on the map, to the move location for our camera.
+        /// </summary>
+        /// <param name="eventData">The pointer event data containing our click position</param>
+        private void ConvertClickToMovePosition(PointerEventData eventData)
+        {
+            Vector2 localPositionInRectTransform;
+            if (!RectTransformUtility.ScreenPointToLocalPointInRectangle(mapImage, eventData.position, eventData.pressEventCamera, out localPositionInRectTransform))
+                return;
+
+            var interpolatedWidth = mapImage.rect.width * 0.5f;
+            var interpolatedHeight = mapImage.rect.height * 0.5f;
+
+            localPositionInRectTransform = new Vector2(
+                Mathf.InverseLerp(-interpolatedWidth, interpolatedWidth, localPositionInRectTransform.x),
+                Mathf.InverseLerp(-interpolatedHeight, interpolatedHeight, localPositionInRectTransform.y)
+            );
+            MoveToLocationOnMap(localPositionInRectTransform);
+        }
+        /// <summary>
+        /// Move the main camera to where we clicked on this map
+        /// </summary>
+        /// <param name="locationOnRectTransform">The click location of this recttransform</param>
+        public void MoveToLocationOnMap(Vector3 locationOnRectTransform){
+            var clickedWorldPositionX = Mathf.Lerp(bottomLeftUnityCoordinates.x, topRightUnityCoordinates.x, locationOnRectTransform.x);
+            var clickedWorldPositionZ = Mathf.Lerp(bottomLeftUnityCoordinates.z, topRightUnityCoordinates.z, locationOnRectTransform.y);
+            Camera.main.transform.position = new Vector3(clickedWorldPositionX, Camera.main.transform.position.y, clickedWorldPositionZ);
         }
     }
 }
