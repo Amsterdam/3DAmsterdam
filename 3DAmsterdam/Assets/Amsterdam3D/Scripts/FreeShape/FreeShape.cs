d5a02cfe8315250d80d8a55ca291d8008629133c
Sam Baas <samuelpetrus@gmail.com>
2020-06-22 18:30:43 +0200

    converting freeshape to mesh to avoid scaling issues

--- 3DAmsterdam/Assets/Amsterdam3D/Scripts/FreeShape/FreeShape.cs
+++ 3DAmsterdam/Assets/Amsterdam3D/Scripts/FreeShape/FreeShape.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 namespace Amsterdam3D.FreeShape
@@ -7,7 +8,7 @@ namespace Amsterdam3D.FreeShape
 	public class FreeShape : MonoBehaviour
 	{
 		[SerializeField]
-		private Transform shape;
+		private MeshFilter shape;
 
 		[SerializeField]
 		private Handle handleXMin;
@@ -21,50 +22,63 @@ namespace Amsterdam3D.FreeShape
 		[SerializeField]
 		private Handle handleY;
 
-		private Vector3 shapeStartScale;
+		private Vector3[] shapeVertices;
 
-		void Start()
+		private void Start()
 		{
-			shapeStartScale = shape.localScale;
+			shapeVertices = shape.sharedMesh.vertices;
 		}
 
-		void LateUpdate()
+		private void UpdateShapeVerts(){
+			OverrideVertPosition(new int[]{ 0, 15, 22 }, handleXPlus.transform.localPosition.x, -handleY.transform.localPosition.x, handleZPlus.transform.localPosition.x);
+			OverrideVertPosition(new int[] { 7, 18, 14 }, handleXPlus.transform.localPosition.x, -handleY.transform.localPosition.x, handleZPlus.transform.localPosition.x);
+			OverrideVertPosition(new int[] { 6, 20, 12 }, handleXPlus.transform.localPosition.x, -handleY.transform.localPosition.x, handleZPlus.transform.localPosition.x);
+			OverrideVertPosition(new int[] { 8, 21, 2 }, handleXPlus.transform.localPosition.x, -handleY.transform.localPosition.x, handleZPlus.transform.localPosition.x);
+
+
+
+			shape.sharedMesh.vertices = shapeVertices;
+		}
+
+		private void OverrideVertPosition(int[] arrayPositions, float newX = 0.0f, float newY = 0.0f, float newZ = 0.0f){
+			foreach (int index in arrayPositions)
+			{
+				shapeVertices[index] = new Vector3(
+					(newX > 0) ? newX : shapeVertices[index].x,
+					(newY > 0) ? newY : shapeVertices[index].y,
+					(newZ > 0) ? newZ : shapeVertices[index].z
+				);
+			}
+		}
+
+		private void Update()
 		{
-			ClampHandlesToShape();
-			ScaleShape();
+			UpdateShapeVerts();
 		}
 
 		public void ScaleShape()
 		{
 			shape.transform.localPosition = new Vector3(
 				Mathf.Lerp(handleXMin.transform.localPosition.x, handleXPlus.transform.localPosition.x, 0.5f),
-				Mathf.Lerp(handleXMin.transform.localPosition.y, handleXPlus.transform.localPosition.y, 0.5f),
-				Mathf.Lerp(handleXMin.transform.localPosition.z, handleXPlus.transform.localPosition.z, 0.5f)
+				Mathf.Lerp(-handleY.transform.localPosition.y, handleY.transform.localPosition.y, 0.5f),
+				Mathf.Lerp(handleZMin.transform.localPosition.z, handleZPlus.transform.localPosition.z, 0.5f)
 			);
-
-			shape.transform.localScale = new Vector3(
+	
+			/*shape.transform.localScale = new Vector3(
 				Vector3.Distance(handleXMin.transform.localPosition, handleXPlus.transform.localPosition) - handleXPlus.StartOffset.x*2.0f,
 				Vector3.Distance(-handleY.transform.localPosition, handleY.transform.localPosition) - handleY.StartOffset.y * 2.0f,
-				Vector3.Distance(handleXMin.transform.localPosition, handleXPlus.transform.localPosition) - handleZPlus.StartOffset.z * 2.0f
-			);
-
+				Vector3.Distance(handleXMin.transform.localPosition, handleXPlus.transform.localPosition)  - handleXPlus.StartOffset.x*2.0f
+			);*/
 		}
 
-		private void ClampHandlesToShape()
+		private void OnDrawGizmos()
 		{
-			ClampHandle(handleXMin);
-			ClampHandle(handleXPlus);
-			ClampHandle(handleZMin);
-			ClampHandle(handleZPlus);
-	}
-
-		private void ClampHandle(Handle handle)
-		{
-			handle.transform.localPosition = new Vector3(
-				(shape.localScale.x / shapeStartScale.x) * handle.StartOffset.x,
-				(shape.localScale.y / shapeStartScale.y) * handle.StartOffset.y,
-				(shape.localScale.z / shapeStartScale.z) * handle.StartOffset.z
-			);
+			string spacer = "";
+			for (int i = 0; i < shape.sharedMesh.vertices.Length; i++)
+			{
+				spacer += "------";
+				Handles.Label(this.transform.position + shape.sharedMesh.vertices[i], "vert:"+i+ spacer);
+			}
 		}
 	}
 }
