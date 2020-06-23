55e8eadda49fffa0ed9364b33d836c90fb8a376d
Sam Baas <samuelpetrus@gmail.com>
2020-06-23 15:20:06 +0200

    added new layer variant overrides for custom shape

---
+++ 3DAmsterdam/Assets/Amsterdam3D/Scripts/Interface/Layers/CustomLayer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Amsterdam3D.Interface
+{
+    public enum CustomLayerType
+    {
+        BASICSHAPE,
+        OBJMODEL
+    }
+    public class CustomLayer : MonoBehaviour
+    {       
+        private CustomLayerType layerType = CustomLayerType.BASICSHAPE;
+        private GameObject linkedObject;
+
+        [SerializeField]
+        private Text layerNameText;
+
+        public void Remove()
+        {
+            //TODO: A confirmation before removing might be required. Verify.
+            Destroy(gameObject);
+        }
+
+        public void Create(string name, GameObject link, CustomLayerType type)
+        {
+            layerType = type;
+            layerNameText.text = name;
+            linkedObject = link;
+        }
+
+        public void ToggleLinkedObject(bool isOn){
+            linkedObject.SetActive(isOn);
+        }
+
+        private void OnDestroy()
+        {
+            GameObject.Destroy(linkedObject);
+        }
+    }
+}
