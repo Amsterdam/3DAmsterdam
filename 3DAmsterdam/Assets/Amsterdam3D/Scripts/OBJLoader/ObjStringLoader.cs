a3db4fd9e322ed3ed22065da2b4d29feff665438
Sam Baas <samuelpetrus@gmail.com>
2020-06-25 14:07:00 +0200

    replaced obj loader with simpler script allowing direct obj parsing from string

--- 3DAmsterdam/Assets/Amsterdam3D/Scripts/OBJLoader/ObjStringLoader.cs
+++ 3DAmsterdam/Assets/Amsterdam3D/Scripts/OBJLoader/ObjStringLoader.cs
@@ -1,15 +1,38 @@
-﻿using Dummiesman;
+﻿using Amsterdam3D.CameraMotion;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using UnityEngine;
 
+namespace Amsterdam3D.UserLayers {
+    public class ObjStringLoader : MonoBehaviour
+    {
+        private Pointer pointer;
 
-public class ObjStringLoader : MonoBehaviour
-{
-    public void LoadOBJFromString(string objText){
-        var textStream = new MemoryStream(Encoding.UTF8.GetBytes(objText));
-        GameObject loadedObj = new OBJLoader().Load(textStream);
+        [SerializeField]
+        private Material defaultLoadedObjectsMaterial;
+
+        private void Start()
+        {
+            pointer = FindObjectOfType<Pointer>();
+        }
+
+#if UNITY_EDITOR
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.L))
+                LoadOBJFromString(File.ReadAllText("C:/Users/Sam/Desktop/untitled.obj"));
+        }
+#endif
+
+        public void LoadOBJFromString(string objText) {
+            var newOBJ = new GameObject().AddComponent<OBJ>();
+            newOBJ.SetGeometryData(objText);
+            newOBJ.Build(defaultLoadedObjectsMaterial);
+            newOBJ.transform.position = pointer.WorldPosition;
+
+            gameObject.SetActive(false);
+        }
     }
 }
