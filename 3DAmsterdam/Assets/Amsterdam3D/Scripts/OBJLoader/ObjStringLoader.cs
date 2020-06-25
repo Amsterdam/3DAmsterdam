ddb001bd0a3a46a4e757d8b2e306aa0ca50a251d
Sam Baas <samuelpetrus@gmail.com>
2020-06-25 12:24:57 +0200

    added obj loading addition to load using a string blob coming from javascript

---
+++ 3DAmsterdam/Assets/Amsterdam3D/Scripts/OBJLoader/ObjStringLoader.cs
@@ -0,0 +1,15 @@
+﻿using Dummiesman;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+
+
+public class ObjStringLoader : MonoBehaviour
+{
+    public void LoadOBJFromString(string objText){
+        var textStream = new MemoryStream(Encoding.UTF8.GetBytes(objText));
+        GameObject loadedObj = new OBJLoader().Load(textStream);
+    }
+}
