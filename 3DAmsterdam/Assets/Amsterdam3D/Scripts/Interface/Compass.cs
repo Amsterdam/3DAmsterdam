cabda6cb98ba6a987fe008ea6a51e1aed119297e
Sam Baas <samuelpetrus@gmail.com>
2020-06-29 16:04:13 +0200

    restored compass script

---
+++ 3DAmsterdam/Assets/Amsterdam3D/Scripts/Interface/Compass.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Compass : MonoBehaviour
+{
+    private Vector3 direction = Vector3.zero;
+    void Update()
+    {
+        direction.z = Camera.main.transform.eulerAngles.y;
+        transform.localEulerAngles = direction * -1.0f;
+    }
+}
