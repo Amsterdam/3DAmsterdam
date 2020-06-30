3415c459d9a9604d1846e731b5a8199fe70c17f2
Sam Baas <samuelpetrus@gmail.com>
2020-06-30 18:44:07 +0200

    added color picker for targeted material color changing

---
+++ 3DAmsterdam/Assets/Amsterdam3D/Scripts/Interface/Visuals/ColorPicker.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ColorPicker : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+	[SerializeField]
+	private RectTransform dragDropRegion;
+
+	[SerializeField]
+	private Image pointer;
+
+	private Rect regionRect;
+	private Rect pickerRect;
+
+	public RawImage colorPalette;
+	public Color pickedColor;
+
+	public Material[] targetMaterials;
+
+	void Start()
+	{
+		Pick();
+	}
+
+	public void OnBeginDrag(PointerEventData EventData)
+	{
+		MovePicker();
+		Pick();
+	}
+	public void OnDrag(PointerEventData EventData = null)
+	{
+		MovePicker();
+		Pick();
+	}
+	public void OnEndDrag(PointerEventData EventData)
+	{
+		MovePicker();
+		Pick();
+	}
+	void MovePicker()
+	{
+		regionRect = RectTransformToScreenSpace(dragDropRegion);
+		pickerRect = RectTransformToScreenSpace(pointer.rectTransform);
+
+		var xPosition = Mathf.Max(Mathf.Min(regionRect.max.x, Input.mousePosition.x), regionRect.min.x);
+		var yPosition = Mathf.Max(Mathf.Min(regionRect.max.y, Input.mousePosition.y), regionRect.min.y);
+
+		pointer.rectTransform.position = new Vector2(xPosition, yPosition);
+	}
+	public void Pick()
+	{
+		//Lets inverse transform point so we can scale stuff as well
+		Vector3 inverseTransform = this.colorPalette.rectTransform.InverseTransformPoint(pointer.rectTransform.position);
+		var colorPalette = (Texture2D)this.colorPalette.texture;
+		int W = colorPalette.width;
+		int H = colorPalette.height;
+
+		var paletteRectangle = this.colorPalette.rectTransform.rect;
+		inverseTransform.x -= paletteRectangle.x;
+		inverseTransform.y -= paletteRectangle.y;
+		inverseTransform.x /= paletteRectangle.width;
+		inverseTransform.y /= paletteRectangle.height;
+
+		//Grab the raw texture pixel at the picker coordinates
+		pickedColor = colorPalette.GetPixel((int)(inverseTransform.x * W), (int)(inverseTransform.y * H));
+		pointer.color = pickedColor;
+
+		//Apply color to selected object/material
+		foreach (var material in targetMaterials)
+		{
+			material.color = pickedColor;
+		}
+	}
+
+	public Rect RectTransformToScreenSpace(RectTransform transform)
+	{
+		Vector2 size = Vector2.Scale(transform.rect.size, transform.lossyScale);
+		return new Rect((Vector2)transform.position - (size * 0.5f), size);
+	}
+}
