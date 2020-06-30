8641071f990836e659fef27798f362ff965f204f
Sam Baas <samuelpetrus@gmail.com>
2020-06-30 17:03:00 +0200

    allow use of transform parent instead only freehape parent

--- 3DAmsterdam/Assets/Amsterdam3D/Scripts/ObjectManipulation/RotationHandle.cs
+++ 3DAmsterdam/Assets/Amsterdam3D/Scripts/ObjectManipulation/RotationHandle.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 
 public class RotationHandle : ObjectManipulation
 {
+	[SerializeField]
+	private Transform rotationCenter;
+
 	private GameObject rotationOrigin;
-	private FreeShape parentFreeShape;
 	private Vector3 dragStartLocation;
 	private float startYRotation = 0;
+
 	private void Start()
 	{
-		parentFreeShape = GetComponentInParent<FreeShape>();
+		if (!rotationCenter)
+			rotationCenter = this.transform;
 	}
 
 	public override void OnMouseDown()
@@ -19,19 +23,19 @@ public class RotationHandle : ObjectManipulation
 		base.OnMouseDown();
 		rotationOrigin = new GameObject();
 		rotationOrigin.name = "CustomShapeRotationOrigin";
-		rotationOrigin.transform.position = parentFreeShape.FloorOrigin.position;
+		rotationOrigin.transform.position = rotationCenter.position;
 
 		dragStartLocation = GetWorldPositionOnPlane(Input.mousePosition, this.transform.position.y);
 
 		startYRotation = rotationOrigin.transform.eulerAngles.y;
 
-		parentFreeShape.transform.SetParent(rotationOrigin.transform);
+		transform.parent.SetParent(rotationOrigin.transform);
 
 	}
 	public override void OnMouseUp()
 	{
 		base.OnMouseUp();
-		this.transform.parent.SetParent(null);
+		transform.parent.SetParent(null);
 		Destroy(rotationOrigin);
 	}
 
@@ -53,7 +57,6 @@ public class RotationHandle : ObjectManipulation
 		Debug.DrawLine(rotationOrigin.transform.position, rotationOrigin.transform.position+targetNormal, Color.red);
 
 		var angle = Vector3.SignedAngle(targetNormal, startNormal,Vector3.up);
-
 		rotationOrigin.transform.rotation = Quaternion.Euler(0.0f,-angle,0.0f);
 	}
 }
