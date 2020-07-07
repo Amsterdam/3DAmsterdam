dc953b2da3ba1659a359d536afbea3b6999f8640
Sam Baas <samuelpetrus@gmail.com>
2020-07-07 15:04:10 +0200

    give me feedback on the selected color

--- 3DAmsterdam/Assets/Amsterdam3D/Scripts/Interface/Visuals/ColorSelector.cs
+++ 3DAmsterdam/Assets/Amsterdam3D/Scripts/Interface/Visuals/ColorSelector.cs
@@ -6,5 +6,15 @@ public abstract class ColorSelector : MonoBehaviour
 	public delegate void SelectedNewColor(Color color, ColorSelector selector);
 	public SelectedNewColor selectedNewColor;
 
+	private void Awake()
+	{
+		selectedNewColor += PickedColorMessage;
+	}
+
+	private void PickedColorMessage(Color color, ColorSelector selector)
+	{
+		print("Picked color " + color + " using " + selector.name);
+	}
+
 	public abstract void ChangeColorInput(Color inputColor);
 }
