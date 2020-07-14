35181bd4cc74d66fa1964142322f7223d75ce790
Sam Baas <samuelpetrus@gmail.com>
2020-07-14 18:33:23 +0200

    added a modular script allowing to register clicks outside their gameobject to close

---
+++ 3DAmsterdam/Assets/Amsterdam3D/Scripts/Interface/ClickOutsideToClose.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ClickOutsideToClose : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1) || Input.GetMouseButtonDown(2))
+        {
+            this.gameObject.SetActive(ClickingSelfOrChild());
+        }
+    }
+    private bool ClickingSelfOrChild()
+    {
+        RectTransform[] rectTransforms = GetComponentsInChildren<RectTransform>();
+        foreach(RectTransform rectTransform in rectTransforms)
+        {
+            if (EventSystem.current.currentSelectedGameObject == rectTransform.gameObject)
+            {
+                return true;
+            };
+        }
+        return false;
+    }
+}
