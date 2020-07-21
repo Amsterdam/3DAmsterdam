9a5c71920abcf0d4a90f9e92f08857b974664178
Sam Baas <samuelpetrus@gmail.com>
2020-07-21 15:25:28 +0200

    added constant url paths for different branch types

--- 3DAmsterdam/Assets/Amsterdam3D/Scripts/Utilities/Constants.cs
+++ 3DAmsterdam/Assets/Amsterdam3D/Scripts/Utilities/Constants.cs
@@ -1,4 +1,21 @@
 ﻿public static class Constants
 {
+    /// <summary>
+    /// The world Y axis is based on GPS coordinates.
+    /// The height of 43 is close to 0 NAP, a nice average value for the Amsterdam ground level.
+    /// </summary>
     public const float ZERO_GROUND_LEVEL_Y = 43.0f;
+
+    /// <summary>
+    /// Swap data URL based on the branch type. If we build, use a relative path in WebGL.
+    /// </summary>
+#if !UNITY_EDITOR && UNITY_WEBGL
+    public const string BASE_DATA_URL = "./AssetBundles/WebGL/";
+#elif UNITY_EDITOR && BRANCH_MASTER
+    public const string BASE_DATA_URL = "https://3d.amsterdam.nl/web/app/AssetBundles/WebGL/";
+#elif UNITY_EDITOR && BRANCH_DEVELOP
+    public const string BASE_DATA_URL = "https://acc.3d.amsterdam.nl/webmap/AssetBundles/WebGL/";
+#elif UNITY_EDITOR && BRANCH_FEATURE
+    public const string BASE_DATA_URL = "https://acc.3d.amsterdam.nl/webmap/AssetBundles/WebGL/";
+#endif
 }
