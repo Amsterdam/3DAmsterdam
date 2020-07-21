019900cec66c9e8b2d6fa1ae6d7390a4768ed18a
Sam Baas <samuelpetrus@gmail.com>
2020-07-21 15:25:07 +0200

    read feature branch names from git HEAD file for specific feature branch pathing

--- 3DAmsterdam/Assets/Amsterdam3D/Scripts/Utilities/Editor/SwapBuildTarget.cs
+++ 3DAmsterdam/Assets/Amsterdam3D/Scripts/Utilities/Editor/SwapBuildTarget.cs
@@ -3,30 +3,55 @@ using UnityEditor;
 using UnityEditor.Build.Reporting;
 using UnityEngine;
 using System.Linq;
+using System.IO;
 
 namespace Amsterdam3D.Utilities
 {
     public class SwapBuildTarget : MonoBehaviour
     {
-		public enum BranchType{
-			MASTER,
-			DEVELOP,
-			FEATURE
-		}
+        [MenuItem("3D Amsterdam/Set branch type/master")]
+        public static void SwitchBranchMaster()
+        {
+            PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.WebGL,"BRANCH_MASTER");
+            PlayerSettings.bundleVersion = "master";
+            Debug.Log("Set scripting define symbols to BRANCH_MASTER");
+        }
+        [MenuItem("3D Amsterdam/Set branch type/develop")]
+        public static void SwitchBranchDevelop()
+        {
+            PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.WebGL, "BRANCH_DEVELOP");
+            PlayerSettings.bundleVersion = "develop";
+            Debug.Log("Set scripting define symbols to BRANCH_DEVELOP");
+        }
+        [MenuItem("3D Amsterdam/Set branch type/feature")]
+        public static void SwitchBranchFeature()
+        {
+            PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.WebGL, "BRANCH_FEATURE");
 
-        [MenuItem("3D Amsterdam/Build for WebGL")]
+            var gitHeadFile = Application.dataPath + "/../../.git/HEAD";
+            var headLine = File.ReadAllText(gitHeadFile);
+            var positionLastSlash = headLine.LastIndexOf("/") + 1;
+            var featureName = headLine.Substring(positionLastSlash, headLine.Length - positionLastSlash);
+
+            PlayerSettings.bundleVersion = "feature/" + featureName;
+            Debug.Log("Version set to feature name: " + Application.version);
+
+            Debug.Log("Set scripting define symbols to BRANCH_FEATURE");
+        }
+        [MenuItem("3D Amsterdam/Build for WebGL platform")]
         public static void BuildWebGL()
         {
             TargetedBuild(BuildTarget.WebGL);
         }
 
-        [MenuItem("3D Amsterdam/Build for Windows 64 bit")]
+        //Optional other future platform targets, for example desktop:
+        /*[MenuItem("3D Amsterdam/Build for Windows 64 bit")]
         public static void BuildWindows()
         {
             TargetedBuild(BuildTarget.StandaloneWindows64);
-        }
+        }*/
 
-        public static void TargetedBuild(BuildTarget buildTarget = BuildTarget.WebGL, BranchType branchType = BranchType.FEATURE)
+        public static void TargetedBuild(BuildTarget buildTarget = BuildTarget.WebGL)
         {           
             BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions()
             {
