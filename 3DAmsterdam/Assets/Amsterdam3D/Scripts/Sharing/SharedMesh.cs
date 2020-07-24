e62de23cc1d01d2ba1ac4f3842bb167b4a44275d
Sam Baas <samuelpetrus@gmail.com>
2020-07-24 13:04:00 +0200

    added own shared mesh structure for serializing meshes. we cant store objects directly , due to unserializable variables

---
+++ 3DAmsterdam/Assets/Amsterdam3D/Scripts/Sharing/SharedMesh.cs
@@ -0,0 +1,21 @@
+﻿using Packages.Rider.Editor.UnitTesting;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Amsterdam3D.Sharing{ 
+	[System.Serializable]
+	public struct SharedMesh{
+		public int meshBitType;
+		public SharedSubMesh[] subMeshes;
+		public float[] verts;
+		public float[] uvs;
+		public float[] normals;
+	}
+	[System.Serializable]
+	public struct SharedSubMesh
+	{
+		public string materialId;
+		public int[] triangles; //vert ids, per 3
+	}
+}
