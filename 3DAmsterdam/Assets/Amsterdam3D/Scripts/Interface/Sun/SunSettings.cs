5a1c1217d1f5a199c71922f8eef2a289c34c7841
Sam Baas <samuelpetrus@gmail.com>
2020-08-05 16:20:40 +0200

    main sun methods

--- 3DAmsterdam/Assets/Amsterdam3D/Scripts/Interface/Sun/SunSettings.cs
+++ 3DAmsterdam/Assets/Amsterdam3D/Scripts/Interface/Sun/SunSettings.cs
@@ -1,18 +1,63 @@
-﻿using System.Collections;
+﻿using Amsterdam3D.Sun;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SunSettings : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private SunDragWheel sunDragWheel;
+
+    [SerializeField]
+    private InputField timeInput;
+
+    [SerializeField]
+    private InputField dayInput;
+    [SerializeField]
+    private InputField monthInput;
+    [SerializeField]
+    private InputField yearInput;
+
+    private Text speedMultiplierText;
+    private int speedMultiplier = 1;
+
+    private bool useTimedSun = false;
+    private bool paused = false;
+
+    [SerializeField]
+    private Image coverTimeSettings;
+
+    public void Start()
+    {
+        sunDragWheel.changedDirection += SunPositionChangedFromOutside;
+    }
+
+    private void SunPositionChangedFromOutside(float rotation){
+        //Convert flat rotation to expected time/sun position
+    }
+
+    private void ToggleTimedSun(bool useTimedSun)
+    {
+        coverTimeSettings.enabled = !useTimedSun;
+    }
+
+    private void TogglePausePlay()
+    {
+        
+    }
+    
+    public void ResetTimeToNow()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void FastForward()
     {
         
+    }
+    public void FastBackward()
+    {
+
     }
 }
