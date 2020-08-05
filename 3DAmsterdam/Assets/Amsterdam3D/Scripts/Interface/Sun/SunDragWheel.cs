b506a6787df276c75f5f84779b9fd355bf8cce0d
Sam Baas <samuelpetrus@gmail.com>
2020-08-05 15:15:34 +0200

    dragging of sun wheel rotates the sun/moon icons

---
+++ 3DAmsterdam/Assets/Amsterdam3D/Scripts/Interface/Sun/SunDragWheel.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SunDragWheel : MonoBehaviour, IBeginDragHandler, IDragHandler
+{
+    [SerializeField]
+    private RectTransform moonIcon;
+    [SerializeField]
+    private RectTransform sunIcon;
+
+    [SerializeField]
+    private float rotationSnapDegrees;
+    private float beginDragAngle = 0.0f;
+
+    private void Update()
+    {
+        //Keep icons straight
+        moonIcon.rotation = Quaternion.identity;
+        sunIcon.rotation = Quaternion.identity;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        //Get starting rotation, so we rotate from this point
+        Vector3 relativePosition = transform.position;
+        relativePosition = Input.mousePosition - relativePosition;
+        beginDragAngle = Mathf.Atan2(relativePosition.y, relativePosition.x) * Mathf.Rad2Deg;
+        beginDragAngle -= Mathf.Atan2(transform.right.y, transform.right.x) * Mathf.Rad2Deg;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        //Rotate wheel according to relative mouse position
+        Vector3 relativePosition = transform.position;
+        relativePosition = Input.mousePosition - relativePosition;
+        float ang = Mathf.Atan2(relativePosition.y, relativePosition.x) * Mathf.Rad2Deg - beginDragAngle;
+        ang -= (ang % (360.0f / rotationSnapDegrees));
+        transform.rotation = Quaternion.AngleAxis(ang, Vector3.forward);
+    }
+}
