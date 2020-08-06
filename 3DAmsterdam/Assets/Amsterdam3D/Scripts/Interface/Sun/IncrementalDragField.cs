150e0e525a07b1408be599f07c0152c9b6cc5d9d
Sam Baas <samuelpetrus@gmail.com>
2020-08-06 17:53:51 +0200

    added incremental drag drop for fields (no scrollng added yet)

--- 3DAmsterdam/Assets/Amsterdam3D/Scripts/Interface/Sun/IncrementalDragField.cs
+++ 3DAmsterdam/Assets/Amsterdam3D/Scripts/Interface/Sun/IncrementalDragField.cs
@@ -1,17 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class IncrementalDragField : MonoBehaviour
+public class IncrementalDragField : MonoBehaviour, IPointerDownHandler,IDragHandler, IEndDragHandler
 {
-    // Start is called before the first frame update
+	[SerializeField]
+	private float horizontalIncrement = 5.0f;
+
+	private float startDragX = 0;
+
+	private int startValue = 0;
+
+	[SerializeField]
+	private int minValue = 0;
+	[SerializeField]
+	private int maxValue = 1440;
+
+	[SerializeField]
+	private bool loop = false;
+
+	[SerializeField]
+	private int value = 0;
+
     void Start()
     {
-        
+		        
     }
 
-    // Update is called once per frame
-    void Update()
+	public void OnPointerDown(PointerEventData eventData)
+	{
+		startDragX = Input.mousePosition.x;
+		startValue = value;
+	}
+
+	public void OnDrag(PointerEventData eventData)
+	{
+		value = startValue + Mathf.RoundToInt((Input.mousePosition.x - startDragX) / horizontalIncrement);
+		if (value > maxValue)
+		{
+			value = minValue + (value - maxValue);
+		}
+		else if(value < minValue)
+		{
+			value = maxValue + value;
+		}
+	}
+
+	public void OnEndDrag(PointerEventData eventData)
+	{
+		
+	}
+
+	// Update is called once per frame
+	void Update()
     {
         
     }
