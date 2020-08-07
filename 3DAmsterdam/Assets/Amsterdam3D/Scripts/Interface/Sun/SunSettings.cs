78e734056c3aa8191ad75f4ce8847368dd3b2aac
Sam Baas <samuelpetrus@gmail.com>
2020-08-07 13:57:00 +0200

    added behaviour for updating input fields

--- 3DAmsterdam/Assets/Amsterdam3D/Scripts/Interface/Sun/SunSettings.cs
+++ 3DAmsterdam/Assets/Amsterdam3D/Scripts/Interface/Sun/SunSettings.cs
@@ -1,8 +1,10 @@
 ﻿using Amsterdam3D.Sun;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Experimental.GlobalIllumination;
+using UnityEngine.Rendering;
 using UnityEngine.UI;
 
 public class SunSettings : MonoBehaviour
@@ -14,14 +16,14 @@ public class SunSettings : MonoBehaviour
     private SunDragWheel sunDragWheel;
 
     [SerializeField]
-    private InputField timeInput;
+    private LimitedNumericInput timeInput;
 
     [SerializeField]
-    private InputField dayInput;
+    private LimitedNumericInput dayInput;
     [SerializeField]
-    private InputField monthInput;
+    private LimitedNumericInput monthInput;
     [SerializeField]
-    private InputField yearInput;
+    private LimitedNumericInput yearInput;
 
     private Text speedMultiplierText;
     private int speedMultiplier = 1;
@@ -32,9 +34,35 @@ public class SunSettings : MonoBehaviour
     [SerializeField]
     private Image coverTimeSettings;
 
+    private DateTime dateTimeNow;
+
+    public int SpeedMultiplier {
+        get {
+            return speedMultiplier;
+        }
+        set {
+            speedMultiplier = value;
+            speedMultiplierText.text = speedMultiplier.ToString();
+        }
+    }
+
     public void Start()
     {
         sunDragWheel.changedDirection += SunPositionChangedFromOutside;
+    }
+
+    private void Update()
+    {
+        if (useTimedSun && !paused)
+        {
+            dateTimeNow.AddMinutes(Time.deltaTime * SpeedMultiplier);
+
+            timeInput.Value = dateTimeNow.Hour*60 + dateTimeNow.Minute; //This value is counted in 'minute of the day'
+
+            dayInput.Value = dateTimeNow.Day;
+            monthInput.Value = dateTimeNow.Month;
+            yearInput.Value = dateTimeNow.Year;
+        }
     }
 
     private void SunPositionChangedFromOutside(float rotation){
@@ -42,27 +70,31 @@ public class SunSettings : MonoBehaviour
         Debug.Log("Rotate sun..");
     }
 
-    private void ToggleTimedSun(bool useTimedSun)
+    public void ToggleTimedSun(bool timedSun)
     {
-        coverTimeSettings.enabled = !useTimedSun;
-    }
-
-    private void TogglePausePlay()
-    {
-        
-    }
-    
-    public void ResetTimeToNow()
-    {
-        
+        useTimedSun = timedSun;
+        ResetTimeToNow();
+        coverTimeSettings.gameObject.SetActive(!useTimedSun);
     }
 
     public void FastForward()
     {
-        
+        SpeedMultiplier *= 10;
+        if(SpeedMultiplier > 1000) SpeedMultiplier = 1000;
     }
     public void FastBackward()
     {
-
+        SpeedMultiplier /= 10;
+        if (SpeedMultiplier < 1) SpeedMultiplier = 1;
     }
+    public void TogglePausePlay()
+    {
+        paused = !paused;
+    }
+    
+    public void ResetTimeToNow()
+    {
+        dateTimeNow = DateTime.Now;
+    }
+
 }
