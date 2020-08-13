a1b9144d2650999aa444f21253828ec42b91fa81
Sam Baas <samuelpetrus@gmail.com>
2020-08-13 15:38:45 +0200

    annotation prefab with new world point follower base class

---
+++ 3DAmsterdam/Assets/Amsterdam3D/Scripts/Interface/Annotations/Annotation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Annotation : WorldPointFollower
+{
+    [SerializeField]
+    private Image balloon;
+
+    [SerializeField]
+    private Text balloonText;
+
+    [SerializeField]
+    private Input editInputField;
+
+    void Start()
+    {
+        MoveToWorldPosition(Vector3.zero);
+    }
+}
