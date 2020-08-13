3e6ce454277f18bad8ac3795b42f104cbeddd292
Sam Baas <samuelpetrus@gmail.com>
2020-08-13 17:05:44 +0200

    added dragging interface for annotation, placing it onto a virtual plane at average world height

--- 3DAmsterdam/Assets/Amsterdam3D/Scripts/Interface/Annotations/Annotation.cs
+++ 3DAmsterdam/Assets/Amsterdam3D/Scripts/Interface/Annotations/Annotation.cs
@@ -1,9 +1,12 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class Annotation : WorldPointFollower
+public class Annotation : WorldPointFollower, IDragHandler, IPointerClickHandler
 {
     [SerializeField]
     private Image balloon;
@@ -12,10 +15,44 @@ public class Annotation : WorldPointFollower
     private Text balloonText;
 
     [SerializeField]
-    private Input editInputField;
+    private InputField editInputField;
 
-    void Start()
+    private Plane groundPlane;
+    private float lastClickTime = 0;
+    private float doubleClickTime = 0.2f;
+
+    public void OnDrag(PointerEventData eventData)
     {
-        MoveToWorldPosition(Vector3.zero);
+        groundPlane = new Plane(Vector3.up,new Vector3(0, Constants.ZERO_GROUND_LEVEL_Y, 0));
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+       
+        if (groundPlane.Raycast(ray, out float enter))
+        {
+            Vector3 hitPoint = ray.GetPoint(enter);
+            MoveToWorldPosition(hitPoint);
+        }
     }
-}
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (Time.time - lastClickTime < doubleClickTime)
+        {
+            StartEditingText();
+        }
+        lastClickTime = Time.time;
+    }
+
+    private void StartEditingText()
+    {
+        editInputField.gameObject.SetActive(true);
+        editInputField.text = balloonText.text;
+
+        editInputField.Select();
+    }
+
+    public void StopEditingText()
+    {
+        balloonText.text = editInputField.text;
+        editInputField.gameObject.SetActive(false);
+    }
+} 
