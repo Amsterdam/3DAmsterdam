f5ae50406f7f127485f20666497a4af2cdc67e34
mvangog <martijn.van.gog@amsterdam.nl>
2020-08-19 16:21:45 +0200

    first setup

---
+++ 3DAmsterdam/Assets/Amsterdam3D/Scripts/LayerSystem/Layer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace LayerSystem
+{
+    public class Layer : MonoBehaviour
+    {
+        [SerializeField]
+        public int tileSize = 1000;
+        public int layerPriority = 0;
+        public List<DataSet> Datasets = new List<DataSet>();
+        public Dictionary<Vector2Int, Tile> tiles = new Dictionary<Vector2Int, Tile>();
+
+        void Start()
+        {
+            foreach (DataSet dataset in Datasets)
+            {
+                dataset.maximumDistanceSquared = dataset.maximumDistance * dataset.maximumDistance;
+            }
+        }
+
+    }
+}
---
+++ 3DAmsterdam/Assets/Amsterdam3D/Scripts/LayerSystem/TileHandler.cs
@@ -0,0 +1,365 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BruTile;
+using ConvertCoordinates;
+using System.Linq;
+using UnityEngine.Networking;
+
+namespace LayerSystem
+{
+    public enum TileAction
+    {
+        Create,
+        Upgrade,
+        Downgrade,
+        Remove
+    }
+    public class TileHandler : MonoBehaviour
+    {
+
+
+        public int maximumConcurrentDownloads = 5;
+        public List<Layer> layers = new List<Layer>();
+        private List<int> tileSizes = new List<int>();
+
+        private List<List<Vector3Int>> TileDistances = new List<List<Vector3Int>>();
+        
+        private List<TileChange> pendingTileChanges = new List<TileChange>();
+        private Dictionary<Vector3Int, TileChange> activeTileChanges = new Dictionary<Vector3Int, TileChange>();
+        // key : Vector3Int where
+        //                  x = bottomleft x-coordinate in RD
+        //                  y = bottomleft y-coordinate in RD
+        //                  z = layerIndex.
+
+        private Vector4 viewRange = new Vector4();
+        // x= minimum X-coordinate in RD
+        // y= minimum Y-coordinate in RD
+        // z= size in X-direction in M.
+        // w= size in Y-direction in M.
+        private CameraView CV;
+        private Vector3Int cameraPosition;
+        private Extent previousCameraViewExtent;
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            CV = Camera.main.GetComponent<CameraView>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (HasCameraViewChanged())
+            {
+                UpdateViewRange();
+                GetTilesizes();
+                getPossibleTiles();
+                GetTileChanges();
+            }
+
+            if (pendingTileChanges.Count==0){return;}
+
+            if (activeTileChanges.Count<maximumConcurrentDownloads)
+            {
+                TileChange highestPriorityTIleChange = FindHighestPriorityTileChange();
+                activeTileChanges.Add(new Vector3Int(highestPriorityTIleChange.X, highestPriorityTIleChange.Y, highestPriorityTIleChange.layerIndex), highestPriorityTIleChange);
+                pendingTileChanges.Remove(highestPriorityTIleChange);
+                HandleTile(highestPriorityTIleChange);
+            }
+            // only if viewRange has changed:
+            // check which tiles are required in view for each tilesize and add to TileDistances (key: X-bottom-left,Y-bottom-left,tilesize)
+            // calculate distance for each tile in TileDistances
+                
+            // for each active Layer:
+                // check which tiles can be destroyed
+                // check which tiles can be removed from DownloadQueue
+
+                // only for tiles that have status.Ready:
+                // check required LOD for each tile
+                // up- or downgrade LOD and set status.pendingdownload
+
+            // only for tiles that have status.pendingdownload:
+            //start the download, prioritize bij LOD and layer-priority
+        }
+
+        private void HandleTile(TileChange tileChange)
+        {
+            int lod=10;
+            string url;
+            switch (tileChange.action)
+            {
+                case TileAction.Create:
+                    lod = 0;
+
+                    break;
+                case TileAction.Upgrade:
+                    lod = layers[tileChange.layerIndex].tiles[new Vector2Int(tileChange.X, tileChange.Y)].LOD + 1;
+                    
+                    break;
+                case TileAction.Downgrade:
+                    lod = layers[tileChange.layerIndex].tiles[new Vector2Int(tileChange.X, tileChange.Y)].LOD - 1;
+                    
+                    break;
+                case TileAction.Remove:
+                    break;
+                default:
+                    break;
+            }
+            url = Constants.BASE_DATA_URL + layers[tileChange.layerIndex].Datasets[lod].path;
+            url = url.Replace("{x}", tileChange.X.ToString());
+            url = url.Replace("{y}", tileChange.Y.ToString());
+            url = url.Replace("{lod}", lod.ToString());
+            StartCoroutine(DownloadTile(url, tileChange));
+        }
+
+        private IEnumerator DownloadTile(string url, TileChange tileChange)
+        {
+            using (UnityWebRequest uwr = UnityWebRequestAssetBundle.GetAssetBundle(url))
+            {
+                yield return uwr.SendWebRequest();
+
+                if (uwr.isNetworkError || uwr.isHttpError)
+                {
+                    activeTileChanges.Remove(new Vector3Int(tileChange.X, tileChange.Y, tileChange.layerIndex));
+                }
+                else
+                {
+                    // Get downloaded asset bundle
+                    //AssetBundle bundle = DownloadHandlerAssetBundle.GetContent(uwr);
+                    //tileData.assetBundle = bundle;
+                    //activeDownloads.Remove(tileData.tileID);
+                    //pendingBuilds.Add(tileData.tileID);
+                }
+            }
+            yield return null;
+
+        }
+        private TileChange FindHighestPriorityTileChange()
+        {
+            TileChange highestPriorityTileChange = pendingTileChanges[0];
+            float highestPriority = highestPriorityTileChange.priorityScore;
+            
+            for (int i = 1; i < pendingTileChanges.Count; i++)
+            {
+                if (pendingTileChanges[i].priorityScore < highestPriority)
+                {
+                    highestPriorityTileChange = pendingTileChanges[i];
+                    highestPriority = highestPriorityTileChange.priorityScore;
+                }
+            }
+            return highestPriorityTileChange;
+        }
+
+        private void UpdateViewRange()
+        {
+            Vector3RD bottomleft = CoordConvert.WGS84toRD(CV.cameraExtent.MinX, CV.cameraExtent.MinY);
+            Vector3RD topright = CoordConvert.WGS84toRD(CV.cameraExtent.MaxX, CV.cameraExtent.MaxY);
+
+            viewRange.x = (float)bottomleft.x;
+            viewRange.y = (float)bottomleft.y;
+            viewRange.z = (float)(topright.x -bottomleft.x);
+            viewRange.w = (float)(topright.y-bottomleft.y);
+
+            Vector3RD cameraPositionRD = CoordConvert.UnitytoRD(CV.gameObject.transform.position);
+            cameraPosition.x = (int)cameraPositionRD.x;
+            cameraPosition.y = (int)cameraPositionRD.y;
+            cameraPosition.z = (int)cameraPositionRD.z;
+        }
+
+        private bool HasCameraViewChanged()
+        {
+            bool cameraviewChanged = false;
+            if (previousCameraViewExtent.CenterX != CV.cameraExtent.CenterX || previousCameraViewExtent.CenterY != CV.cameraExtent.CenterY)
+            {
+                cameraviewChanged = true;
+                previousCameraViewExtent = CV.cameraExtent;
+            }
+            return cameraviewChanged;
+        }
+
+        private void getPossibleTiles()
+        {
+            TileDistances.Clear();
+
+            int startX;
+            int startY;
+            int endX;
+            int endY;
+            foreach (int tileSize in tileSizes)
+            {
+                List<Vector3Int> tileList = new List<Vector3Int>();
+                startX = (int)Math.Floor(viewRange.x / tileSize) * tileSize;
+                startY = (int)Math.Floor(viewRange.y / tileSize) * tileSize;
+                endX = (int)Math.Ceiling((viewRange.x+viewRange.z) / tileSize) * tileSize;
+                endY = (int)Math.Ceiling((viewRange.y + viewRange.z) / tileSize) * tileSize;
+                for (int x = startX; x < endX; x+=tileSize)
+                {
+                    for (int y = startY; y < endY; y+=tileSize)
+                    {
+                        Vector3Int tileID = new Vector3Int(x, y, tileSize);
+                        tileList.Add(new Vector3Int(x,y, (int)GetTileDistanceSquared(tileID)));
+                    }
+                }
+                TileDistances.Add(tileList);
+            }
+        }
+
+        private float GetTileDistanceSquared(Vector3Int tileID)
+        {
+            float distance =0;
+            int centerOffset = (int)tileID.z / 2;
+            Vector3Int center = new Vector3Int(tileID.x + centerOffset, tileID.y + centerOffset, 0);
+            float delta = center.x - cameraPosition.x;
+            distance += delta * delta;
+            delta = center.y - cameraPosition.y;
+            distance += delta * delta;
+            delta = cameraPosition.z * cameraPosition.z;
+            distance += delta;
+
+            //Vector3Int difference = new Vector3Int(, tileID.y+centerOffset, 0) - cameraPosition;
+            //distance = difference.magnitude;
+            return distance;
+        }
+
+        private void GetTilesizes()
+        {
+            int tilesize;
+            tileSizes.Clear();
+            foreach (Layer layer in layers)
+            {
+                if (layer.gameObject.activeSelf==true)
+                {
+                    tilesize = layer.tileSize;
+                    if (tileSizes.Contains(tilesize) == false)
+                    {
+                        tileSizes.Add(tilesize);
+                    }
+                }
+            }
+        }
+
+        private int CalculatePriorityScore(int layerPriority, int lod)
+        {
+            return (10 * lod) - layerPriority;
+        }
+
+        private int CalculateLOD(Vector3Int tiledistance, Layer layer)
+        {
+            int lod = -1;
+
+            foreach (DataSet dataSet in layer.Datasets)
+            {
+                if (dataSet.maximumDistanceSquared>tiledistance.z)
+                {
+                    lod = dataSet.lod;
+                    return lod;
+                }
+            }
+
+            return lod;
+        }
+
+        private void GetTileChanges()
+        {
+            pendingTileChanges.Clear();
+
+            for (int layerIndex = 0; layerIndex < layers.Count; layerIndex++)
+            {
+                Layer layer = layers[layerIndex];
+            if (layer.gameObject.activeSelf==false){continue;}
+                int tilesizeIndex = tileSizes.IndexOf(layer.tileSize);
+                foreach (Vector3Int tileDistance in TileDistances[tilesizeIndex])
+                {
+                    Vector2Int tileKey = new Vector2Int(tileDistance.x, tileDistance.y);
+                    
+                    if (activeTileChanges.ContainsKey(new Vector3Int(tileKey.x, tileKey.y, layerIndex)))
+                    {
+                        continue;
+                    }
+                    int LOD = CalculateLOD(tileDistance, layer);
+                    if (LOD == -1)
+                    {
+                        if (layer.tiles.ContainsKey(tileKey))
+                        {
+                            TileChange tileChange = new TileChange();
+                            tileChange.action = TileAction.Remove;
+                            tileChange.X = tileKey.x;
+                            tileChange.Y = tileKey.y;
+                            pendingTileChanges.Add(tileChange);
+                            
+                        }
+                        continue;
+                    }
+
+                    if (layer.tiles.ContainsKey(tileKey))
+                    {
+                        int activeLOD = layer.tiles[tileKey].LOD;
+                        if (activeLOD > LOD)
+                        {
+                            TileChange tileChange = new TileChange();
+                            tileChange.action = TileAction.Downgrade;
+                            tileChange.X = tileKey.x;
+                            tileChange.Y = tileKey.y;
+                            tileChange.layerIndex = layerIndex;
+                            tileChange.priorityScore = CalculatePriorityScore(layer.layerPriority, activeLOD - 1);
+                            pendingTileChanges.Add(tileChange);
+                        }
+                        else if (activeLOD<LOD)
+                        {
+                            TileChange tileChange = new TileChange();
+                            tileChange.action = TileAction.Upgrade;
+                            tileChange.X = tileKey.x;
+                            tileChange.Y = tileKey.y;
+                            tileChange.layerIndex = layerIndex;
+                            tileChange.priorityScore = CalculatePriorityScore(layer.layerPriority, activeLOD + 1);
+                            pendingTileChanges.Add( tileChange);
+                        }
+                    }
+                    else
+                    {
+                        TileChange tileChange = new TileChange();
+                        tileChange.action = TileAction.Create;
+                        tileChange.X = tileKey.x;
+                        tileChange.Y = tileKey.y;
+                        tileChange.priorityScore = CalculatePriorityScore(layer.layerPriority, 0);
+                        tileChange.layerIndex = layerIndex;
+                        pendingTileChanges.Add( tileChange);
+                    }
+                }
+            }
+           
+        }
+
+    }
+    [Serializable]
+    public class DataSet
+    {
+        public string Description;
+        public int lod;
+        public string path;
+        public float maximumDistance;
+        [HideInInspector]
+        public float maximumDistanceSquared;
+        public bool enabled = true;
+    }
+
+    public class Tile
+    {
+        public int LOD;
+        public GameObject gameObject;
+        public AssetBundle assetBundle;
+        public Vector2Int tileKey;
+    }
+    public class TileChange
+    {
+        public TileAction action;
+        public int priorityScore;
+        public int layerIndex;
+        public int X;
+        public int Y;
+    }
+
+}
