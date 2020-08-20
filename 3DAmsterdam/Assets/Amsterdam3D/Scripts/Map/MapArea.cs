f1fbd1633e39b2a84e6a51282e3fc694b7e9c12d
Sam Baas <samuelpetrus@gmail.com>
2020-08-20 10:34:18 +0200

    catch hover events for resize and showing buttons

--- 3DAmsterdam/Assets/Amsterdam3D/Scripts/Map/MapArea.cs
+++ 3DAmsterdam/Assets/Amsterdam3D/Scripts/Map/MapArea.cs
@@ -4,19 +4,63 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class MapArea : MonoBehaviour, IBeginDragHandler,IDragHandler, IScrollHandler
+public class MapArea : MonoBehaviour, IBeginDragHandler,IDragHandler, IScrollHandler, IPointerEnterHandler, IPointerExitHandler
 {
     private Vector3 dragOffset;
     private Map map;
 
+    private RectTransform rectTransform;
+
+    [SerializeField]
+    private float hoverResizeSpeed = 1.0f;
+
+    [SerializeField]
+    private RectTransform navigation;
+
+    private Vector2 defaultSize;
+    [SerializeField]
+    private Vector2 hoverSize;
+
+    private void Awake()
+    {
+        rectTransform = this.GetComponent<RectTransform>();
+    }
+
     private void Start()
     {
         map = GetComponentInChildren<Map>();
+        defaultSize = rectTransform.sizeDelta;
+        navigation.gameObject.SetActive(false);
     }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
         dragOffset = map.transform.position - Input.mousePosition;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        navigation.gameObject.SetActive(true);
+
+        StopAllCoroutines();
+        StartCoroutine(HoverResize(hoverSize));
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        navigation.gameObject.SetActive(false);
+
+        StopAllCoroutines();
+        StartCoroutine(HoverResize(defaultSize));
+    }
+
+    IEnumerator HoverResize(Vector2 targetScale)
+    {
+        while (Vector2.Distance(targetScale, transform.localScale) > 0.01f)
+        {
+            rectTransform.sizeDelta = Vector2.Lerp(rectTransform.sizeDelta, targetScale, hoverResizeSpeed * Time.deltaTime);
+            yield return null;
+        }
     }
 
     public void OnDrag(PointerEventData eventData)
