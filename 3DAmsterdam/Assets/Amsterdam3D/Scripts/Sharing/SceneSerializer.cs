650e774ed4aa3f357f547122d1246b5baf65d1b1
Sam Baas <samuelpetrus@gmail.com>
2020-08-31 16:04:08 +0200

    added path based on constants

--- 3DAmsterdam/Assets/Amsterdam3D/Scripts/Sharing/SceneSerializer.cs
+++ 3DAmsterdam/Assets/Amsterdam3D/Scripts/Sharing/SceneSerializer.cs
@@ -56,9 +56,9 @@ namespace Amsterdam3D.Sharing
 
         IEnumerator GetSharedScene(string sceneId)
         {
-            Debug.Log(Constants.SHARE_URL + sceneId + "_scene.json");
+            Debug.Log(Constants.SHARE_URL + Constants.SHARE_OBJECTSTORE_PATH + sceneId + "_scene.json");
 
-            UnityWebRequest getSceneRequest = UnityWebRequest.Get(Constants.SHARE_URL + sceneId + "_scene.json");
+            UnityWebRequest getSceneRequest = UnityWebRequest.Get(Constants.SHARE_URL + Constants.SHARE_OBJECTSTORE_PATH + sceneId + "_scene.json");
             getSceneRequest.SetRequestHeader("Content-Type", "application/json");
             yield return getSceneRequest.SendWebRequest();
             if (getSceneRequest.isNetworkError)
@@ -141,8 +141,8 @@ namespace Amsterdam3D.Sharing
         private IEnumerator GetCustomMeshObject(GameObject gameObjectTarget, string sceneId, string token, SerializableScene.Vector3 position, SerializableScene.Quaternion rotation, SerializableScene.Vector3 scale)
         {
             
-            Debug.Log(Constants.SHARE_URL + token + ".dat");
-            UnityWebRequest getModelRequest = UnityWebRequest.Get(Constants.SHARE_URL + token + ".dat");
+            Debug.Log(Constants.SHARE_URL + Constants.SHARE_OBJECTSTORE_PATH + token + ".dat");
+            UnityWebRequest getModelRequest = UnityWebRequest.Get(Constants.SHARE_URL + Constants.SHARE_OBJECTSTORE_PATH + token + ".dat");
             getModelRequest.SetRequestHeader("Content-Type", "application/json");
             yield return getModelRequest.SendWebRequest();
             if (getModelRequest.isNetworkError)
