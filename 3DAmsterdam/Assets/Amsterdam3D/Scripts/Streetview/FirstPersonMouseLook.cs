1a31a4ba295fc5b048fd8a5065b1c3c1e978c9af
Lucas Schilperoort <398389@student.fontys.nl>
2020-09-10 15:00:54 +0200

    First person mode first draft

---
+++ 3DAmsterdam/Assets/Amsterdam3D/Scripts/Streetview/FirstPersonMouseLook.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+//TODO: Make Class related with CameraControls somehow? (Abstract base class that both god view and first person view inherit from, that functions that need the camera use)
+// Or we make a singleton CameraManager that scripts can get the current camera data from
+public class FirstPersonMouseLook : MonoBehaviour
+{
+
+   
+    
+    Vector2 rotation = new Vector2(0, 0);
+    public float speed = 3;
+
+
+
+    private void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        rotation.y += Input.GetAxis("Mouse X") * speed;
+        rotation.x += -Input.GetAxis("Mouse Y") * speed;
+        rotation.x = ClampAngle(rotation.x, -90, 90);
+        transform.eulerAngles = (Vector2)rotation;
+
+
+        if (Input.GetKeyDown(KeyCode.Escape)) 
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+
+    public float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360F)
+            angle += 360F;
+        if (angle > 360F)
+            angle -= 360F;
+        return Mathf.Clamp(angle, min, max);
+    }
+}
---
+++ 3DAmsterdam/Assets/Amsterdam3D/Scripts/Streetview/FirstPersonMovement.cs
@@ -0,0 +1,123 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstPersonMovement : MonoBehaviour
+{
+
+    // Use this for initialization
+
+
+
+    [SerializeField]
+    float groundOffset;
+
+
+    [SerializeField]
+    Vector3 velocity;
+
+    [SerializeField]
+    bool isgrounded;
+
+    [SerializeField]
+    float rayDistance = 0.1f;
+
+    [SerializeField]
+    BoxCollider col;
+
+    [SerializeField]
+    float gravity = -9.81f;
+
+    [SerializeField]
+    float moveSpeed = 1;
+
+    [SerializeField]
+    float runspeed = 3;
+    void Start()
+    {
+        col = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+        CheckPhysics();
+        CheckInput();
+
+     
+
+
+
+
+    }
+
+
+    private void CheckPhysics() 
+    {
+        if (!isgrounded)
+        {
+            velocity -= -Vector3.up * gravity * Time.deltaTime;
+
+
+
+        }
+
+        if (velocity.y <= 0)
+        {
+            Vector3 center = transform.TransformPoint(new Vector3(col.center.x, col.center.y, col.center.z));
+            Ray ray = new Ray(center, -Vector3.up);
+            rayDistance = 0.1f - (velocity.y * Time.deltaTime) + groundOffset;
+            RaycastHit hit;
+            Debug.DrawRay(ray.origin, ray.direction * rayDistance, Color.blue, 10);
+            if (Physics.Raycast(ray, out hit, rayDistance))
+            {
+                transform.position = new Vector3(transform.position.x, hit.point.y + groundOffset, transform.position.z);
+                velocity = new Vector3(velocity.x, 0, velocity.z);
+                isgrounded = true;
+            }
+
+            else
+            {
+                isgrounded = false;
+            }
+        }
+    }
+
+    private void CheckInput() 
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            velocity.x = Input.GetAxis("Horizontal") * runspeed;
+            velocity.z = Input.GetAxis("Vertical") * runspeed;
+        }
+
+        else 
+        {
+            
+            
+            velocity.x = Input.GetAxis("Horizontal") * moveSpeed;
+            velocity.z = Input.GetAxis("Vertical") * moveSpeed;
+        }
+
+        if (isgrounded)
+        {
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                velocity.y = 5;
+                isgrounded = false;
+            }
+        }
+
+        // rotate velocity 
+        velocity = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0) * velocity;
+
+    }
+    
+    private void LateUpdate()
+    {
+        transform.position += velocity * Time.deltaTime;
+    }
+}
