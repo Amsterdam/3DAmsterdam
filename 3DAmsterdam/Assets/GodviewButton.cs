e729af2b2fae6b62dbe534ae30a31a1886b08f52
Lucas Schilperoort <398389@student.fontys.nl>
2020-09-16 10:53:58 +0200

    added prefabs

---
+++ 3DAmsterdam/Assets/GodviewButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GodviewButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        CameraManager.instance.OnFirstPersonModeEvent += EnableObject;
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void EnableObject() 
+    {
+        gameObject.SetActive(true);
+    }
+}
