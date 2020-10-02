565bdc68b5a59c81da8819c58ce8680cdccb0447
Sam Baas <samuelpetrus@gmail.com>
2020-10-02 11:23:03 +0200

    added memory stat in settings window, to show how much memory is consumed inside the heap

---
+++ 3DAmsterdam/Assets/Amsterdam3D/Scripts/Interface/Memory.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Memory : MonoBehaviour
+{
+	private Text memoryOutput;
+
+	private void Awake()
+	{
+		memoryOutput = GetComponent<Text>();
+	}
+
+	IEnumerator Start()
+	{
+		while (true)
+		{
+			DrawMemoryUsageInHeap();
+			yield return new WaitForSeconds(0.5f);
+		}
+	}
+
+	private void DrawMemoryUsageInHeap(){
+		memoryOutput.text = ConvertBytesToMegabytes(System.GC.GetTotalMemory(false)).ToString("F2") + "MB";
+	}
+	
+    private double ConvertBytesToMegabytes(long bytes)
+    {
+        return (bytes / 1024f) / 1024f;
+    }
+}
