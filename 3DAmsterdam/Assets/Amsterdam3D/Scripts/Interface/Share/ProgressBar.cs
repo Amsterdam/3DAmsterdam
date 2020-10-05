1702e757567f0c2222d17d70effde169bdfff5c1
Sam Baas <samuelpetrus@gmail.com>
2020-10-05 13:28:32 +0200

    added progress bar to object loading screen

--- 3DAmsterdam/Assets/Amsterdam3D/Scripts/Interface/Share/ProgressBar.cs
+++ 3DAmsterdam/Assets/Amsterdam3D/Scripts/Interface/Share/ProgressBar.cs
@@ -17,6 +17,10 @@ namespace Amsterdam3D.Interface
             progressStepText.text = message;
         }
 
+        /// <summary>
+        /// Sets the amount the bar is filled
+        /// </summary>
+        /// <param name="normalisedPercentage">Value from 0.0 to 1.0</param>
         public void Percentage(float normalisedPercentage){
             progressBar.fillAmount = normalisedPercentage;
         }
