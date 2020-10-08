9f02c3724b749cd79a6a2a093126108d9ab8f8f8
Sam Baas <samuelpetrus@gmail.com>
2020-10-08 15:50:09 +0200

    increased memory is currently not needed anymore since we split up the OBJ parsing

--- 3DAmsterdam/Assets/Amsterdam3D/Scripts/Utilities/Editor/MemoryManagement.cs
+++ 3DAmsterdam/Assets/Amsterdam3D/Scripts/Utilities/Editor/MemoryManagement.cs
@@ -10,6 +10,7 @@ public class MemoryManagement
     {
         //Reserve a bit more memory at start. (Default is 32M)
         //Please note that this value can automatically multiply, up to the browser max of ~2Gig
-        PlayerSettings.WebGL.emscriptenArgs = "-s TOTAL_MEMORY=55MB";
+
+        //PlayerSettings.WebGL.emscriptenArgs = "-s TOTAL_MEMORY=55MB";
     }
 }
