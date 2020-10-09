530fa691a8fc9eb0fe92d89067b6c8c46f66d567
Sam Baas <samuelpetrus@gmail.com>
2020-10-09 10:08:51 +0200

    explained disabling parameters in memorymanagement

--- 3DAmsterdam/Assets/Amsterdam3D/Scripts/Utilities/Editor/MemoryManagement.cs
+++ 3DAmsterdam/Assets/Amsterdam3D/Scripts/Utilities/Editor/MemoryManagement.cs
@@ -11,6 +11,6 @@ public class MemoryManagement
         //Reserve a bit more memory at start. (Default is 32M)
         //Please note that this value can automatically multiply, up to the browser max of ~2Gig
 
-        //PlayerSettings.WebGL.emscriptenArgs = "-s TOTAL_MEMORY=55MB";
+        //PlayerSettings.WebGL.emscriptenArgs = "-s TOTAL_MEMORY=55MB"; //Arguments disabled for now. Default 32MB seems to work more stable
     }
 }
