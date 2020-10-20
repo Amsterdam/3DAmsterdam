46b7f3a36483e17510339b8e2306b08e5ad31cfb
Sam Baas <samuelpetrus@gmail.com>
2020-10-20 21:50:23 +0200

    seperate editor script for exporting meshes to assetbundles

---
+++ 3DAmsterdam/Assets/Amsterdam3D/Scripts/Editor/ExportTreeTiles.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class ExportTreeTiles
+{
+	[MenuItem("Tools/Genereer Bomen naar AssetBundles")]
+	// Start is called before the first frame update
+	public static void GenerateAssetBundles()
+	{
+		DirectoryInfo directory = new DirectoryInfo(Application.dataPath + "/TreeTiles");
+		var fileInfo = directory.GetFiles();
+		foreach (var file in fileInfo)
+		{
+			if (!file.Name.Contains(".meta"))
+			{
+				Debug.Log("Exporting tile: " + file.Name);
+				//Create asset bundle from mesh we just made
+				AssetBundleBuild[] buildMap = new AssetBundleBuild[1];
+				string[] assetNames = new string[1];
+				assetNames[0] = "Assets/TreeTiles/" + file.Name;
+
+				buildMap[0].assetBundleName = file.Name.Replace(".asset", "");
+				buildMap[0].assetNames = assetNames;
+
+				BuildPipeline.BuildAssetBundles("Assets/TreeTilesAssetBundles", buildMap, BuildAssetBundleOptions.None, BuildTarget.WebGL);
+			}
+		}
+	}
+}
