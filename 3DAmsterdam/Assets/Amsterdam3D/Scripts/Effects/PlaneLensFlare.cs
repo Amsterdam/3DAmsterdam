204cc4803c1a86bc8063eaef7ef5e41ef6e68ce5
Sam Baas <samuelpetrus@gmail.com>
2020-10-26 19:44:58 +0100

    renamed to non conflicting class name

--- 3DAmsterdam/Assets/Amsterdam3D/Scripts/Effects/PlaneLensFlare.cs
+++ 3DAmsterdam/Assets/Amsterdam3D/Scripts/Effects/PlaneLensFlare.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LensFlare : MonoBehaviour
+public class PlaneLensFlare : MonoBehaviour
 {
     [SerializeField]
     private Transform sun;
