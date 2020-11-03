f71417a76a1d6df7fa5e53b6e50ab836e694a1fe
Lucas Schilperoort <398389@student.fontys.nl>
2020-11-03 10:57:02 +0000

    added polygon Contains point Function

--- 3DAmsterdam/Assets/Amsterdam3D/Scripts/SelectionTools/SelectionTool.cs
+++ 3DAmsterdam/Assets/Amsterdam3D/Scripts/SelectionTools/SelectionTool.cs
@@ -12,11 +12,25 @@ namespace Amsterdam3D.SelectionTools
         public GameObject Canvas;
         public UnityEvent onSelectionCompleted;
         public ToolType toolType { get; protected set; }
-        public Bounds bounds = new Bounds();
         public List<Vector3> vertexes = new List<Vector3>();
 
         // Use this for initialization
         public abstract void EnableTool();
         public abstract void DisableTool();
+        // source: https://wiki.unity3d.com/index.php/PolyContainsPoint
+        public bool ContainsPoint(Vector3 p)
+        {
+            var j = vertexes.Count - 1;
+            var inside = false;
+            for (int i = 0; i < vertexes.Count; j = i++)
+            {
+                var pi = vertexes[i];
+                var pj = vertexes[j];
+                if (((pi.z <= p.z && p.z < pj.z) || (pj.z <= p.z && p.z < pi.z)) &&
+                    (p.x < (pj.x - pi.x) * (p.z - pi.z) / (pj.z - pi.z) + pi.x))
+                    inside = !inside;
+            }
+            return inside;
+        }
     }
 }
