a67dc36749fe3e57a04b74e03bde11c83852c475
Sam Baas <samuelpetrus@gmail.com>
2020-11-05 17:35:39 +0100

    starting all texture downloads in nested folders simultaniously to speed up the downloading progress

--- 3DAmsterdam/Assets/DownloadAllTerrainTextures.cs
+++ 3DAmsterdam/Assets/DownloadAllTerrainTextures.cs
@@ -15,11 +15,15 @@ using UnityEngine.Rendering;
 
 public class DownloadAllTerrainTextures : MonoBehaviour
 {
+    [SerializeField]
     private string url = "https://saturnus.geodan.nl/mapproxy/bgt/service?crs=EPSG%3A3857&service=WMS&version=1.1.1&request=GetMap&styles=&format=image%2Fjpeg&layers=bgt&bbox={xMin}%2C{yMin}%2C{xMax}%2C{yMax}&width=256&height=256&srs=EPSG%3A4326";
+    [SerializeField]
     private string localTileFilesFolder = "C:\\Users\\Sam\\Desktop\\terrain";
-	private Terrain.TmsGlobalGeodeticTileSchema schema;
 
-	void Start()
+	private Terrain.TmsGlobalGeodeticTileSchema schema;
+	private int filesDownloaded = 0;
+    private int fileDownloadsStarted = 0;
+    void Start()
     {
         schema = new Terrain.TmsGlobalGeodeticTileSchema();
         StartCoroutine(DownloadAll());
@@ -34,48 +38,66 @@ public class DownloadAllTerrainTextures : MonoBehaviour
         string[] zDirs = Directory.GetDirectories(localTileFilesFolder);
         foreach(var zDir in zDirs)
         {
+            if (!(Path.GetFileNameWithoutExtension(zDir) == "18")) continue;
+
             string[] xDirs = Directory.GetDirectories(zDir);
             foreach (var xDir in xDirs)
             {
+                
                 string[] yFiles = Directory.GetFiles(xDir);
                 foreach (var yFile in yFiles)
                 {
-                    var tileId = new Vector3(
+                    Debug.Log("Started downloading a texture");
+                    StartCoroutine(SeperateFileProgress(xDir,yFile,zDir));
+                }
+
+                while(filesDownloaded < fileDownloadsStarted)
+                {
+                    Debug.Log("Downloaded " + filesDownloaded + "/" + fileDownloadsStarted);
+                    yield return new WaitForEndOfFrame();
+                }
+
+                Debug.Log("Textures downloaded: " + filesDownloaded);
+                yield return new WaitForEndOfFrame();
+            }
+        }   
+        
+    }
+
+	private IEnumerator SeperateFileProgress(string xDir, string yFile, string zDir)
+	{
+        var tileId = new Vector3(
                        int.Parse(Path.GetFileNameWithoutExtension(xDir)),
                        int.Parse(Path.GetFileNameWithoutExtension(yFile)),
                        int.Parse(Path.GetFileNameWithoutExtension(zDir))
                      );
-                     Debug.Log(tileId);
 
-                    var tileRange = new TileRange(UnityEngine.Mathf.RoundToInt(tileId.x), UnityEngine.Mathf.RoundToInt(tileId.y));
-                    Extent subtileExtent = TileTransform.TileToWorld(tileRange, tileId.z.ToString(), schema);
-                    string wmsUrl = url.Replace("{xMin}", subtileExtent.MinX.ToString()).Replace("{yMin}", subtileExtent.MinY.ToString()).Replace("{xMax}", subtileExtent.MaxX.ToString()).Replace("{yMax}", subtileExtent.MaxY.ToString()).Replace(",", ".");
-                    if (tileId.z == 17)
-                    {
-                        wmsUrl = wmsUrl.Replace("width=256", "width=1024").Replace("height=256", "height=1024");
-                    }
-                    UnityWebRequest www = UnityWebRequest.Get(wmsUrl);
-                    yield return www.SendWebRequest();
-
-                    if (!www.isNetworkError && !www.isHttpError)
-                    {
-                        var loadedTextureData = www.downloadHandler.data;
-                        var savedFile = yFile.Replace(".terrain",".jpg");
-                        Debug.Log("Downloaded:");
-                        Debug.Log(savedFile);
-                        File.WriteAllBytes(savedFile, loadedTextureData);
-                    }
-                    else
-                    {
-                        Debug.LogWarning("Tile: [" + tileId.x + " " + tileId.y + "] Error loading texture data");
-                    }
-                    yield return new WaitForEndOfFrame();
-                }
-            }
+        fileDownloadsStarted++;
+        var tileRange = new TileRange(UnityEngine.Mathf.RoundToInt(tileId.x), UnityEngine.Mathf.RoundToInt(tileId.y));
+        Extent subtileExtent = TileTransform.TileToWorld(tileRange, tileId.z.ToString(), schema);
+        string wmsUrl = url.Replace("{xMin}", subtileExtent.MinX.ToString()).Replace("{yMin}", subtileExtent.MinY.ToString()).Replace("{xMax}", subtileExtent.MaxX.ToString()).Replace("{yMax}", subtileExtent.MaxY.ToString()).Replace(",", ".");
+        if (tileId.z == 17)
+        {
+            wmsUrl = wmsUrl.Replace("width=256", "width=1024").Replace("height=256", "height=1024");
         }
+        UnityWebRequest www = UnityWebRequest.Get(wmsUrl);
+        yield return www.SendWebRequest();
 
-    
-        
+        filesDownloaded++;
+
+        if (!www.isNetworkError && !www.isHttpError)
+        {
+            var loadedTextureData = www.downloadHandler.data;
+            var savedFile = yFile.Replace(".terrain", ".jpg");
+            Debug.Log("Downloaded:");
+            Debug.Log(savedFile);
+            File.WriteAllBytes(savedFile, loadedTextureData);
+        }
+        else
+        {
+            Debug.LogWarning("Tile: [" + tileId.x + " " + tileId.y + "] Error loading texture data");
+        }
+        yield return new WaitForEndOfFrame();
     }
 }
 
