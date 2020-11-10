6e5455517beeaa2e277f69fac173fb3fa98ae1dc
Sam Baas <samuelpetrus@gmail.com>
2020-11-10 11:32:20 +0100

    added context menu with script that toggles on right click

---
+++ 3DAmsterdam/Assets/Amsterdam3D/Scripts/Interface/ContextPointerMenu/ContextPointerMenu.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContextPointerMenu : MonoBehaviour
+{
+    [SerializeField]
+    private RectTransform contextItemsPanel;    
+
+    void Start()
+    {
+        contextItemsPanel.gameObject.SetActive(false);
+    }
+    
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(1)){
+            RetrieveOptions();  
+		}
+    }
+
+	private void RetrieveOptions()
+	{
+        contextItemsPanel.gameObject.SetActive(false);
+        contextItemsPanel.gameObject.SetActive(true);
+
+        contextItemsPanel.transform.position = Input.mousePosition;
+
+    }
+}
