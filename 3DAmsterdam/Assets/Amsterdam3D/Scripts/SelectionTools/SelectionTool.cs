4e6cf88d8e4528f24fd50a4f39d5842563cfa860
Sam Baas <samuelpetrus@gmail.com>
2020-11-10 15:00:01 +0100

    renamed vertexes to vertices and removed some whitespaces

--- 3DAmsterdam/Assets/Amsterdam3D/Scripts/SelectionTools/SelectionTool.cs
+++ 3DAmsterdam/Assets/Amsterdam3D/Scripts/SelectionTools/SelectionTool.cs
@@ -7,13 +7,11 @@ namespace Amsterdam3D.SelectionTools
 {
     public abstract class SelectionTool : MonoBehaviour
     {
-
-
         public GameObject Canvas;
         public UnityEvent onSelectionCompleted;
         public UnityEvent OnDeselect;
         public ToolType toolType { get; protected set; }
-        public List<Vector3> vertexes = new List<Vector3>();
+        public List<Vector3> vertices = new List<Vector3>();
 
         // Use this for initialization
         public abstract void EnableTool();
@@ -21,12 +19,12 @@ namespace Amsterdam3D.SelectionTools
         // source: https://wiki.unity3d.com/index.php/PolyContainsPoint
         public bool ContainsPoint(Vector3 p)
         {
-            var j = vertexes.Count - 1;
+            var j = vertices.Count - 1;
             var inside = false;
-            for (int i = 0; i < vertexes.Count; j = i++)
+            for (int i = 0; i < vertices.Count; j = i++)
             {
-                var pi = vertexes[i];
-                var pj = vertexes[j];
+                var pi = vertices[i];
+                var pj = vertices[j];
                 if (((pi.z <= p.z && p.z < pj.z) || (pj.z <= p.z && p.z < pi.z)) &&
                     (p.x < (pj.x - pi.x) * (p.z - pi.z) / (pj.z - pi.z) + pi.x))
                     inside = !inside;
