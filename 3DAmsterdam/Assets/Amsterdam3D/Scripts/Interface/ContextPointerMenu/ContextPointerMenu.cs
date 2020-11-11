628ea251a6ea4f58cdbea1d6718e856257e382f6
Sam Baas <samuelpetrus@gmail.com>
2020-11-11 14:54:54 +0100

    always close the context menu if we chose one of the options

--- 3DAmsterdam/Assets/Amsterdam3D/Scripts/Interface/ContextPointerMenu/ContextPointerMenu.cs
+++ 3DAmsterdam/Assets/Amsterdam3D/Scripts/Interface/ContextPointerMenu/ContextPointerMenu.cs
@@ -2,32 +2,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class ContextPointerMenu : MonoBehaviour
 {
     [SerializeField]
-    private RectTransform contextItemsPanel;    
+    private RectTransform contextItemsPanel;
 
     void Start()
     {
         contextItemsPanel.gameObject.SetActive(false);
+
+        Button[] buttons = contextItemsPanel.GetComponentsInChildren<Button>();
+        foreach(Button button in buttons)
+            button.onClick.AddListener(CloseContextMenu);
     }
-    
-    void Update()
+
+    void CloseContextMenu()
     {
+        contextItemsPanel.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {   
+        //TODO: replace with centralized input system
         if(Input.GetMouseButtonUp(1) && !EventSystem.current.IsPointerOverGameObject())
         {
-            RetrieveOptions();  
+            ShowOptions();  
 		}
     }
 
-	private void RetrieveOptions()
+	private void ShowOptions()
 	{
-        contextItemsPanel.gameObject.SetActive(false);
-        contextItemsPanel.gameObject.SetActive(true);
-
         contextItemsPanel.transform.position = Input.mousePosition;
 
+        contextItemsPanel.gameObject.SetActive(false);
+        contextItemsPanel.gameObject.SetActive(true);
+        
+        //Disable/enable right buttons
+        //Check if an object is was selected
     }
 }
