85b4b19439a7ee01c50e868ed9e349a5485cb981
Sam Baas <samuelpetrus@gmail.com>
2020-11-17 15:42:56 +0100

    changed boxcast to cast from higher up and use centroid. seems to solve the problem.

--- 3DAmsterdam/Assets/Amsterdam3D/Scripts/SelectionTools/SelectionTools.cs
+++ 3DAmsterdam/Assets/Amsterdam3D/Scripts/SelectionTools/SelectionTools.cs
@@ -66,18 +66,17 @@ namespace Amsterdam3D.Interface
 
 		private void OnSelectionFunction()
 		{
-			//Hard coded for now, should be calculated later based on what type of selection tool etc?
-			var min = selectionTool.vertices[0];
-			var max = selectionTool.vertices[2];
-			Vector3 center = (min + max) / 2;
-			Vector3 extends = max - min;
+			vertices = GetVertices();
 
+			float extends = Vector3.Distance(selectionTool.vertices[0], selectionTool.vertices[1]) *0.5f;
 			layer.AddMeshColliders();
-			StartCoroutine(BoxCastToFindTilesInRange(center, extends));
+			StartCoroutine(BoxCastToFindTilesInRange(GetPointsCentroid(), new Vector3(extends, extends, extends)));
 		}
 
 		private Vector3 GetPointsCentroid()
 		{
+			if (vertices.Count <= 0) return default;
+
 			Vector3 centroid = new Vector3(0, 0, 0);
 			foreach (var point in vertices)
 			{
@@ -93,10 +92,10 @@ namespace Amsterdam3D.Interface
 		{
 			//We wait one frame to make sure the colliders are there.
 			yield return new WaitForEndOfFrame();
-
-			var hits = Physics.BoxCastAll(center + groundLevel, extends, -Vector3.up, Quaternion.Euler(Vector3.zero), (center.y + Constants.ZERO_GROUND_LEVEL_Y), buildingLayer);
+			var hits = Physics.BoxCastAll(center, extends, Vector3.down, Quaternion.identity, center.y + 1000.0f, buildingLayer);
 			foreach (var hit in hits)
 			{
+				Debug.Log("HIT " + hit.collider.name);
 				tileHandler.GetIDData(hit.collider.gameObject, hit.triangleIndex * 3);
 			}
 		}
