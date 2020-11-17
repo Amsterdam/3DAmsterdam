d029a7afb81a697529cb4252886387c9fc5f6982
Sam Baas <samuelpetrus@gmail.com>
2020-11-17 17:57:45 +0100

    added list with states and activated buttons, that can be triggered from singleton

--- 3DAmsterdam/Assets/Amsterdam3D/Scripts/Interface/ContextPointerMenu/ContextPointerMenu.cs
+++ 3DAmsterdam/Assets/Amsterdam3D/Scripts/Interface/ContextPointerMenu/ContextPointerMenu.cs
@@ -8,40 +8,83 @@ using UnityEngine.UI;
 
 public class ContextPointerMenu : MonoBehaviour
 {
-    [SerializeField]
-    private RectTransform contextItemsPanel;
+	[SerializeField]
+	private RectTransform contextItemsPanel;
 
-    void Start()
-    {
-        contextItemsPanel.gameObject.SetActive(false);
+	public static ContextPointerMenu Instance = null;
 
-        Button[] buttons = contextItemsPanel.GetComponentsInChildren<Button>();
-        foreach(Button button in buttons)
-            button.onClick.AddListener(CloseContextMenu);
-    }
+	public ContextState state = ContextState.DEFAULT;
 
-    void CloseContextMenu()
-    {
-        contextItemsPanel.gameObject.SetActive(false);
-    }
+	[SerializeField]
+	[Tooltip("Select buttons that should be active for specific states")]
+	private StateButtons[] buttonsAvailableOnState;
 
-    void Update()
-    {   
-        //TODO: replace with centralized input system
-        if(Input.GetMouseButtonUp(1) && !EventSystem.current.IsPointerOverGameObject())
-        {
-            ShowOptions();  
+	[Serializable]
+	public class StateButtons
+	{
+		public ContextState state;
+		public Button[] activeButtons;
+	}
+
+	public enum ContextState
+	{
+		DEFAULT,
+		CUSTOM_OBJECTS,
+		SELECTABLE_STATICS
+	}
+
+	private void Start()
+	{
+		if (Instance == null)
+		{
+			Instance = this;
 		}
-    }
+		contextItemsPanel.gameObject.SetActive(false);
+
+		Button[] buttons = contextItemsPanel.GetComponentsInChildren<Button>();
+		foreach (Button button in buttons)
+			button.onClick.AddListener(CloseContextMenu);
+	}
+
+	void CloseContextMenu()
+	{
+		contextItemsPanel.gameObject.SetActive(false);
+		state = ContextState.DEFAULT;
+	}
+
+	public void SwitchState(ContextState newState)
+	{
+		state = newState;
+
+		var buttons = GetComponentsInChildren<Button>();
+		foreach (Button button in buttons)
+			button.interactable = false;
+
+		foreach (StateButtons stateButtons in buttonsAvailableOnState)
+		{
+			if (state == stateButtons.state)
+			{
+				foreach (Button button in stateButtons.activeButtons)
+					button.interactable = true;
+				return;
+			}
+		}
+	}
+
+	void Update()
+	{
+		//TODO: replace with centralized input system
+		if (Input.GetMouseButtonUp(1) && !EventSystem.current.IsPointerOverGameObject())
+		{
+			ShowOptions();
+		}
+	}
 
 	private void ShowOptions()
 	{
-        contextItemsPanel.transform.position = Input.mousePosition;
+		contextItemsPanel.transform.position = Input.mousePosition;
 
-        contextItemsPanel.gameObject.SetActive(false);
-        contextItemsPanel.gameObject.SetActive(true);
-        
-        //Disable/enable right buttons
-        //Check if an object is was selected
-    }
+		contextItemsPanel.gameObject.SetActive(false);
+		contextItemsPanel.gameObject.SetActive(true);
+	}
 }
