bf72b869fa56d1a5fe3f3e2ebebf708180236f09
Sam Baas <samuelpetrus@gmail.com>
2020-11-19 13:55:38 +0100

    added comments to the context menu methods

--- 3DAmsterdam/Assets/Amsterdam3D/Scripts/Interface/ContextPointerMenu/ContextPointerMenu.cs
+++ 3DAmsterdam/Assets/Amsterdam3D/Scripts/Interface/ContextPointerMenu/ContextPointerMenu.cs
@@ -9,7 +9,7 @@ using UnityEngine.UI;
 public class ContextPointerMenu : MonoBehaviour
 {
 	[SerializeField]
-	private RectTransform contextItemsPanel;
+	private RectTransform contextItemsPanel = default;
 
 	public static ContextPointerMenu Instance = null;
 
@@ -46,20 +46,29 @@ public class ContextPointerMenu : MonoBehaviour
 			button.onClick.AddListener(CloseContextMenu);
 	}
 
+	/// <summary>
+	/// Disables our right mouse context menu, and resets its state do default
+	/// </summary>
 	void CloseContextMenu()
 	{
 		contextItemsPanel.gameObject.SetActive(false);
 		state = ContextState.DEFAULT;
 	}
 
+	/// <summary>
+	/// Switches the right click menu state, enabling/disabling the right buttons matching that state
+	/// </summary>
+	/// <param name="newState">The new state detemining what buttons are active/disabled</param>
 	public void SwitchState(ContextState newState)
 	{
 		state = newState;
 
+		//Start by disabling all buttons but disablig their interactibility
 		var buttons = GetComponentsInChildren<Button>();
 		foreach (Button button in buttons)
 			button.interactable = false;
 
+		//Now only active the buttons that should be active in this new state
 		foreach (StateButtons stateButtons in buttonsAvailableOnState)
 		{
 			if (state == stateButtons.state)
@@ -73,17 +82,21 @@ public class ContextPointerMenu : MonoBehaviour
 
 	void Update()
 	{
-		//TODO: replace with centralized input system
+		//TODO: replace with centralized input system to avoid conflicts with other scripts polling for the same buttons
 		if (Input.GetMouseButtonUp(1) && !EventSystem.current.IsPointerOverGameObject())
 		{
-			ShowOptions();
+			Appear();
 		}
 	}
 
-	private void ShowOptions()
+	/// <summary>
+	/// Moves our context menu to our pointer, and actives it with an animation. 
+	/// </summary>
+	private void Appear()
 	{
 		contextItemsPanel.transform.position = Input.mousePosition;
 
+		//Always disable the panel first, so our appear animation plays again
 		contextItemsPanel.gameObject.SetActive(false);
 		contextItemsPanel.gameObject.SetActive(true);
 	}
