12fc7080ddc7e0940a9799908bc0a6242180bbed
Sam Baas <samuelpetrus@gmail.com>
2020-11-27 13:12:35 +0100

    added world space sphere masking to ground shader

---
+++ 3DAmsterdam/Assets/Amsterdam3D/Scripts/Sewer/RuntimeMaskSphere.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RuntimeMaskSphere : MonoBehaviour
+{
+    [SerializeField]
+    private Material groundMaterial;
+
+    void Update()
+    {
+        groundMaterial.SetVector("_ClippingMaskDome", new Vector4(this.transform.position.x, this.transform.position.y, this.transform.position.z, this.transform.localScale.x-1));
+    }
+}
