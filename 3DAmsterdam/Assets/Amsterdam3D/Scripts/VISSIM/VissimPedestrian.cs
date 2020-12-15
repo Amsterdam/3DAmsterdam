fc3abf455ce8efa4d5e99dab3ab72ceeb753eebb
Niels P <niels.poelder@gmail.com>
2020-12-15 09:49:10 +0100

    More comments added

--- 3DAmsterdam/Assets/Amsterdam3D/Scripts/VISSIM/VissimPedestrian.cs
+++ 3DAmsterdam/Assets/Amsterdam3D/Scripts/VISSIM/VissimPedestrian.cs
@@ -11,7 +11,7 @@ public class VissimPedestrian : VissimCar
         pedestrianAnimator = transform.GetChild(0).GetComponent<Animator>();
     }
 
-
+    // adds a walking animation to the pedestrian characters and only gets activated when its time for the pedestrian to move
     public override void MoveAnimation(Vector3 startPos, Vector3 endPos, float animationTime)
     {
         base.MoveAnimation(startPos, endPos, animationTime);
