31042245d4bdce8bfeb73bcaf57b750b9a8f1804
mvangog <martijn.van.gog@amsterdam.nl>
2020-12-21 11:52:40 +0100

    restored fireworks-prefabs

---
+++ 3DAmsterdam/Assets/Special/Fireworks/FireworksSound.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireworksSound : MonoBehaviour
+{
+    private ParticleSystem particleSystem;
+    private AudioClip audioClip;
+    private bool IsBorn = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        particleSystem = GetComponentInParent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsBorn)
+        {
+            return;
+        }
+        if (particleSystem.particleCount>0)
+        {
+            IsBorn = true;
+            GetComponentInParent<AudioSource>().Play();
+        }
+    }
+}
