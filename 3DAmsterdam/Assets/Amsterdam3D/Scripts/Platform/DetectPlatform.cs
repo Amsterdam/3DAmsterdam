a0704a83836a4376eef1719c0713edfbf49a8aad
Sam Baas <samuelpetrus@gmail.com>
2020-12-29 12:32:05 +0100

    added platform detection to enable/disable mobile override, with lower resolution textures, and lower rendering dpi

---
+++ 3DAmsterdam/Assets/Amsterdam3D/Scripts/Platform/DetectPlatform.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+using Amsterdam3D.Rendering;
+
+namespace Amsterdam3D.Performance
+{
+    public class DetectPlatform : MonoBehaviour
+    {
+        [DllImport("__Internal")]
+        private static extern bool IsMobile();
+
+        [SerializeField]
+        private int mobileQualitySettingSlot = 0;
+
+        [SerializeField]
+        PostProcessingSettings postProcessingSettings;
+
+		private void Start()
+		{
+            #if !UNITY_EDITOR
+                if(IsMobile())
+                {
+                    MobileOverrides();
+			    }
+            #endif
+        }
+
+
+
+        [ContextMenu("Set mobile quality settings")]
+        private void MobileOverrides()
+        {
+            QualitySettings.SetQualityLevel(mobileQualitySettingSlot, true);
+            postProcessingSettings.SetRenderScale(0.5f);
+        }
+    }
+}
