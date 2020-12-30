e59b923388600b0a90a014cd88360cc4615aed44
Sam Baas <samuelpetrus@gmail.com>
2020-12-30 15:03:57 +0100

    remove text on walking input

---
+++ 3DAmsterdam/Assets/Special/Scripts/RemoveTextOnInput.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveTextOnInput : MonoBehaviour
+{
+    [SerializeField]
+    KeyCode[] keysThatHideText;
+
+    void Update()
+    {
+        foreach(KeyCode key in keysThatHideText)
+        {
+            if(Input.GetKeyDown(key))
+            {
+                this.gameObject.SetActive(false);
+			}
+		}
+    }
+}
