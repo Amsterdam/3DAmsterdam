a03740b56bdb35274a3577a4f5642bdb2d09371f
Sam Baas <samuelpetrus@gmail.com>
2021-01-12 16:50:35 +0100

    added summaries and use sharedmesh to avoid leaking into memory

--- 3DAmsterdam/Assets/Amsterdam3D/Scripts/ObjectManipulation/OutlineObject.cs
+++ 3DAmsterdam/Assets/Amsterdam3D/Scripts/ObjectManipulation/OutlineObject.cs
@@ -9,13 +9,18 @@ public class OutlineObject : MonoBehaviour
 
 	private void Awake()
 	{
+		//Make sure our position mathes that of our parent transform
 		this.transform.localRotation = Quaternion.identity;
 		this.transform.localPosition = Vector3.zero;
 
 		//Just use my parent's mesh at initialization
-		SetMesh(transform.parent.GetComponent<MeshFilter>().mesh);
+		SetMesh(transform.parent.GetComponent<MeshFilter>().sharedMesh);
 	}
 
+	/// <summary>
+	/// Swap the mesh used by our outline
+	/// </summary>
+	/// <param name="mesh">A target shared mesh</param>
 	public void SetMesh(Mesh mesh)
 	{
 		meshFilter.sharedMesh = mesh;
