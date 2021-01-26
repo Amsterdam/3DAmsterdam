f9bd434f0c8fe2a436cd0d8abddb2c04298a73cb
Sam Baas <samuelpetrus@gmail.com>
2021-01-26 11:16:45 +0100

    sun applies intensity to sunlight

--- 3DAmsterdam/Assets/Amsterdam3D/Scripts/Sun/SunVisuals.cs
+++ 3DAmsterdam/Assets/Amsterdam3D/Scripts/Sun/SunVisuals.cs
@@ -19,6 +19,9 @@ public class SunVisuals : MonoBehaviour
     [SerializeField]
     private float crossFadeColorRange = 20.0f;
 
+    [SerializeField]
+    private Material intensityMaterialTrees;
+
     private void Awake()
     {
         sunDirectionalLight = GetComponent<Light>();
@@ -32,6 +35,9 @@ public class SunVisuals : MonoBehaviour
 
         //Reduce sun strength when we go down the horizon
         sunDirectionalLight.intensity = Mathf.InverseLerp(-crossFadeColorRange, 0.1f, newAngles.x);
+
+        //Apply sunlight to tree darkness (who use a very simple unlit shader)
+        intensityMaterialTrees.SetFloat("_Light", Mathf.Max(sunDirectionalLight.intensity,0.3f));
 
         //Change the fog and ambient color based on this intensity
         RenderSettings.fogColor = Color.Lerp(fogColorNight, fogColorDay, sunDirectionalLight.intensity);
