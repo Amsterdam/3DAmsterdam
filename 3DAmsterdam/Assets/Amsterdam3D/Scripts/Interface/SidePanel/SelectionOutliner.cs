8b04b0d13bbd3e4f3ce258497a3aed72c350dcf4
Sam Baas <samuelpetrus@gmail.com>
2021-01-29 12:18:07 +0100

    new SelectionOutliner class to make selections visible in a list in the sidepanel

---
+++ 3DAmsterdam/Assets/Amsterdam3D/Scripts/Interface/SidePanel/SelectionOutliner.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Amsterdam3D.Interface {
+	public class SelectionOutliner : MonoBehaviour
+	{
+		private string id = "";
+		private string title = "";
+
+		[SerializeField]
+		private Text titleText;
+
+		private GameObject linkedGameObject;
+
+		public GameObject LinkedGameObject { get => linkedGameObject; set => linkedGameObject = value; }
+		public string Id { get => id; set => id = value; }
+		public string Title {
+			get => title;
+			set
+			{
+				title = value;
+				titleText.text = title;
+			}
+		}
+
+		private void Update()
+		{
+			if (!LinkedGameObject) Close();
+		}
+
+		private void Close()
+		{
+			var selectable = linkedGameObject.GetComponent<Interactable>();
+			if (selectable)
+			{
+				selectable.Deselect();
+			}
+
+			var selectByID = linkedGameObject.GetComponent<SelectByID>();
+			if (selectByID)
+			{
+				selectByID.DeselectSpecificID(id);
+				return;
+			}
+
+
+			Destroy(gameObject);
+		}
+	}
+}
