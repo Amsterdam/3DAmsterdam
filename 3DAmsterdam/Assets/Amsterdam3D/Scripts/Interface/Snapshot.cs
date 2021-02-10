4578d50a8f94cadb8e07c19676dfa1ba508b3fe4
AtanasValkov <a_elianator@abv.bg>
2021-02-10 19:37:36 +0100

    Functionality equivalent to the PrintScreen button (capturing only the application window) and saving the file as a .png in the project files

--- 3DAmsterdam/Assets/Amsterdam3D/Scripts/Interface/Snapshot.cs
+++ 3DAmsterdam/Assets/Amsterdam3D/Scripts/Interface/Snapshot.cs
@@ -1,11 +1,12 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Snapshot : MonoBehaviour
 {
-    /*
+
     [SerializeField]
     private int width = 1920;
     [SerializeField]
@@ -20,19 +21,63 @@ public class Snapshot : MonoBehaviour
     [SerializeField]
     private RenderTexture screenshotRenderTexture;
 
-    void OnEnable()
+    private bool takeScreenshotOnNextFrame;
+    private IEnumerator screenshotCoroutine;
+    private Texture2D screenShot;
+
+
+
+    private void Awake()
     {
-        Texture2D screenShot = new Texture2D(width, height, TextureFormat.RGB24, false);
-        snapshotCamera.Render();
-        RenderTexture.active = screenshotRenderTexture;
-        screenshotRenderTexture.tex.ReadPixels(new Rect(0, 0, width, height), 0, 0);
-        snapshotCamera.targetTexture = null;
-        RenderTexture.active = null; // JC: added to avoid errors
-        Destroy(screenshotRenderTexture);
-        byte[] bytes = screenShot.EncodeToPNG();
-        string filename = "Screenshot";
-        System.IO.File.WriteAllBytes(filename, bytes);
-        
+        snapshotCamera = Camera.main.GetComponent<Camera>();
+        screenshotCoroutine = Screenshotting();
     }
-    */
+
+    private IEnumerator Screenshotting()
+    {
+        while(true)
+        {
+            // Helps with making sure the camera is ready to render
+            yield return new WaitForEndOfFrame();
+            if (takeScreenshotOnNextFrame)
+            {
+                takeScreenshotOnNextFrame = false;
+
+                if (screenshotRenderTexture == null)
+                {
+                    // Creates off-screen render texture that can be rendered into
+                    screenshotRenderTexture = new RenderTexture(width, height, 24);
+                    screenShot = new Texture2D(width, height, TextureFormat.RGB24, false);
+                }
+
+                snapshotCamera.targetTexture = screenshotRenderTexture;
+                // Calls events on the camera related to rendering
+                snapshotCamera.Render();
+     
+                RenderTexture.active = screenshotRenderTexture;
+                screenShot.ReadPixels(new Rect(0, 0, width, height),0, 0);
+
+                // Resets variables
+                snapshotCamera.targetTexture = null;
+                RenderTexture.active = null;
+
+                byte[] bytes = screenShot.EncodeToPNG();
+                string filename = "Screenshot" + DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss" )+ ".png";
+                System.IO.File.WriteAllBytes(filename, bytes);
+
+                // Exits out of loop
+                StopCoroutine(screenshotCoroutine);
+            }
+        }
+
+    }
+    /// <summary>
+    /// Saves the immediate view as a .png in the 3DAmsterdam folder
+    /// </summary>
+    public void TakeScreenshot()
+    {
+        StopCoroutine(screenshotCoroutine);
+        takeScreenshotOnNextFrame = true;
+        StartCoroutine(screenshotCoroutine);
+    }
 }
