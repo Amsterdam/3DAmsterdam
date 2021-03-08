aa1438936ccc95311814d99f1d249eed73e5e7e8
Sam Baas <samuelpetrus@gmail.com>
2021-03-08 11:13:32 +0100

    added GridSelection prefab with resizable grid

---
+++ 3DAmsterdam/Assets/Amsterdam3D/Scripts/SelectionTools/GridSelection/GridSelection.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridSelection : MonoBehaviour
+{
+	[SerializeField]
+	private Material gridMaterial;
+
+	[SerializeField]
+	private float gridSize = 100.0f; //Meter
+
+	[SerializeField]
+	private float gridPlaneSize = 10000.0f;
+
+    void Awake()
+    {
+		this.transform.position = new Vector3(0, Constants.ZERO_GROUND_LEVEL_Y, 0);
+		SetGridSize();
+    }
+
+	private void SetGridSize()
+	{
+		gridMaterial.SetTextureScale("_MainTex", Vector2.one * (gridPlaneSize / gridSize));
+	}
+
+	 
+	public void OnValidate()
+	{
+		SetGridSize();
+	}
+
+}
