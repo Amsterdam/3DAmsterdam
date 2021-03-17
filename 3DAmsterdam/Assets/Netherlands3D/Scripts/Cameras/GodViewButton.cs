bf9113a84c719a3166728f0ea995f35faf0f2dc0
Sam Baas <samuelpetrus@gmail.com>
2021-03-17 16:33:20 +0100

    changed a lot of namespaces and moved scripts into corresponding folders

---
+++ 3DAmsterdam/Assets/Netherlands3D/Scripts/Cameras/GodViewButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Netherlands3D.Cameras;
+
+namespace Netherlands3D.Cameras
+{
+    public class GodViewButton : MonoBehaviour
+    {
+        void Start()
+        {
+            CameraModeChanger.Instance.OnFirstPersonModeEvent += EnableObject;
+            CameraModeChanger.Instance.OnGodViewModeEvent += DisableObject;
+            gameObject.SetActive(false);
+        }
+        public void EnableObject()
+        {
+            gameObject.SetActive(true);
+        }
+
+        public void DisableObject()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
