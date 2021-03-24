e597b3e511bb40a4f515543675097df4f524dfd2
mvangog <martijn.van.gog@amsterdam.nl>
2021-03-24 16:30:52 +0100

    first test to see if it works runtime

--- 3DAmsterdam/Assets/Netherlands3D/Plugins/netDxf/DxfFile.cs
+++ 3DAmsterdam/Assets/Netherlands3D/Plugins/netDxf/DxfFile.cs
@@ -13,7 +13,7 @@ public class DxfFile
 {
     private DxfDocument doc;
 
-    private void SetupDXF()
+    public void SetupDXF()
     {
         doc = new DxfDocument();
         doc.DrawingVariables.InsUnits = netDxf.Units.DrawingUnits.Meters;
@@ -24,18 +24,29 @@ public class DxfFile
         // create Mesh
         PolyfaceMeshVertex[] pfmVertices = new PolyfaceMeshVertex[triangleVertices.Count];
         PolyfaceMeshFace[] pfmFaces = new PolyfaceMeshFace[triangleVertices.Count/3];
+
         for (int i = 0; i < triangleVertices.Count; i+=3)
         {
+            
             pfmVertices[i] = new PolyfaceMeshVertex(triangleVertices[i].x,triangleVertices[i].y,triangleVertices[i].z);
-            pfmFaces[i / 3] = new PolyfaceMeshFace(new List<short>() { (short)i, (short)(i + 1), (short)(i + 2) });
-
+            PolyfaceMeshFace pfmFace = new PolyfaceMeshFace(new List<short>() { (short)i, (short)(i + 1), (short)(i + 2) });
+            
+            pfmFaces[i / 3] = pfmFace;
         }
         PolyfaceMesh pfm = new PolyfaceMesh(pfmVertices, pfmFaces);
+
 
         //create Layer"
         Layer Laag = new Layer(layerName);
         pfm.Layer = Laag;
+        Laag.Color = netDxf.AciColor.LightGray;
+        
         doc.AddEntity(pfm);
+        
+        
+    }
+    public void Save()
+    {
         MemoryStream stream = new MemoryStream();
         if (doc.Save(stream))
         {
@@ -45,8 +56,19 @@ public class DxfFile
         {
             Debug.Log("cant write file");
         }
-        
     }
+
+    public void AddTriangles(List<Vector3RD> triangleVertices)
+    {
+        netDxf.Vector3 vertex1 = new netDxf.Vector3(triangleVertices[0].x, triangleVertices[0].y, triangleVertices[0].z);
+        netDxf.Vector3 vertex2 = new netDxf.Vector3(triangleVertices[1].x, triangleVertices[1].y, triangleVertices[1].z);
+        netDxf.Vector3 vertex3 = new netDxf.Vector3(triangleVertices[2].x, triangleVertices[2].y, triangleVertices[2].z);
+        Face3d face = new Face3d(vertex1, vertex2, vertex3);
+        Layer laag = new Layer("laagnaam");
+        face.Layer = laag;
+        doc.AddEntity(face);
+    }
+
     [DllImport("__Internal")]
     private static extern void DownloadFile(byte[] array, int byteLength, string fileName);
 }
---
+++ 3DAmsterdam/Assets/Netherlands3D/Plugins/netDxf/DxfRuntimeTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ConvertCoordinates;
+
+public class DxfRuntimeTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<Vector3RD> verts = new List<Vector3RD>();
+        verts.Add(new Vector3RD(0, 0, 0));
+        verts.Add(new Vector3RD(0, 1, 0));
+        verts.Add(new Vector3RD(0, 0, 1));
+
+        DxfFile file = new DxfFile();
+        file.SetupDXF();
+        file.AddTriangles(verts);
+        file.Save();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
