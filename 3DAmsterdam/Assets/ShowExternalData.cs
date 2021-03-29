354d79ed61f367f04c4288018d64f4e9af9a4fd6
Sam Baas <samuelpetrus@gmail.com>
2021-03-29 15:37:24 +0200

    added sibebar content generation (for layer metadata) via json files

--- 3DAmsterdam/Assets/ShowExternalData.cs
+++ 3DAmsterdam/Assets/ShowExternalData.cs
@@ -7,20 +7,17 @@ namespace Netherlands3D.Interface.SidePanel
 {
     public class ShowExternalData : MonoBehaviour
     {
-        [SerializeField]
-        private string metaDataPath = "/metadata.xml";
-
-        public void ShowMetaData()
+        public void Load(string metaDataPath = "metadata.xml")
         {
-            PropertiesPanel.Instance.OpenPanel();
-            PropertiesPanel.Instance.ClearGeneratedFields();
+            PropertiesPanel.Instance.OpenObjectInformation("Laag informatie");
             PropertiesPanel.Instance.AddLoadingSpinner();
-            StartCoroutine(GetText());
+            StartCoroutine(GetText(metaDataPath));
         }
 
-        private IEnumerator GetText()
+        private IEnumerator GetText(string metaDataPath)
         {
-            UnityWebRequest www = UnityWebRequest.Get(metaDataPath + metaDataPath);
+            print("Load external data: " + Config.activeConfiguration.webserverRootPath + "/" + metaDataPath);
+            UnityWebRequest www = UnityWebRequest.Get(Config.activeConfiguration.webserverRootPath + "/" + metaDataPath);
             yield return www.SendWebRequest();
 
             if (www.isNetworkError || www.isHttpError)
@@ -28,13 +25,58 @@ namespace Netherlands3D.Interface.SidePanel
                 Debug.Log(www.error);
             }
             else
-            {
-                // Show results as text
-                Debug.Log(www.downloadHandler.text);
+			{
+				PropertiesPanel.Instance.ClearGeneratedFields();
+				var parsedJson = JsonUtility.FromJson<ExternalData>(www.downloadHandler.text);
+				DrawFields(parsedJson);
+			}
+		}
 
-                // Or retrieve results as binary data
-                byte[] results = www.downloadHandler.data;
-            }
+		private static void DrawFields(ExternalData parsedJson)
+		{
+			foreach (var field in parsedJson.fields)
+			{
+				switch (field.type)
+				{
+					case "title": 
+                        PropertiesPanel.Instance.AddTitle(field.content);
+                        break;
+					case "text":
+                        PropertiesPanel.Instance.AddTextfield(field.content);
+                        break;
+                    case "seperator_line":
+                        PropertiesPanel.Instance.AddSeperatorLine();
+                        break;
+                    case "label":
+                        PropertiesPanel.Instance.AddLabel(field.content);
+                        break;
+                    case "data_field":
+                        PropertiesPanel.Instance.AddDataField(field.key,field.content);
+                        break;
+                    case "link":
+                        PropertiesPanel.Instance.AddLink(field.key, field.content);
+                        break;
+                    case "spacer":
+                        PropertiesPanel.Instance.AddSpacer(field.space);
+                        break;
+				}
+			}
+		}
+	}
+
+    [System.Serializable]
+    public class ExternalData
+    {
+        public string title = "";
+        public Field[] fields;
+
+        [System.Serializable]
+        public class Field
+        {
+            public string type = "title";
+            public string key = "";
+            public string content = "";
+            public float space = 5.0f;
         }
     }
 }
