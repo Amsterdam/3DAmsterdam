23eacc7a1370cbc09fb495c290061d2617945f7d
Sam Baas <samuelpetrus@gmail.com>
2021-04-06 07:17:13 +0200

    removed menu item (clearing clutter)

--- 3DAmsterdam/Assets/3DAmsterdam/Scripts/AssetGeneration/Editor/StartThisScene.cs
+++ 3DAmsterdam/Assets/3DAmsterdam/Scripts/AssetGeneration/Editor/StartThisScene.cs
@@ -6,7 +6,6 @@ using UnityEngine;
 
 public class AutoRun
 {
-    [MenuItem("3D Amsterdam/Tools/Bake")]
     public static void StartThisScene(){
         EditorSceneManager.OpenScene("Assets/3DAmsterdam/Scenes/DataGeneration/GenerateBuildingTilesFromGeoJSON.unity");
         EditorApplication.isPlaying = true;
