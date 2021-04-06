411ca2075d20c2ed8a18ee8e8265d76ffb59f2d4
Sam Baas <samuelpetrus@gmail.com>
2021-04-06 08:42:02 +0200

    make autorun example script point to existing scene

--- 3DAmsterdam/Assets/3DAmsterdam/Scripts/AssetGeneration/Editor/AutoRun.cs
+++ 3DAmsterdam/Assets/3DAmsterdam/Scripts/AssetGeneration/Editor/AutoRun.cs
@@ -10,7 +10,7 @@ public class AutoRun
     /// Creating a static method like this allows you to run the method via batchmode
     /// </summary>
     public static void StartThisScene(){
-        EditorSceneManager.OpenScene("Assets/3DAmsterdam/Scenes/DataGeneration/GenerateBuildingTilesFromGeoJSON.unity");
+        EditorSceneManager.OpenScene("Assets/3DAmsterdam/Scenes/DataGeneration/GenerateBuildingTiles.unity");
         EditorApplication.isPlaying = true;
     }
 }
