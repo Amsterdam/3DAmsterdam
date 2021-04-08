a0505a5f7fdb385d3497e4d7586a3b0fee031e41
Sam Baas <samuelpetrus@gmail.com>
2021-04-08 12:55:10 +0200

    added enviroment settings object to allow applying enviroment settings files with color overrides for different skyboxes / suntime

---
+++ 3DAmsterdam/Assets/Netherlands3D/ScriptableObjects/EnviromentProfile.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/EnviromentProfile", order = 0)]
+[System.Serializable]
+public class EnviromentProfile : ScriptableObject
+{
+    public string enviromentName = "Clear";
+
+    public Texture2D skyIcon;
+
+    public Cubemap skyMap;
+
+    public float exposureDay = 1.0f;
+    public float exposureNight = 0.1f;
+
+    public Color fogColorDay = Color.white;
+    public Color fogColorNight = Color.black;
+
+    public Color ambientColorDay = Color.white;
+    public Color ambientColorNight = Color.black;
+
+    public Color skyTintColorDay = Color.gray;
+    public Color skyTintColorNight = Color.blue;
+
+    public Texture2D sunTexture; //Cloudy sky would require a more fuzzy sun visual
+    public Color sunTextureTintColor = Color.yellow;
+}
---
+++ 3DAmsterdam/Assets/Netherlands3D/Scripts/Settings/EnviromentSettings.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnviromentSettings : MonoBehaviour
+{
+    [SerializeField]
+    private EnviromentProfile enviromentProfile;
+
+    [SerializeField]
+    private EnviromentProfile[] selectableProfiles;
+
+    public static EnviromentSettings Instance;
+
+	public EnviromentProfile[] SelectableProfiles { get => selectableProfiles; private set => selectableProfiles = value; }
+
+	private void Awake()
+	{
+        Instance = this;
+    }
+
+	void Start()
+    {
+        ApplyEnviromentProfile();
+    }
+
+    void ApplyEnviromentProfile()
+    {
+        
+    }
+}
