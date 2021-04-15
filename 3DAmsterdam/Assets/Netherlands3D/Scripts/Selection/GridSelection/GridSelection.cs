e7e11fa9f14fd8d3a062f5b240e0c2841bc2a427
Sam Baas <samuelpetrus@gmail.com>
2021-04-15 12:27:50 +0200

    dont show selection block in our thumbnail

--- 3DAmsterdam/Assets/Netherlands3D/Scripts/Selection/GridSelection/GridSelection.cs
+++ 3DAmsterdam/Assets/Netherlands3D/Scripts/Selection/GridSelection/GridSelection.cs
@@ -256,11 +256,14 @@ namespace Netherlands3D.Interface
 			PropertiesPanel.Instance.OpenObjectInformation("Grid selectie", true, 10);
 
 			//Lets render a ortographic thumbnail for a proper grid topdown view
+			gridSelectionBlock.SetActive(false);
 			PropertiesPanel.Instance.RenderThumbnailContaining(
 				scaleBlock.GetComponent<MeshRenderer>().bounds, 
 				PropertiesPanel.ThumbnailRenderMethod.ORTOGRAPHIC, 
 				scaleBlock.GetComponent<MeshRenderer>().bounds.center + Vector3.up * 150.0f
 			);
+			gridSelectionBlock.SetActive(true);
+
 			PropertiesPanel.Instance.AddTitle("Lagen");
 			PropertiesPanel.Instance.AddActionCheckbox("Gebouwen", Convert.ToBoolean(PlayerPrefs.GetInt("exportLayer0Toggle",1)), (action) =>
 			{
