bea4f6af55d268999b7bbe515e83a7a1bcd14b20
Sam Baas <samuelpetrus@gmail.com>
2021-04-19 19:03:18 +0200

    added layer for cars/physics

---
+++ 3DAmsterdam/Assets/Netherlands3D/Scripts/Utilities/MultiSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 offset;
+    [SerializeField]
+    private int copies = 100;
+
+    void Start()
+    {
+        var copySource = transform.GetChild(0);
+
+        for (int i = 1; i <= copies; i++)
+		{
+            GameObject copy = Instantiate(copySource.gameObject, copySource.transform.parent, true);
+            copy.transform.position = copySource.transform.position + offset*i;
+        }
+    }
+}
