f0ae3f026a6d919f06f671354c1127ff5ec4cf06
Sam Baas <samuelpetrus@gmail.com>
2021-04-20 16:04:23 +0200

    test scene script

---
+++ 3DAmsterdam/Assets/CarGPUInstancingTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarGPUInstancingTest : MonoBehaviour
+{
+    [SerializeField]
+    private Material carShader;
+
+    [SerializeField]
+    private Material wheelShader;
+
+    private bool instancing = true;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            instancing = !instancing;
+
+            carShader.enableInstancing = instancing;
+            wheelShader.enableInstancing = instancing;
+            Debug.Log("Instancing enabled: " + instancing);
+        }
+    }
+}
