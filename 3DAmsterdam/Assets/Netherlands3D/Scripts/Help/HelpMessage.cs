9fd309657e90b8ea34b4bbf155aa814c3f1e1e59
Sam Baas <samuelpetrus@gmail.com>
2021-04-22 11:20:59 +0200

    added fadeout time to the help message

--- 3DAmsterdam/Assets/Netherlands3D/Scripts/Help/HelpMessage.cs
+++ 3DAmsterdam/Assets/Netherlands3D/Scripts/Help/HelpMessage.cs
@@ -7,24 +7,43 @@ namespace Netherlands3D.Help
 {
     public class HelpMessage : MonoBehaviour
     {
-        public static HelpMessage instance;
+        public static HelpMessage Instance;
         [SerializeField]
         private Text textMessageField;
 
+        [SerializeField]
+        private float messageTime = 5.0f;
+
+        [SerializeField]
+        private float fadeOutTime = 1.0f;
+
         void Awake()
         {
-            instance = this;
+            Instance = this;
             gameObject.SetActive(false);
         }
 
         public void Show(string textMessage)
         {   
-            instance.textMessageField.text = textMessage;
+            Instance.textMessageField.text = textMessage;
+
             gameObject.SetActive(true);
+
+            StopAllCoroutines(); //Stop any running fade counters
+            StartCoroutine(WaitForFadeOut());
         }
+
+        private IEnumerator WaitForFadeOut()
+        {
+            yield return new WaitForSeconds(messageTime);
+            textMessageField.CrossFadeAlpha(0, fadeOutTime, false);
+            yield return new WaitForSeconds(fadeOutTime);
+            Hide();
+		}
 
         public void Hide()
         {
+            StopAllCoroutines();
             gameObject.SetActive(false);
         }
     }
