10dde7adaa24be5e8277d31b02163172d3bf8cb8
Sam Baas <samuelpetrus@gmail.com>
2021-04-22 12:00:01 +0200

    have a min. time for help messages

--- 3DAmsterdam/Assets/Netherlands3D/Scripts/Help/HelpMessage.cs
+++ 3DAmsterdam/Assets/Netherlands3D/Scripts/Help/HelpMessage.cs
@@ -12,10 +12,15 @@ namespace Netherlands3D.Help
         private Text textMessageField;
 
         [SerializeField]
-        private float messageTime = 5.0f;
+        private float messageTime = 10.0f;
+
+        [SerializeField]
+        private float minimumMessageTime = 2.0f;
 
         [SerializeField]
         private float fadeOutTime = 1.0f;
+
+        private bool allowHideViaInteraction = false;
 
         void Awake()
         {
@@ -24,27 +29,44 @@ namespace Netherlands3D.Help
         }
 
         public void Show(string textMessage)
-        {   
+        {
+            allowHideViaInteraction = false;
+
             Instance.textMessageField.text = textMessage;
 
             gameObject.SetActive(true);
 
             StopAllCoroutines(); //Stop any running fade counters
+            StartWaitForAllowHide();
             StartCoroutine(WaitForFadeOut());
+        }
+
+        private IEnumerator StartWaitForAllowHide()
+        {
+            yield return new WaitForSeconds(minimumMessageTime);
+            allowHideViaInteraction = true;
         }
 
         private IEnumerator WaitForFadeOut()
         {
             yield return new WaitForSeconds(messageTime);
+            yield return FadeOut();
+		}
+
+        private IEnumerator FadeOut()
+        {
             textMessageField.CrossFadeAlpha(0, fadeOutTime, false);
             yield return new WaitForSeconds(fadeOutTime);
-            Hide();
-		}
+            gameObject.SetActive(false);
+        }
 
         public void Hide()
         {
-            StopAllCoroutines();
-            gameObject.SetActive(false);
+            if (gameObject.activeSelf && allowHideViaInteraction)
+            {
+                StopAllCoroutines();
+                StartCoroutine(FadeOut());
+            }
         }
     }
 }
