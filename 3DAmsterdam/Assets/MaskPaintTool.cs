c6328ca28399f1a4dc7333badca3b5bdabceeb20
Sam Baas <samuelpetrus@gmail.com>
2021-04-29 17:20:10 +0200

    added script for the masking paint tool, using the gridselection to get bounds, and throw the bounds into the masking object when its done

---
+++ 3DAmsterdam/Assets/MaskPaintTool.cs
@@ -0,0 +1,40 @@
+﻿using Netherlands3D.Help;
+using Netherlands3D.Interface.SidePanel;
+using Netherlands3D.Underground;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Netherlands3D.Interface
+{
+    public class MaskPaintTool : MonoBehaviour
+    {
+        [SerializeField]
+        private GridSelection gridSelection;
+        private Bounds previousBounds;
+
+        [SerializeField]
+        private RuntimeMask runtimeRectangularMask;
+
+        public void StartPaintSelection()
+        {
+            gridSelection.gameObject.SetActive(true);
+            GridSelection.onGridSelected += SelectedGridBounds;
+            runtimeRectangularMask.gameObject.SetActive(true);
+            runtimeRectangularMask.Clear();
+            if (previousBounds != default)
+            {
+                runtimeRectangularMask.MoveToBounds(previousBounds);
+            }
+        }
+
+        private void SelectedGridBounds(Bounds bounds)
+        {
+            previousBounds = bounds;
+            runtimeRectangularMask.MoveToBounds(bounds);
+            PropertiesPanel.Instance.OpenLayers();
+            gridSelection.gameObject.SetActive(false);
+            GridSelection.onGridSelected -= SelectedGridBounds;
+        }
+    }
+}
