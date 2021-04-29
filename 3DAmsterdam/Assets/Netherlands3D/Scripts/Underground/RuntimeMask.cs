8f5fa374bf039b839c0ca7d555f5b87bc30a771b
Sam Baas <samuelpetrus@gmail.com>
2021-04-29 17:17:39 +0200

    changed mask to not switch, but simply live on a unique prefab with different masking settings

--- 3DAmsterdam/Assets/Netherlands3D/Scripts/Underground/RuntimeMask.cs
+++ 3DAmsterdam/Assets/Netherlands3D/Scripts/Underground/RuntimeMask.cs
@@ -1,4 +1,5 @@
 ﻿using Netherlands3D.Cameras;
+using Netherlands3D.Interface;
 using Netherlands3D.LayerSystem;
 using System;
 using System.Collections;
@@ -23,6 +24,11 @@ namespace Netherlands3D.Underground
 		}
 
 		[SerializeField]
+		private MaskShape maskShape = MaskShape.SPHERICAL;
+		[SerializeField]
+		private MaskState maskType = MaskState.STATIC_TRANSFORM;
+
+		[SerializeField]
 		private bool updateRuntimeDynamicMaterials = false;
 
 		[SerializeField]
@@ -32,18 +38,8 @@ namespace Netherlands3D.Underground
 		private Material[] specificMaterials;
 
 		[SerializeField]
-		private Material[] rectangularOnlyMaterials;
-
-		private float maskMultiplier = 1.0f;
-
-		[SerializeField]
+		private float maskScaleMultiplier = 1.0f;
 		private Vector4 maskVector = default;
-
-		[SerializeField]
-		private AssetbundleMeshLayer groundMeshLayer;
-
-		private MaskShape maskShape = MaskShape.SPHERICAL;
-		private MaskState maskState = MaskState.STATIC_TRANSFORM;
 
 		private const string clipppingMaskPositionVector = "_ClippingMask";
 		private const string clippingMaskTexture = "_MaskMap";
@@ -53,66 +49,50 @@ namespace Netherlands3D.Underground
 		private Vector2 maskSize;
 
 		[SerializeField]
-		private Texture2D[] maskTextures;
 		private Texture2D maskTexture;
 
-		[SerializeField]
-		private MeshRenderer domeRenderer;
+		public static RuntimeMask current;
+		private RuntimeMask previousMask;
 
-		public static RuntimeMask Instance;
+		private Bounds lastBounds;
 
-		public MaskState State { get => maskState; private set => maskState = value; }
+		private MeshRenderer visual;
 
 		private void Awake()
 		{
-			Instance = this;
-
-			if (!maskTexture) ChangeMaskShape(maskShape);
-			Clear();
-			gameObject.SetActive(false);
+			visual = GetComponent<MeshRenderer>();
 		}
 
 		//We enable shadows for the underground when we use the mask sphere, to give more depth
 		private void OnEnable()
 		{
-			//groundMeshLayer.EnableShadows(true); //We use shadows all the time now, but this might give a performance boost later on
+			//We only allow one runtime mask atm. Make sure to enable any active one.
+			if (current && current != this)
+			{
+				previousMask = current; //Save this, because we want to restore this one if we deactive this mask
+				current.gameObject.SetActive(false);
+			}
+			current = this;
+
+			if(maskType == MaskState.STATIC_TRANSFORM && lastBounds != null)
+			{
+				MoveToBounds(lastBounds);
+			}
 		}
 
 		//We enable shadows for the underground when we use the mask sphere, to give more depth
 		private void OnDisable()
 		{
-			//groundMeshLayer.EnableShadows(false); //We use shadows all the time now, but this might give a performance boost later on
 			//make sure to reset mask shaders
+			current = (previousMask != null) ? previousMask : null;
+			if(previousMask)
+				previousMask.gameObject.SetActive(true);
 			Clear();
-		}
-
-		public void ChangeMaskShape(MaskShape shape)
-		{
-			maskShape = shape;
-			switch (maskShape)
-			{
-				case MaskShape.RECTANGULAR:
-					if (!maskTextures[1].name.Contains("Clone"))
-					{
-						//Make sure our rectangular image is used as an instance, because we want to manipulate it at runtime.
-						maskTextures[1] = Instantiate(maskTextures[1]);
-					}
-					maskTexture = maskTextures[1];
-					break;
-				case MaskShape.SPHERICAL:
-					maskTexture = maskTextures[0];
-					break;
-				default:
-					break;
-			}
-			UpdateSpecificMaterials();
-			UpdateDynamicCreatedInstancedMaterials();
 		}
 
 		public void MoveWithMouse()
 		{
-			State = MaskState.FOLLOW_MOUSE;
-			domeRenderer.enabled = true;
+			maskType = MaskState.FOLLOW_MOUSE;
 			gameObject.SetActive(true);
 		}
 
@@ -143,18 +123,13 @@ namespace Netherlands3D.Underground
 			
 		}
 
-		public void MoveToBounds(Bounds bounds = default)
+		public void MoveToBounds(Bounds bounds)
 		{
-			ChangeMaskShape(MaskShape.RECTANGULAR);
+			lastBounds = bounds;
+			maskType = MaskState.STATIC_TRANSFORM;
 
-			State = MaskState.STATIC_TRANSFORM;
-			domeRenderer.enabled = false;
-
-			if (bounds != default)
-			{
-				this.transform.position = bounds.center;
-				this.transform.localScale = bounds.size * (1.0f + (2.0f / maskTexture.width)); //We use a margin of 1 pixel, so the white edge of our mask texture can be clamped
-			}
+			this.transform.position = bounds.center;
+			this.transform.localScale = bounds.size * (1.0f + (2.0f / maskTexture.width)); //We use a margin of 1 pixel, so the white edge of our mask texture can be clamped
 			CalculateMaskStencil();
 			UpdateSpecificMaterials();
 			UpdateDynamicCreatedInstancedMaterials();
@@ -166,7 +141,7 @@ namespace Netherlands3D.Underground
 		{
 			if (CameraModeChanger.Instance.CameraMode != CameraMode.GodView) return;
 
-			if (State == MaskState.FOLLOW_MOUSE)
+			if (maskType == MaskState.FOLLOW_MOUSE)
 			{
 				//Continious update for moving camera/mouse
 				MoveMaskWithPointer();
@@ -178,8 +153,14 @@ namespace Netherlands3D.Underground
 
 		private void MoveMaskWithPointer()
 		{
+			if (Selector.Instance.HoveringInterface())
+			{
+				transform.transform.localScale = Vector3.zero;
+				return;
+			}
+
 			transform.position = CameraModeChanger.Instance.CurrentCameraControls.GetMousePositionInWorld();
-			transform.transform.localScale = Vector3.one * maskMultiplier * CameraModeChanger.Instance.ActiveCamera.transform.position.y;
+			transform.transform.localScale = Vector3.one * maskScaleMultiplier * CameraModeChanger.Instance.ActiveCamera.transform.position.y;
 		}
 
 		private void CalculateMaskStencil()
@@ -203,16 +184,6 @@ namespace Netherlands3D.Underground
 				sharedMaterial.SetVector(clipppingMaskPositionVector, (resetToZero) ? Vector4.zero : maskVector);
 				sharedMaterial.SetTexture(clippingMaskTexture, (resetToZero) ? null : maskTexture);
 				sharedMaterial.SetVector(clippingMaskSize, maskSize);
-			}
-
-			if(maskShape == MaskShape.RECTANGULAR)
-			{
-				foreach(Material sharedMaterial in rectangularOnlyMaterials)
-				{
-					sharedMaterial.SetVector(clipppingMaskPositionVector, (resetToZero) ? Vector4.zero : maskVector);
-					sharedMaterial.SetTexture(clippingMaskTexture, (resetToZero) ? null : maskTexture);
-					sharedMaterial.SetVector(clippingMaskSize, maskSize);
-				}
 			}
 		}
 
