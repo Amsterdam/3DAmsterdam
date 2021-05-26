b9ed88cc542697d0100a3a355dc17e30bb4ac8fe
AtanasValkov <a_elianator@abv.bg>
2021-05-26 11:22:19 +0200

    Restored UI and Jslib scripts.

---
+++ 3DAmsterdam/Assets/Netherlands3D/Scripts/Traffic/VISSIM/UI/VissimTypeButton.cs
@@ -0,0 +1,39 @@
+﻿using Netherlands3D.Interface;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Netherlands3D.Traffic.VISSIM
+{
+    public class VissimTypeButton : MonoBehaviour
+    {
+        public VissimType thisType;
+        public VissimTypeUI[] allUITypes = default;
+        private Button button = default;
+        private Image image = default;
+
+        private void Start()
+        {
+            button = GetComponent<Button>();
+            image = transform.GetChild(0).GetComponent<Image>();
+            allUITypes = FindObjectsOfType<VissimTypeUI>();
+            button.onClick.AddListener(IsChosen);
+            // sets the tool tip name
+            this.gameObject.GetComponent<TooltipTrigger>().TooltipText = thisType.name;
+            image.sprite = thisType.typeImage;
+        }
+
+        /// <summary>
+        /// Checks if this vehicle type is chosen and adds the name and types to the object that have been chosen in the interface.
+        /// </summary>
+        public void IsChosen()
+        {
+            foreach (VissimTypeUI type in allUITypes)
+            {
+                // updates the assigned vehicle to the active element
+                type.UpdateAssignedVehicles(thisType.vissimTypeAssets, thisType.name);
+            }
+        }
+    }
+}
---
+++ 3DAmsterdam/Assets/Netherlands3D/Scripts/Traffic/VISSIM/VissimStringLoader.cs
@@ -0,0 +1,73 @@
+﻿
+using Netherlands3D.Cameras;
+using Netherlands3D.Interface;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+using Netherlands3D.JavascriptConnection;
+using UnityEngine.Events;
+
+namespace Netherlands3D.Traffic.VISSIM 
+{ 
+
+    public class VissimStringLoader : MonoBehaviour
+    {
+        [SerializeField] private LoadingScreen loadingObjScreen = default;
+        [SerializeField] private Playback playback = default;
+        [SerializeField] private ConvertFZP converter = default;
+        [SerializeField] private GameObject vissimLayerObject = default;
+        [SerializeField] private Transform vissimScriptObject = default;
+
+        private void Start()
+        {
+            vissimLayerObject.SetActive(false);
+            //loadingObjScreen = FindObjectOfType<LoadingScreen>();
+            //playback = FindObjectOfType<VissimPlayback>();
+            //converter = FindObjectOfType<ConvertFZP>();
+        }
+        public void LoadVissimFromJavascript()
+        {
+            StartCoroutine(LoadingProgress());
+            //Debug.Log(JavascriptMethodCaller.FetchVissimDataAsString());
+        }
+
+        IEnumerator LoadingProgress()
+        {
+            // starts loading and sets to 50% by default
+            vissimLayerObject.SetActive(true);
+            loadingObjScreen.ProgressBar.SetMessage("50%");
+            loadingObjScreen.ProgressBar.Percentage(.5f);
+            loadingObjScreen.ShowMessage("FZP wordt geladen: Vissim");
+            yield return new WaitForEndOfFrame();
+            yield return new WaitForEndOfFrame();
+            yield return new WaitForEndOfFrame();
+            // calls vissim data
+            string temp = JavascriptMethodCaller.FetchVissimDataAsString();
+            converter.ReadFileFZP(temp);
+            yield return new WaitForSeconds(1f);
+            // loading "done"
+            loadingObjScreen.ProgressBar.SetMessage("100%");
+            loadingObjScreen.ProgressBar.Percentage(1f);
+            yield return new WaitForSeconds(0.5f);
+            loadingObjScreen.Hide();
+
+        }
+
+        public void DestroyVissim()
+        {
+
+            converter.allVissimData.Clear(); // cleans all old files
+            converter.finishedLoadingData = false;
+            playback.vehicles.Clear(); //  clears all old data
+            foreach (Transform child in vissimScriptObject.transform)
+            {
+                // disables all old cars
+                child.gameObject.SetActive(false);
+            }
+
+            vissimLayerObject.SetActive(false); // disables layer
+        }
+    }
+}
