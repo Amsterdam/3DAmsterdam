4922bc592e0981692a650f9f399636b86927e781
Sam Baas <samuelpetrus@gmail.com>
2021-05-31 17:02:12 +0200

    turned canvas escape script into a more generaly named behaviour script for other panels that need escaping/blocking interactions

---
+++ 3DAmsterdam/Assets/Netherlands3D/Scripts/Interface/SelectorInterfaceInteractable.cs
@@ -0,0 +1,25 @@
+﻿using Netherlands3D.ObjectInteraction;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Netherlands3D.Interface
+{
+    public class SelectorInterfaceInteractable : Interactable
+    {
+        [SerializeField]
+        private Button closeWindowButton;
+
+        void OnEnable()
+        {
+            TakeInteractionPriority();
+        }
+
+		public override void Escape()
+        {
+            //Simply force the close click of the window panel, to share logic.
+            closeWindowButton.onClick.Invoke();
+        }
+    }
+}
