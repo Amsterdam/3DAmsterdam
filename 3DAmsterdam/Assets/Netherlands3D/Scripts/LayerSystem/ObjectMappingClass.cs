ae3046f5c89557c941eb6daaf48d9b7b07825814
Sam Baas <samuelpetrus@gmail.com>
2021-06-17 15:16:41 +0200

    extra comments for what the metadata is

--- 3DAmsterdam/Assets/Netherlands3D/Scripts/LayerSystem/ObjectMappingClass.cs
+++ 3DAmsterdam/Assets/Netherlands3D/Scripts/LayerSystem/ObjectMappingClass.cs
@@ -5,6 +5,6 @@ using UnityEngine;
 public class ObjectMappingClass : ScriptableObject
 {
     public List<string> ids; //List of all Bag ID's in this tile
-    public Vector2[] uvs;
-    public List<int> vectorMap;
+    public Vector2[] uvs; //List of all vert UV coordinates
+    public List<int> vectorMap; //List of all uv/verts belonging to what bag ID in ids
 }
