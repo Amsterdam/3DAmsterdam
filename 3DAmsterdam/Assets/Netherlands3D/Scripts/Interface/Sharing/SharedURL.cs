43a456cd26c999d581739a6427bd6c3d1c45fe15
Sam Baas <samuelpetrus@gmail.com>
2021-06-28 16:35:00 +0200

    comments explaining animation trigger

--- 3DAmsterdam/Assets/Netherlands3D/Scripts/Interface/Sharing/SharedURL.cs
+++ 3DAmsterdam/Assets/Netherlands3D/Scripts/Interface/Sharing/SharedURL.cs
@@ -19,6 +19,8 @@ public class SharedURL : MonoBehaviour
         #else
             GUIUtility.systemCopyBuffer = inputField.text;
         #endif
+
+        //Trigger animator start
         copiedText.gameObject.SetActive(false);
         copiedText.gameObject.SetActive(true);
     }
