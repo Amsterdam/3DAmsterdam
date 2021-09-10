067462ca91dffed473f7cb5cd1c421eb1d7dbd26
Sam Baas <samuelpetrus@gmail.com>
2021-09-10 12:34:54 +0200

    execute in editmode, so we can use our config in editor scripts too

--- 3DAmsterdam/Assets/Netherlands3D/Scripts/ApplicationConfiguration.cs
+++ 3DAmsterdam/Assets/Netherlands3D/Scripts/ApplicationConfiguration.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 namespace Netherlands3D
 {
+    [ExecuteInEditMode]
     public class ApplicationConfiguration : MonoBehaviour
     {
         [Tooltip("Specific configuration file with custom external data paths and viewer options.\nTo create a config file right click in the assets menu and select ScriptableObjects/ConfigurationFile")]
@@ -15,7 +16,9 @@ namespace Netherlands3D
         [SerializeField]
         private ConfigurationFile acceptanceConfigurationFile;
 
-        void Awake()
+		public ConfigurationFile ConfigurationFile { get => configurationFile; }
+
+		void Awake()
         {   
             #if DEVELOPMENT
             Config.activeConfiguration = (acceptanceConfigurationFile) ? acceptanceConfigurationFile : configurationFile;
