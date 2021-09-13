1f8f8fdb8a21a4ce9044e01b620bedce05982143
Sam Baas <samuelpetrus@gmail.com>
2021-09-13 14:42:03 +0200

    added script to load up siteimprove analytics and show main stats as labels for action events

--- 3DAmsterdam/Assets/Netherlands3D/Scripts/Logging/Services/SiteImproveAnalyticsService.cs
+++ 3DAmsterdam/Assets/Netherlands3D/Scripts/Logging/Services/SiteImproveAnalyticsService.cs
@@ -1,8 +1,15 @@
 using Netherlands3D.Interface;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Runtime.InteropServices;
+using System.Text;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
 //using UnityEngine.Analytics;
 
 //Siteimprove.com used by Amsterdam as a Analytics Service
@@ -10,6 +17,9 @@ namespace Netherlands3D.Logging.Services
 {
 	public class SiteImproveAnalyticsService : AnalyticsService
 	{
+		[SerializeField]
+		private VisualStatOverlay visualStatOverlay;
+
 		[DllImport("__Internal")]
 		private static extern void PushEvent(string category = "category", string action = "action", string label = "label");
 
@@ -18,6 +28,86 @@ namespace Netherlands3D.Logging.Services
 #if !UNITY_EDITOR && UNITY_WEBGL
 			PushEvent(category, action, label);
 #endif
+		}
+
+		public void DisplayStatsHeatMap(string urlToCSV)
+		{
+			StartCoroutine(GetCSVStats(urlToCSV));
+		}
+
+#if UNITY_EDITOR
+		[ContextMenu("Load CSV statistic file")]
+		public void DisplayStatsHeatMapEditor()
+		{
+			if (Application.isPlaying)
+			{
+				string path = EditorUtility.OpenFilePanel("Open SiteImprove analytics csv file", "", "csv");
+				StartCoroutine(GetCSVStats($"file:///{path}"));
+			}
+			else
+			{
+				Debug.Log("Make sure the editor is playing");
+			}
+		}
+#endif
+		private void InjectStatisticVisual(AnalyticsClickTrigger targetEventTriggerObject, string percentageOfTotal, string visitsWithThisEvent)
+		{
+			VisualStatOverlay statOverlay = Instantiate(visualStatOverlay, FindObjectOfType<Canvas>().transform);
+			statOverlay.transform.position = targetEventTriggerObject.transform.position;
+			statOverlay.SetParameters(
+				targetEventTriggerObject.transform as RectTransform,
+				percentageOfTotal.Replace("%","").Trim(),
+				visitsWithThisEvent.Trim()
+			);
+		}
+
+		IEnumerator GetCSVStats(string urlToCSV)
+		{
+			Debug.Log($"Loading: {urlToCSV}");
+			UnityWebRequest www = UnityWebRequest.Get(urlToCSV);
+			yield return www.SendWebRequest();
+
+			if (www.result != UnityWebRequest.Result.Success)
+			{
+				Debug.Log(www.error);
+			}
+			else
+			{
+				//Get all events firing objects
+				AnalyticsClickTrigger[] clickTriggers = this.transform.parent.GetComponentsInChildren<AnalyticsClickTrigger>(true);
+
+				StringReader strReader = new StringReader(Encoding.Unicode.GetString(www.downloadHandler.data));
+				while (true)
+				{
+					var csvLine = strReader.ReadLine();
+					if (csvLine != null)
+					{
+						if (csvLine.Contains("\"") && !csvLine.Contains("Performance") && !csvLine.Contains("Categorie"))
+						{
+							//Data line (filtered out performance)
+							string[] fields = csvLine.Split('\t');
+							var actionObject = fields[1].Replace("\"","");
+							var withParentObject = fields[0].Replace("\"", "");
+							var percentageOfTotal = fields[4].Replace("\"", "");
+							var visitsWithThisEvent = fields[5].Replace("\"", "");
+
+							foreach (var clickTrigger in clickTriggers)
+							{
+								Debug.Log($"{clickTrigger.name} == {actionObject} && {clickTrigger.transform.parent.name} == {withParentObject}");
+								if (clickTrigger.name == actionObject && clickTrigger.transform.parent.name == withParentObject)
+								{
+									InjectStatisticVisual(clickTrigger, percentageOfTotal, visitsWithThisEvent);
+								}
+							}
+						}
+					}
+					else
+					{
+						strReader.Close();
+						yield break;
+					}
+				}
+			}
 		}
 	}
 }
---
+++ 3DAmsterdam/Assets/VisualStatOverlay.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VisualStatOverlay : MonoBehaviour
+{
+    [SerializeField]
+    private Text text;
+
+    [SerializeField]
+    private Image backgroundImage;
+
+    private RectTransform followParent;
+
+    public void SetParameters(RectTransform parent, string percentageOfTotal, string visitsWithThisEvent)
+    {
+        followParent = parent;
+
+        //We multiply this by two, because every event also send a Performance event which we want to ignore in % count
+        float percentageOfTotalFloat = float.Parse(percentageOfTotal) * 2;
+        int visitsWithThisEventNumber = int.Parse(visitsWithThisEvent);
+        text.text = $"{visitsWithThisEventNumber}%, {visitsWithThisEvent}";
+
+        float colorLerp = percentageOfTotalFloat / 100.0f;
+        Color bgColor = Color.blue;
+        if (percentageOfTotalFloat < 0.5f){
+            bgColor = Color.Lerp(Color.blue, Color.green, percentageOfTotalFloat/0.5f);
+        }
+        else{
+            bgColor = Color.Lerp(Color.green, Color.red, (colorLerp- 0.5f) / 0.5f);
+        }
+        backgroundImage.color = bgColor;
+    }
+
+    private void Update()
+    {
+        this.transform.localScale = followParent.gameObject.activeInHierarchy ? Vector3.one:Vector3.zero;
+        this.transform.position = followParent.TransformPoint((followParent.transform as RectTransform).rect.center);
+    }
+}
