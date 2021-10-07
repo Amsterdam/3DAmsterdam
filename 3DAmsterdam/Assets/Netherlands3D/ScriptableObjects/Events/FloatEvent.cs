c828b2ca4238820a6bc9ae8a7e4a33a89ba0f838
Sam <samuelpetrus@gmail.com>
2021-10-07 15:41:40 +0200

    moved script reading url parameters into proper folder

---
+++ 3DAmsterdam/Assets/Netherlands3D/ScriptableObjects/Events/FloatEvent.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class FloatValueUnityEvent : UnityEvent<string> { }
+
+[CreateAssetMenu(fileName = "FloatEvent", menuName = "ScriptableObjects/Events/FloatEvent", order = 0)]
+[System.Serializable]
+public class FloatEvent : ScriptableObjectEvent
+{
+    public FloatValueUnityEvent floatEvent;
+}
+
