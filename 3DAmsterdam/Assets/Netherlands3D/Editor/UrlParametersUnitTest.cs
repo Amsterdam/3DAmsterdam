92c653bd5f971455da96135ab4cabdffde067678
Sam <samuelpetrus@gmail.com>
2021-10-07 13:49:59 +0200

    added unit test for retrieving url param and value

---
+++ 3DAmsterdam/Assets/Netherlands3D/Editor/UrlParametersUnitTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+using System.IO;
+using UnityEngine.TestTools;
+
+public class UrlParametersUnitTest 
+{
+    [UnityTest]
+    public IEnumerator TestReadURLParameters()
+    {
+        var urlParameterTriggersObject = new GameObject().AddComponent<URLParameterTriggers>();
+        var parametersAndValues = urlParameterTriggersObject.ReadURLParameters("https://3d.amsterdam.nl/web/app/index.html?T=OPENBARE_TOILETTEN&C=52.357800,4.899740&Z=13&B=52.404827658407925,5.064525604248048,52.31068263374237,4.734935760498048");
+        yield return new WaitForEndOfFrame();
+
+        //We should be able to retrieve the C parameters string value properly:
+        Assert.AreEqual(parametersAndValues["C"], "52.357800,4.899740");
+    }
+}
