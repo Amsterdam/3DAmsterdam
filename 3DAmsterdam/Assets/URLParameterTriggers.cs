6adbf04e26957d80b3baba9ebe7d7f3c0d8f43de
Sam <samuelpetrus@gmail.com>
2021-10-07 14:03:00 +0200

    debug logs

--- 3DAmsterdam/Assets/URLParameterTriggers.cs
+++ 3DAmsterdam/Assets/URLParameterTriggers.cs
@@ -18,18 +18,21 @@ public class URLParameterTriggers : MonoBehaviour
     {
         Dictionary<string, string> nameAndValueCombination = new Dictionary<string, string>();
         var url = (customUrl != "") ? customUrl : Application.absoluteURL;
+        Debug.Log($"Getting parameters from url: {url}");
         var hash = url.Split('#');
         var hasValue = (hash.Length == 2) ? hash[1] : "";
+
         var parameters = url.Replace("?", "&").Split('&');
 
         foreach(var parameter in parameters)
         {
+            Debug.Log($"Parameter: {parameter}");
             if (parameter.Contains("="))
             {
                 var nameAndValue = parameter.Split('=');
                 var parameterName = nameAndValue[0];
-                var value = (parameter.Length == 2) ? nameAndValue[1] : "";
-
+                var value = (nameAndValue.Length == 2) ? nameAndValue[1] : "";
+                Debug.Log($"{parameterName}={value}");
                 var trigger = urlParameterEvents.First(parameterEventTrigger => parameterEventTrigger.eventName == parameterName);
                 if (trigger)
                 {
