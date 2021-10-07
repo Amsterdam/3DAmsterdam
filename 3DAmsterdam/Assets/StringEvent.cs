f152c60f172e0418fc49c8b4c1b2a186c33150c4
Sam <samuelpetrus@gmail.com>
2021-10-07 13:48:33 +0200

    added scriptable object string event for multipurpose decoupling of systems, using communication with string key and value

---
+++ 3DAmsterdam/Assets/StringEvent.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class StringValueUnityEvent : UnityEvent<string> { }
+
+
+[CreateAssetMenu(fileName = "StringEvent", menuName = "ScriptableObjects/Events/StringEvent", order = 0)]
+[System.Serializable]
+public class StringEvent : ScriptableObject
+{
+    public string eventName;
+    public string description;
+    public StringValueUnityEvent stringEvent;
+}
