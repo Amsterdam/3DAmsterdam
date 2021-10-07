8a32aad56252d5a67a9719917e123082d06b585a
Sam <samuelpetrus@gmail.com>
2021-10-07 15:28:37 +0200

    added test for reading hash value

--- 3DAmsterdam/Assets/Netherlands3D/Editor/UrlParametersUnitTest.cs
+++ 3DAmsterdam/Assets/Netherlands3D/Editor/UrlParametersUnitTest.cs
@@ -8,13 +8,22 @@ using UnityEngine.TestTools;
 public class UrlParametersUnitTest 
 {
     [UnityTest]
-    public IEnumerator TestReadURLParameters()
+    public IEnumerator TestReadURLParameter()
     {
         var urlParameterTriggersObject = new GameObject().AddComponent<URLParameterTriggers>();
         var parametersAndValues = urlParameterTriggersObject.ReadURLParameters("https://3d.amsterdam.nl/web/app/index.html?T=OPENBARE_TOILETTEN&C=52.357800,4.899740&Z=13&B=52.404827658407925,5.064525604248048,52.31068263374237,4.734935760498048");
-        yield return new WaitForEndOfFrame();
-
+        yield return null;
         //We should be able to retrieve the C parameters string value properly:
         Assert.AreEqual(parametersAndValues["C"], "52.357800,4.899740");
+    }
+
+    [UnityTest]
+    public IEnumerator TestReadURLHashValue()
+    {
+        var urlParameterTriggersObject = new GameObject().AddComponent<URLParameterTriggers>();
+        var parametersAndValues = urlParameterTriggersObject.ReadURLParameters("https://3d.amsterdam.nl/web/app/index.html?T=OPENBARE_TOILETTEN&C=52.357800,4.899740&Z=13&B=52.404827658407925,5.064525604248048,52.31068263374237,4.734935760498048#hashvaluefound");
+        yield return null;
+        //We should be able to retrieve the C parameters string value properly:
+        Assert.AreEqual(parametersAndValues["#"], "hashvaluefound");
     }
 }
