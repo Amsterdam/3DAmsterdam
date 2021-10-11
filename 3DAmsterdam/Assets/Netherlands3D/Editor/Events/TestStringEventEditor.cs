60708fc4f06609c5c78d6b9ea5d9eb1e993e6573
Sam Baas <samuelpetrus@gmail.com>
2021-10-11 16:43:41 +0200

    allow using predefined testdata in a string event

--- 3DAmsterdam/Assets/Netherlands3D/Editor/Events/TestStringEventEditor.cs
+++ 3DAmsterdam/Assets/Netherlands3D/Editor/Events/TestStringEventEditor.cs
@@ -5,19 +5,16 @@ using UnityEngine;
 
 [CustomEditor(typeof(StringEvent))]
 public class TestStringEventEditor : Editor
-{
-    private string dummyPayload = "";
-    public override void OnInspectorGUI()
+{    public override void OnInspectorGUI()
     {
         base.OnInspectorGUI();
         var script = (StringEvent)target;
 
         if (Application.isPlaying)
         {
-            dummyPayload = EditorGUILayout.TextField("Payload", dummyPayload);
             if (GUILayout.Button("Trigger", GUILayout.Height(40)))
             {
-                script.unityEvent?.Invoke(dummyPayload);
+                script.unityEvent?.Invoke(script.testData);
             }
         }
     }
