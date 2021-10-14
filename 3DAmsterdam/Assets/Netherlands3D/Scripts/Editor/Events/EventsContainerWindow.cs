5d057321a3fd514159039ef8d22f8d2ab06e6fd6
Sam Baas <samuelpetrus@gmail.com>
2021-10-14 17:57:49 +0200

    small poc of unity window listing all events with the option to trigger them with a payload

---
+++ 3DAmsterdam/Assets/Netherlands3D/Scripts/Editor/Events/EventsContainerWindow.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using UnityEditor;
+
+public class EventsContainerWindow : EditorWindow
+{
+    static string[] stringEventContainers;
+    static string[] vector3EventContainers;
+
+    static string testData = "";
+
+    Vector2 scrollPosition;
+    // Add menu named "My Window" to the Window menu
+    [MenuItem("Netherlands 3D/Event Containers Window")]
+    static void Init()
+    {
+        EventsContainerWindow window = (EventsContainerWindow)EditorWindow.GetWindow(typeof(EventsContainerWindow),true,"Events Container Window");
+        window.Show();
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("Event test data", EditorStyles.boldLabel);
+        testData = EditorGUILayout.TextField("", testData);
+
+
+        scrollPosition = GUILayout.BeginScrollView(
+            scrollPosition);
+
+        //StringEvent
+        GUILayout.Label("StringEvent Containers", EditorStyles.boldLabel);
+        if (stringEventContainers == null) stringEventContainers = AssetDatabase.FindAssets("t:StringEvent");
+        foreach (var stringEventContainer in stringEventContainers)
+		{
+            var assetPath = AssetDatabase.GUIDToAssetPath(stringEventContainer);
+            StringEvent asset = (StringEvent)AssetDatabase.LoadAssetAtPath(assetPath, typeof(StringEvent));
+            GUILayout.BeginHorizontal();
+            EditorGUILayout.ObjectField(asset, typeof(StringEvent),false);
+            if (GUILayout.Button("Trigger", GUILayout.Height(20)))
+            {
+                asset.unityEvent?.Invoke(testData);
+            }
+            GUILayout.EndHorizontal();
+        }
+
+        //Vector3Event
+        GUILayout.Label("Vector3Event Containers", EditorStyles.boldLabel);
+        if (vector3EventContainers == null) vector3EventContainers = AssetDatabase.FindAssets("t:Vector3Event");
+        foreach (var vector3EventContainer in vector3EventContainers)
+        {
+            var assetPath = AssetDatabase.GUIDToAssetPath(vector3EventContainer);
+            Vector3Event asset = (Vector3Event)AssetDatabase.LoadAssetAtPath(assetPath, typeof(Vector3Event));
+            GUILayout.BeginHorizontal();
+            EditorGUILayout.ObjectField(asset, typeof(Vector3Event),false);
+            if (GUILayout.Button("Trigger", GUILayout.Height(20)))
+            {
+                var vector3string = testData.Split(',');
+                Vector3 vector3 = new Vector3(
+                    float.Parse(vector3string[0]), 
+                    float.Parse(vector3string[1]), 
+                    float.Parse(vector3string[2])
+                );
+                asset.unityEvent?.Invoke(vector3);
+            }
+            GUILayout.EndHorizontal();
+        }
+
+        GUILayout.EndScrollView();
+    }
+}
