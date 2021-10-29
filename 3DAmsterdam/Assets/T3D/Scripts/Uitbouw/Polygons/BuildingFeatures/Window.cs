1c2653dc2936a2b4e0cd019924319bb2ff0d5a1a
Tom Simons <tom.v.simons@gmail.com>
2021-10-29 14:46:53 +0200

    Added resize feature for window

--- 3DAmsterdam/Assets/T3D/Scripts/Uitbouw/Polygons/BuildingFeatures/Window.cs
+++ 3DAmsterdam/Assets/T3D/Scripts/Uitbouw/Polygons/BuildingFeatures/Window.cs
@@ -19,15 +19,5 @@ namespace Netherlands3D.T3D.Uitbouw
             windowTransform.position = wall.transform.position;
             windowTransform.forward = wall.transform.forward;
         }
-
-        public void SetSize(Vector2 size)
-        {
-            leftBound.localPosition = new Vector3(-size.x / 2, 0, 0);
-            rightBound.localPosition = new Vector3(size.x / 2, 0, 0);
-            topBound.localPosition = new Vector3(0, size.y / 2, 0);
-            bottomBound.localPosition = new Vector3(0, -size.y / 2, 0);
-
-            RecalculateScale();
-        }
     }
 }
--- 3DAmsterdam/Assets/T3D/Scripts/Uitbouw/Polygons/SquarePolygon.cs
+++ 3DAmsterdam/Assets/T3D/Scripts/Uitbouw/Polygons/SquarePolygon.cs
@@ -21,15 +21,34 @@ public class SquarePolygon : CityPolygon
 
                 GetCorner(leftBound, topBound),
                 GetCorner(rightBound, topBound),
-                GetCorner(leftBound, bottomBound),
                 GetCorner(rightBound, bottomBound),
+                GetCorner(leftBound, bottomBound),
             };
         }
     }
 
+    public Vector2 Size { get; private set; }
+
+    private void Start()
+    {
+        RecalculateScale();
+    }
+
+    public void SetSize(Vector2 size)
+    {
+        Size = size;
+        leftBound.localPosition = new Vector3(-size.x / 2, 0, 0);
+        rightBound.localPosition = new Vector3(size.x / 2, 0, 0);
+        topBound.localPosition = new Vector3(0, size.y / 2, 0);
+        bottomBound.localPosition = new Vector3(0, -size.y / 2, 0);
+
+        RecalculateScale();
+    }
+
     public void RecalculateScale()
     {
-        transform.localScale = CalculateXYScale(leftBound, rightBound, topBound, bottomBound);
+        Size = CalculateXYScale(leftBound, rightBound, topBound, bottomBound);
+        transform.localScale = Size;
     }
 
     protected static Vector3 CalculateXYScale(Transform left, Transform right, Transform top, Transform bottom)
@@ -50,7 +69,7 @@ public class SquarePolygon : CityPolygon
         float hDist = Vector3.Distance(transform.position, projectedHPoint);
         float vDist = Vector3.Distance(transform.position, projectedVPoint);
 
-        var hDir = (projectedHPoint- transform.position).normalized;
+        var hDir = (projectedHPoint - transform.position).normalized;
         var vDir = (projectedVPoint - transform.position).normalized;
 
         return transform.position + hDir * hDist + vDir * vDist;
