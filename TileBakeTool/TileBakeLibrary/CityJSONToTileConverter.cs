384b41ac09e1567571d4c15d6832467be7a0224e
Sammey <samuelpetrus@gmail.com>
2021-11-22 14:36:34 +0100

    added tasks for parsing CityJSON files and passing down optional config parameters

--- TileBakeTool/TileBakeLibrary/CityJSONToTileConverter.cs
+++ TileBakeTool/TileBakeLibrary/CityJSONToTileConverter.cs
@@ -1,20 +1,53 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Numerics;
+using System.Threading.Tasks;
 using TileBakeLibrary.Coordinates;
+using SimpleJSON;
+using System.Linq;
 
 namespace TileBakeLibrary
 {
 	public class CityJSONToTileConverter : IConverter
 	{
 		private string sourcePath = "";
-		private string targetPath = "";
+		private string outputPath = "";
 
-		/// <summary>
-		/// The source folder path containing all .cityjson files that need to be converted
-		/// </summary>
-		/// <param name="source"></param>
-		public void SetSourcePath(string source)
+        private string identifier = "";
+        private string removeFromID = "";
+
+        private int lodIndex = 0;
+
+        private List<CityObject> allCityObjects = new List<CityObject>();
+        private List<Tile> tiles = new List<Tile>();
+        private Task<List<CityObject>>[] conversionTasks;
+
+        /// <summary>
+        /// The LOD index we want to parse. 
+        /// </summary>
+        /// <param name="targetLodIndex">Defaults to 0</param>
+        public void SetLODSlot(int targetLodIndex)
+        {
+            lodIndex = targetLodIndex;
+        }
+
+        /// <summary>
+        /// Determines the property that will be used as unique object identifier
+        /// </summary>
+        /// <param name="id">The propery field name, for example 'building_id'</param>
+        /// <param name="remove">Remove this substring from the ID before storing it</param>
+        public void SetID(string id, string remove)
+        {
+            identifier = id;
+            removeFromID = remove;
+        }
+
+        /// <summary>
+        /// The source folder path containing all .cityjson files that need to be converted
+        /// </summary>
+        /// <param name="source"></param>
+        public void SetSourcePath(string source)
 		{
 			sourcePath = source;
 		}
@@ -25,21 +58,126 @@ namespace TileBakeLibrary
 		/// <param name="target"></param>
 		public void SetTargetPath(string target)
 		{
-			targetPath = target;
+			outputPath = target;
 		}
 
 		/// <summary>
-		/// Start converting the .cityjson files into binary tile files
+		/// Start converting the cityjson files into binary tile files
 		/// </summary>
 		public void Convert()
 		{
-			var someDoubleVector3 = new Vector3Double(
-				1,
-				2,
-				3
-			);
+            //If no specific filename or wildcard was supplied, default to .json files
+			var filter = Path.GetFileName(sourcePath);
+			if(filter == "") filter = "*.json";
 
-			File.WriteAllText($"{targetPath}/dummy.txt", "dummy file " + someDoubleVector3);
+            //List the files that we are going to parse
+			string[] sourceFiles = Directory.GetFiles(sourcePath, filter);
+            if(sourceFiles.Length == 0){
+                Console.WriteLine($"No \"{filter}\" files found in {sourcePath}");
+                return;
+            }
+
+            //Create a threadable task for every file, that returns a list of parsed cityobjects
+			conversionTasks = new Task<List<CityObject>>[sourceFiles.Length];
+			for (int i = 0; i < sourceFiles.Length; i++)
+			{
+				conversionTasks[i] = Task.Run(() => ConvertProcess(sourceFiles[i]));
+			}
+
+            //Wait for all the files to be parsed, and combine the results
+			Task.WaitAll(conversionTasks);
+            foreach (var task in conversionTasks)
+            {
+                allCityObjects.AddRange(task.Result);
+            }
+
+            BakeTiles();
+			Console.ReadLine();
+		}
+
+        /// <summary>
+        /// Bake the city objects into binary tiles
+        /// </summary>
+        private void BakeTiles()
+        {
+            File.WriteAllLines(outputPath, allCityObjects.Select(cityObject => cityObject.id).ToArray());
+		}
+
+		private async Task<List<CityObject>> ConvertProcess(string sourceFile)
+		{
+            List<CityObject> foundCityObjects = new List<CityObject>();
+
+            //Parse the file
+            var jsonstring = File.ReadAllText(sourceFile);
+            Console.WriteLine($"Parsing CityJSON: {jsonstring}");
+            await Task.Delay(10);
+
+            var cityjsonNode = JSON.Parse(jsonstring);
+            if (cityjsonNode == null || cityjsonNode["CityObjects"] == null)
+            {
+                Console.WriteLine($"Failed to parse CityJSON file {sourceFile}");
+                return null;
+            }
+
+            //Get vertices
+            var allVerts = new List<Vector3Double>();
+
+            //Optionaly parse transform scale and offset
+            var transformScale = (cityjsonNode["transform"] != null && cityjsonNode["transform"]["scale"] != null) ? new Vector3Double(
+                cityjsonNode["transform"]["scale"][0].AsDouble,
+                cityjsonNode["transform"]["scale"][1].AsDouble,
+                cityjsonNode["transform"]["scale"][2].AsDouble
+            ) : new Vector3Double(1, 1, 1);
+
+            var transformOffset = (cityjsonNode["transform"] != null && cityjsonNode["transform"]["translate"] != null) ? new Vector3Double(
+                   cityjsonNode["transform"]["translate"][0].AsDouble,
+                   cityjsonNode["transform"]["translate"][1].AsDouble,
+                   cityjsonNode["transform"]["translate"][2].AsDouble
+            ) : new Vector3Double(0, 0, 0);
+
+            //Now load all the vertices with the scaler and offset applied
+            foreach (JSONNode node in cityjsonNode["vertices"])
+            {
+                var vert = new Vector3Double(
+                       node[0].AsDouble * transformScale.x + transformOffset.x,
+                       node[1].AsDouble * transformScale.y + transformOffset.y,
+                       node[2].AsDouble * transformScale.z + transformOffset.z
+                );
+
+                allVerts.Add(vert);
+            }
+
+            //Now build the meshes and create the city objects
+            foreach (JSONNode buildingNode in cityjsonNode["CityObjects"])
+            {
+                //Object ID
+                var objectID = buildingNode["attributes"][identifier].Value.Replace(removeFromID, "");
+                                
+                //The building verts/triangles
+                var boundaries = buildingNode["geometry"][lodIndex]["boundaries"][0];
+
+                var newCityObject = new CityObject()
+                {
+                    id = objectID
+				};
+
+                foundCityObjects.Add(newCityObject);
+            }
+            return foundCityObjects;
+        }
+
+        /// <summary>
+        /// Add our parsed city object
+        /// </summary>
+        /// <param name="id"></param>
+        private void AddCityObject(string id)
+        {
+            allCityObjects.Add(
+                new CityObject()
+                {
+                    id = id
+                }
+            );
 		}
 
 		/// <summary>
@@ -47,7 +185,7 @@ namespace TileBakeLibrary
 		/// </summary>
 		public void Cancel()
 		{
-			
-		}
+            
+        }
 	}
 }
--- TileBakeTool/TileBakeTool/Program.cs
+++ TileBakeTool/TileBakeTool/Program.cs
@@ -11,14 +11,41 @@ namespace TileBakeTool
 		private static string outputPath = "";
 		private static string newline = "\n";
 
+		private static string identifier = "id";
+		private static string removeFromIdentifier = "";
+
+
+		private static int lodIndex = 0;
+
 		static void Main(string[] args)
 		{
-			if(args.Length == 0)
+			if (args.Length == 0 || (args.Length == 1 && args[0].ToLower().Contains("help")))
 			{
+				//No parameters or an attempt to call for help? Show help in console.
 				ShowHelp();
 			}
+			else if (args.Length == 1)
+			{
+				//One parameter? Assume its a source path.
+				DefaultArgument(args[0]);
+			}
+			else
+			{
+				//More parameters? Parse them
+				ParseArguments(args);
+			}
+		}
 
-			ParseArguments(args);
+		private static void DefaultArgument(string sourcePath)
+		{
+			if(!Path.IsPathFullyQualified(sourcePath)){
+				Console.WriteLine($"Not a valid path: {sourcePath}");
+				Console.WriteLine($"This might help: ");
+				ShowHelp();
+				return;
+			}
+
+			StartConverting(sourcePath, sourcePath);
 		}
 
 		private static void ShowHelp()
@@ -32,30 +59,23 @@ Required options
 
 Extra options:
 
---add                  Add objects to existing binary tile files
---replace              Replace objects with the same ID
---id <property name>   Unique ID property name
---filter-type <type>   Filter object on type
---filter-lod <lod>     Filter LOD level
+--add						Add objects to existing binary tile files
+--replace					Replace objects with the same ID
+--id <property name>		Unique ID property name
+--id-remove <string>		Remove this substring from the ID's
+--filter-type <type>		Filter object on type
+--filter-lod <lod>			Target LOD index
 
 Pipeline example:
 
-TileBakeTool.exe --source ""C:/MyProject/CityJsonFiles/*.json"" --output ""C:/MyProject/BinaryTiles/"" --filter-lod ""2.2"" --filter-type ""gebouw"" --id ""GebouwNummer""
+TileBakeTool.exe --source ""C:/MyProject/CityJsonFiles/*.json"" --output ""C:/MyProject/BinaryTiles/"" --filter-lod ""2"" --filter-type ""gebouw"" --id ""GebouwNummer""
 
 TileBakeTool.exe --source ""C:/MyProject/CustomMadeBuildings/*.json""--output ""C:/MyProject/BinaryTiles/"" --id ""BAGID"" --add --replace");
 		}
 
-		private static void StartConverting(string sourcePath, string targetPath)
-		{
-			var tileBaker = new CityJSONToTileConverter();
-			tileBaker.SetSourcePath(sourcePath);
-			tileBaker.SetTargetPath(targetPath);
-
-			tileBaker.Convert();
-		}
-
 		private static void ParseArguments(string[] args)
 		{
+			//Read the arguments and apply corresponding settings
 			for (int i = 0; i < args.Length; i++)
 			{
 				var argument = args[i];
@@ -69,8 +89,19 @@ TileBakeTool.exe --source ""C:/MyProject/CustomMadeBuildings/*.json""--output ""
 				StartConverting(sourcePath, outputPath);
 		}
 
+		private static void StartConverting(string sourcePath, string targetPath)
+		{
+			var tileBaker = new CityJSONToTileConverter();
+			tileBaker.SetSourcePath(sourcePath);
+			tileBaker.SetTargetPath(targetPath);
+			tileBaker.SetLODSlot(lodIndex);
+			tileBaker.SetID(identifier, removeFromIdentifier);
+			tileBaker.Convert();
+		}
+
 		private static void ApplySetting(string argument, string value)
 		{
+			Console.WriteLine($"Setting {argument} = {value}");
 			switch (argument)
 			{
 				case "--source":
@@ -78,6 +109,15 @@ TileBakeTool.exe --source ""C:/MyProject/CustomMadeBuildings/*.json""--output ""
 					break;
 				case "--output":
 					outputPath = value;
+					break;
+				case "--filter-lod":
+					lodIndex = int.Parse(value);
+					break;
+				case "--id":
+					identifier = value;
+					break;
+				case "--id-remove":
+					removeFromIdentifier = value;
 					break;
 				default:
 					break;
