222c423984b116ed23ad1a2955509671b4eed842
Sammey <samuelpetrus@gmail.com>
2021-11-22 16:36:18 +0100

    added initial binarymeshwriter, using the tile object

---
+++ TileBakeTool/TileBakeLibrary/BinaryMeshWriter.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TileBakeLibrary
+{
+	class BinaryMeshWriter
+	{
+        public static void SaveAsBinaryFile(int version, Tile tile, string filePath)
+        {
+            using (FileStream file = File.Create(filePath))
+            {
+                using (BinaryWriter writer = new BinaryWriter(file))
+                {
+                    //Version int
+                    writer.Write(version);
+
+                    //Verts
+                    var vertices = tile.vertices;
+                    writer.Write(vertices.Length);
+                    foreach (Vector3 vert in tile.vertices)
+                    {
+                        writer.Write(vert.x);
+                        writer.Write(vert.y);
+                        writer.Write(vert.z);
+                    }
+
+                    var normals = tile.normals;
+                    //Normals
+                    writer.Write(normals.Length);
+                    foreach (Vector3 normal in normals)
+                    {
+                        writer.Write(normal.x);
+                        writer.Write(normal.y);
+                        writer.Write(normal.z);
+                    }
+
+                    //UV
+                    var uvs = tile.uv;
+                    writer.Write(uvs.Length);
+                    foreach (Vector2 uv in uvs)
+                    {
+                        writer.Write(uv.x);
+                        writer.Write(uv.y);
+                    }
+
+                    //Every triangle list per submesh
+                    writer.Write(tile.subMeshCount);
+                    for (int i = 0; i < tile.subMeshCount; i++)
+                    {
+                        int[] submeshTriangleList = tile.GetTriangles(i);
+                        writer.Write(submeshTriangleList.Length);
+                        writer.Write(tile.GetSubMesh(i).baseVertex);
+                        //var offset = sourceMesh.GetSubMesh(i).baseVertex;
+                        foreach (int index in submeshTriangleList)
+                        {
+                            writer.Write(index);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- TileBakeTool/TileBakeLibrary/Tile.cs
+++ TileBakeTool/TileBakeLibrary/Tile.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using TileBakeLibrary.Coordinates;
@@ -9,7 +10,13 @@ namespace TileBakeLibrary
 {
 	class Tile
 	{
-		Vector2Double position;
-		List<CityObject> cityObjects;
+		public Vector2Double position;
+		public List<CityObject> cityObjects;
+
+		public List<Vector3> vertices;
+		public List<Vector3> normals;
+		public List<Vector2> uvs;
+
+		public List<int> triangleIndices;
 	}
 }
