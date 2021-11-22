7463a899f824fa2ffd9a5c7ab57efbf93846a8fe
Sammey <samuelpetrus@gmail.com>
2021-11-22 14:34:50 +0100

    added classes for tiles, and the containing cityjson object(s)

---
+++ TileBakeTool/TileBakeLibrary/CityObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Numerics;
+
+namespace TileBakeLibrary
+{
+	class CityObject
+	{
+		public string id = "";
+		public List<Vector3> vertices; 
+		public List<Vector3> normals; 
+		public List<Vector3> uvs; 
+		public List<int> triangleIndices;
+
+		public Vector2 centroid;
+	}
+}
---
+++ TileBakeTool/TileBakeLibrary/Tile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TileBakeLibrary.Coordinates;
+
+namespace TileBakeLibrary
+{
+	class Tile
+	{
+		Vector2Double position;
+		List<CityObject> cityObjects;
+	}
+}
