b991dc797130c4c1d0f85541bc6d953a1afef182
Tom Simons <tom.v.simons@gmail.com>
2021-11-23 12:20:10 +0100

    Saved step 1 data, made units cm

---
+++ 3DAmsterdam/Assets/T3D/Scripts/Export/IDataSaver.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IDataSaver
+{
+    public void SaveFloat(string key, float value);
+    public void SaveInt(string key, int value);
+    public void SaveString(string key, string value);
+    public void ExportSaveData();
+}
---
+++ 3DAmsterdam/Assets/T3D/Scripts/Export/SavableVariable.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class SavableVariable<T>
+{
+    //public KeyValuePair<string, T> KeyValuePair { get; private set; }
+
+    //public string Key { get; private set; }
+    //public abstract KeyValuePair<string, T> Pair { get; protected set; }
+    public string Key { get; set; }
+    public T Value { get; protected set; }
+    public abstract void Save();
+    public abstract void Load();
+
+    public SavableVariable(string key, bool loadSavedData)
+    {
+        Key = key;
+        if (loadSavedData)
+            Load();
+    }
+
+    public void SetValue(T value)
+    {
+        Value = value;
+        Save();
+    }
+}
+
+public class SaveableFloat : SavableVariable<float>
+{
+    public SaveableFloat(string key, bool loadSavedData) : base(key, loadSavedData)
+    {
+    }
+
+    public override void Load()
+    {
+        Value = SessionSaver.LoadFloat(Key);
+    }
+
+    public override void Save()
+    {
+        SessionSaver.SaveFloat(Key, Value);
+    }
+}
+
+public class SaveableInt : SavableVariable<int>
+{
+    public SaveableInt(string key, bool loadSavedData) : base(key, loadSavedData)
+    {
+    }
+
+    public override void Load()
+    {
+        Value = SessionSaver.LoadInt(Key);
+    }
+
+    public override void Save()
+    {
+        SessionSaver.SaveInt(Key, Value);
+    }
+}
+
+public class SaveableString : SavableVariable<string>
+{
+    public SaveableString(string key, bool loadSavedData) : base(key, loadSavedData)
+    {
+    }
+
+    public override void Load()
+    {
+        Value = SessionSaver.LoadString(Key);
+    }
+
+    public override void Save()
+    {
+        SessionSaver.SaveString(Key, Value);
+    }
+}
