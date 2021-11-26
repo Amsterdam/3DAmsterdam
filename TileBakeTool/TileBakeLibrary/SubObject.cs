16b3bfa3aad100e6067c3d7c371ce36aec913322
Sammey <samuelpetrus@gmail.com>
2021-11-26 12:16:50 +0100

    merge similar verts

--- TileBakeTool/TileBakeLibrary/SubObject.cs
+++ TileBakeTool/TileBakeLibrary/SubObject.cs
@@ -18,7 +18,57 @@ namespace TileBakeLibrary
 
 		public void MergeSimilarVertices(float mergeVerticesBelowNormalAngle)
 		{
-			throw new NotImplementedException();
+			var radians = (Math.PI / 180) * mergeVerticesBelowNormalAngle;
+			float cosAngleThreshold = (float)Math.Cos(radians);
+
+			List<Vector3Double> cleanedVertices = new();
+			List<Vector3> cleanedNormals = new();
+			List<Vector2> cleanedUvs = new();
+
+			//Traverse the triangles, and if we encounter verts+normals that are similar, dispose them
+			for (int i = 0; i < triangleIndices.Count; i++)
+			{
+				var index = triangleIndices[i];
+				triangleIndices[i] = GetOrAddVertexIndex(index ,cleanedVertices,cleanedNormals,cleanedUvs, cosAngleThreshold);
+			}
+
+			var merged = vertices.Count - cleanedVertices.Count;
+			Console.WriteLine($"{id} has {merged} verts merged.");
+			//Now use our new cleaned geometry
+			vertices = cleanedVertices;
+			normals = cleanedNormals;
+			uvs = cleanedUvs;
+		}
+
+		private int GetOrAddVertexIndex(int index, List<Vector3Double> cleanedVertices, List<Vector3> cleanedNormals, List<Vector2> cleanedUvs, float angleThreshold)
+		{
+			Vector3Double inputVertex = vertices[index];
+			Vector3 inputNormal = normals[index];
+			//Vector2 inputUv = uvs[index]; //When we support uv's, a vertex with a unique UV should not be merged and be added as a unique one
+
+		    //Add vertex with unique positions
+			if(!cleanedVertices.Contains(inputVertex))
+			{
+				cleanedVertices.Add(inputVertex);
+				cleanedNormals.Add(inputNormal);
+				return cleanedVertices.Count - 1;
+			}
+			else 
+			{
+				//Compare the normal using a threshold
+				var vertexIndex = cleanedVertices.IndexOf(inputVertex);
+				var existingVertNormal = cleanedNormals[vertexIndex];
+				if (Vector3.Dot(inputNormal, existingVertNormal) >= angleThreshold)
+				{
+					//Similar enough normal reuse existing vert
+					return vertexIndex;
+				}
+				else{
+					cleanedVertices.Add(inputVertex);
+					cleanedNormals.Add(inputNormal);
+					return cleanedVertices.Count - 1;
+				}
+			}
 		}
 	}
 }
