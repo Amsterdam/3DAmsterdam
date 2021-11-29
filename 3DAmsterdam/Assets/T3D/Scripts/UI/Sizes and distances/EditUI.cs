15d7cd7d26d46fc7a64cc11933b4ae63c06cb978
Tom Simons <tom.v.simons@gmail.com>
2021-11-29 15:13:20 +0100

    Started adding code to move edit ui when behind other labels

--- 3DAmsterdam/Assets/T3D/Scripts/UI/Sizes and distances/EditUI.cs
+++ 3DAmsterdam/Assets/T3D/Scripts/UI/Sizes and distances/EditUI.cs
@@ -15,6 +15,14 @@ namespace Netherlands3D.T3D.Uitbouw.BoundaryFeatures
 
         private BoundaryFeature activeFeature;
 
+        //private RectTransform rectTransform;
+
+        //public override void Awake()
+        //{
+        //    base.Awake();
+        //    rectTransform = GetComponent<RectTransform>();
+        //}
+
         public void UpdateSprites(EditMode newMode)
         {
             if (newMode == EditMode.Resize)
@@ -34,6 +42,44 @@ namespace Netherlands3D.T3D.Uitbouw.BoundaryFeatures
 
             editButton.onClick.AddListener(activeFeature.EditFeature);
             deleteButton.onClick.AddListener(activeFeature.DeleteFeature);
+        }
+
+        Vector3 offset = new Vector3(-35, -35, 0);
+        float moveSpeed = 1f;
+        //protected override void Update()
+        void LateUpdate()
+        {
+            //base.Update();
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                moveSpeed -= 1;
+                print(moveSpeed);
+            }
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                moveSpeed += 1;
+                print(moveSpeed);
+            }
+
+            var labels = CoordinateNumbers.Instance.GetComponentsInChildren<NumberInputField>();
+            foreach (var label in labels)
+            {
+                if (label == rectTransform)
+                {
+                    continue;
+                }
+                var otherRect = label.GetComponent<RectTransform>();
+                if (rectTransform.Overlaps(otherRect))
+                {
+                    var dir = otherRect.position - rectTransform.position;
+                    print(dir);
+                    //rectTransform.position -= offset * moveSpeed;
+                    //break;
+                }
+            }
+
         }
     }
 }
