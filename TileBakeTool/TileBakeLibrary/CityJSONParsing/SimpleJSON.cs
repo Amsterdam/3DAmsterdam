3c37cf97c2a3fda9bebcf7c30431c2dd0360d6e5
mvangog <martijn.van.gog@amsterdam.nl>
2021-12-08 12:27:19 +0100

    add licensing statements

--- TileBakeTool/TileBakeLibrary/CityJSONParsing/SimpleJSON.cs
+++ TileBakeTool/TileBakeLibrary/CityJSONParsing/SimpleJSON.cs
@@ -115,6 +115,17 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  * 
+ * 
+ * The Following notice applies to the JSONNode.StreamParse.
+ * / Copyright (C) X Gemeente
+ *                 X Amsterdam 
+ *                 X Economic Services Departments
+ * Licensed under the EUPL, Version 1.2 or later (the "License");
+ * You may not use this work except in compliance with the License. You may obtain a copy of the License at:
+ * EUPL text (EUPL-1.2)
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing permissions and limitations under the License.
  * * * * */
 using System;
 using System.Collections;
