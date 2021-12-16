09bc58566cc39123033075866f1c44d36426c4a6
Sammey <samuelpetrus@gmail.com>
2021-12-16 12:06:29 +0100

    moved events

--- 3DAmsterdam/Assets/ImportGeoJSON.cs
+++ 3DAmsterdam/Assets/ImportGeoJSON.cs
@@ -1,4 +1,5 @@
 using Netherlands3D.Events;
+using Netherlands3D.Utilities;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
@@ -6,11 +7,17 @@ using UnityEngine;
 
 public class ImportGeoJSON : MonoBehaviour
 {
+	[Header("Listen to events:")]
     [SerializeField]
     private StringEvent filesImportedEvent;
 
-    [SerializeField]
+	[Header("Trigger events:")]
+	[SerializeField]
     private BoolEvent clearDataBaseEvent;
+	[SerializeField]
+	private Vector3ListsEvent drawGeometryEvent;
+
+	private List<Dictionary<string, object>> propertyList = new List<Dictionary<string, object>>();
 
 	private void Awake()
 	{
@@ -39,6 +46,32 @@ public class ImportGeoJSON : MonoBehaviour
 		}
 
 		var json = File.ReadAllText(file);
+		GeoJSON geojson = new GeoJSON(json);
+		float count = 0;
+		while (geojson.GotoNextFeature())
+		{
+			count++;
 
+			var polygons = geojson.GetPolygon();
+			var properties = geojson.GetProperties();
+			propertyList.Add(properties);
+
+			foreach (var polygon in polygons)
+			{
+				List<IList<Vector3>> polyList = new List<IList<Vector3>>();
+				List<Vector3> list = new List<Vector3>();
+				foreach (var point in polygon)
+				{
+					var p = ConvertCoordinates.CoordConvert.WGS84toUnity(point.x, point.y);
+					p.y = count;
+					list.Add(p);
+				}
+
+				polyList.Add(list);
+
+				drawGeometryEvent.started?.Invoke(polyList);
+			}
+		}
+		clearDataBaseEvent.started.Invoke(true);
 	}
 }
