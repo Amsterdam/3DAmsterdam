9acf9887f91ec32b17e735c1623fc6d907e09afc
Tom Simons <tom.v.simons@gmail.com>
2021-12-17 12:58:42 +0000

    started adding feedback form functionality

---
+++ 3DAmsterdam/Assets/SendFeedback.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SendFeedback : MonoBehaviour
+{
+    //[SerializeField]
+    //private InputField hardwareInfoInputField;
+    [SerializeField]
+    private InputField userFeedbackInputField;
+
+    private string userFeedbackKey;
+    private SaveableString userFeedback;
+
+    private string timestampKey;
+    private SaveableString timestamp;
+
+    private void Awake()
+    {
+        userFeedbackKey = GetType() + ".userFeedback";
+        userFeedback = new SaveableString(userFeedbackKey);
+
+        timestampKey = GetType() + ".userFeedback";
+        timestamp = new SaveableString(timestampKey);
+    }
+
+    public void SubmitFeedback()
+    {
+        userFeedback.SetValue(userFeedbackInputField.text);
+        timestamp.SetValue(DateTime.Now.ToString());
+        SessionSaver.ExportSavedData();
+    }
+}
