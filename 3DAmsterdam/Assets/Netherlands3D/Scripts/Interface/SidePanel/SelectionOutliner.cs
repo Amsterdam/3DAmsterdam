b521cb3b93f692c3e284696d689f6717817c98ea
Sammey <samuelpetrus@gmail.com>
2022-01-05 13:43:08 +0100

    select specific ID using same event as used by building selection

--- 3DAmsterdam/Assets/Netherlands3D/Scripts/Interface/SidePanel/SelectionOutliner.cs
+++ 3DAmsterdam/Assets/Netherlands3D/Scripts/Interface/SidePanel/SelectionOutliner.cs
@@ -2,6 +2,8 @@
 using UnityEngine.UI;
 using Netherlands3D.TileSystem;
 using Netherlands3D.ObjectInteraction;
+using Netherlands3D.Events;
+using System.Collections.Generic;
 
 namespace Netherlands3D.Interface.SidePanel
 {
@@ -14,6 +16,9 @@ namespace Netherlands3D.Interface.SidePanel
 		private Text titleText;
 
 		private GameObject linkedGameObject;
+
+		[SerializeField]
+		private StringListEvent onSelect;
 
 		public GameObject LinkedGameObject { get => linkedGameObject; set => linkedGameObject = value; }
 		public string Id { get => id; set => id = value; }
@@ -39,8 +44,8 @@ namespace Netherlands3D.Interface.SidePanel
 		}
 
 		public void Select()
-		{
-			//Todo. Send select event with ID
+		{	
+			onSelect.started.Invoke(new List<string> { Id });
 		}
 
 		public void Close()
