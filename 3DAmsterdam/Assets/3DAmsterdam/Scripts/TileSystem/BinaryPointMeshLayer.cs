bfa4c6ef7713d43a92bda9697300e470babed51f
Sammey <samuelpetrus@gmail.com>
2022-01-10 17:38:11 +0100

    just added Graphics.DrawInstanced test scripts to branch

---
+++ 3DAmsterdam/Assets/3DAmsterdam/Scripts/TileSystem/BinaryPointMeshLayer.cs
@@ -0,0 +1,162 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Netherlands3D.Core;
+using UnityEngine.Networking;
+using System;
+using System.Linq;
+using UnityEngine.Rendering;
+
+namespace Netherlands3D.TileSystem
+{
+    public class BinaryPointMeshLayer : Layer
+    {
+		public Mesh[] instanceableMeshes;
+		public Material sharedMaterial;
+
+		public ShadowCastingMode tileShadowCastingMode = ShadowCastingMode.On;
+
+		public string brotliCompressedExtention = ".br";
+
+		private GameObject container;
+		private Mesh mesh;
+
+		public override void HandleTile(TileChange tileChange, System.Action<TileChange> callback = null)
+		{
+			TileAction action = tileChange.action;
+			var tileKey = new Vector2Int(tileChange.X, tileChange.Y);
+			switch (action)
+			{
+				case TileAction.Create:
+					Tile newTile = CreateNewTile(tileKey);
+					tiles.Add(tileKey, newTile);
+					break;
+				case TileAction.Upgrade:
+					tiles[tileKey].LOD++;
+					break;
+				case TileAction.Downgrade:
+					tiles[tileKey].LOD--;
+					break;
+				case TileAction.Remove:
+					InteruptRunningProcesses(tileKey);
+					RemoveGameObjectFromTile(tileKey);
+					tiles.Remove(tileKey);
+					callback(tileChange);
+					return;
+				default:
+					break;
+			}
+			tiles[tileKey].runningCoroutine = StartCoroutine(DownloadBinaryMesh(tileChange, callback));
+		}
+
+		private Tile CreateNewTile(Vector2Int tileKey)
+		{
+			Tile tile = new Tile();
+			tile.LOD = 0;
+			tile.tileKey = tileKey;
+			tile.layer = transform.gameObject.GetComponent<Layer>();
+			tile.gameObject = new GameObject();
+			tile.gameObject.transform.parent = transform.gameObject.transform;
+			tile.gameObject.layer = tile.gameObject.transform.parent.gameObject.layer;
+			tile.gameObject.transform.position = CoordConvert.RDtoUnity(tileKey);
+
+			return tile;
+		}
+		private void RemoveGameObjectFromTile(Vector2Int tileKey)
+		{
+			if (tiles.ContainsKey(tileKey))
+			{
+				Tile tile = tiles[tileKey];
+				if (tile == null)
+				{
+					return;
+				}
+				if (tile.gameObject == null)
+				{
+					return;
+				}
+				MeshFilter mf = tile.gameObject.GetComponent<MeshFilter>();
+				if (mf != null)
+				{
+					DestroyImmediate(tile.gameObject.GetComponent<MeshFilter>().sharedMesh, true);
+				}
+				Destroy(tiles[tileKey].gameObject);		
+			}
+		}
+		private IEnumerator DownloadBinaryMesh(TileChange tileChange, System.Action<TileChange> callback = null)
+		{
+			var tileKey = new Vector2Int(tileChange.X, tileChange.Y);
+			int lod = tiles[tileKey].LOD;
+			string url = Datasets[lod].path;
+            if (Datasets[lod].path.StartsWith("https://") || Datasets[lod].path.StartsWith("file://"))
+            {
+				url = Datasets[lod].path;
+			}
+
+			url = url.ReplaceXY(tileChange.X, tileChange.Y);
+
+			//On WebGL we request brotli encoded files instead. We might want to base this on browser support.
+
+#if !UNITY_EDITOR && UNITY_WEBGL
+			if(brotliCompressedExtention.Length>0)
+				url += brotliCompressedExtention;
+#endif
+			var webRequest = UnityWebRequest.Get(url);
+
+			tiles[tileKey].runningWebRequest = webRequest;
+			yield return webRequest.SendWebRequest();
+
+			if (!tiles.ContainsKey(tileKey)) yield break;
+
+			tiles[tileKey].runningWebRequest = null;
+
+			if (webRequest.result != UnityWebRequest.Result.Success)
+			{
+				RemoveGameObjectFromTile(tileKey);
+				callback(tileChange);
+			}
+			else
+			{
+				byte[] results = webRequest.downloadHandler.data;
+
+				yield return new WaitUntil(() => pauseLoading == false);
+
+				GameObject newGameobject = CreateNewGameObject(url,results, tileChange);	
+				RemoveGameObjectFromTile(tileKey);
+				tiles[tileKey].gameObject = newGameobject;
+
+				callback(tileChange);
+			}
+		}
+
+		public void EnableShadows(bool enabled)
+		{
+			tileShadowCastingMode = (enabled) ? ShadowCastingMode.On : ShadowCastingMode.Off;
+
+			MeshRenderer[] existingTiles = GetComponentsInChildren<MeshRenderer>();
+			foreach(var renderer in existingTiles)
+			{
+				renderer.shadowCastingMode = tileShadowCastingMode;
+			}
+		}
+
+		private GameObject CreateNewGameObject(string source,byte[] binaryMeshData, TileChange tileChange)
+		{
+			container = new GameObject();
+			
+			container.name = tileChange.X.ToString() + "-" + tileChange.Y.ToString();
+			container.transform.parent = transform.gameObject.transform;
+			container.layer = container.transform.parent.gameObject.layer;
+			container.transform.position = CoordConvert.RDtoUnity(new Vector2(tileChange.X + (tileSize/2), tileChange.Y + (tileSize/2)));
+			
+			container.SetActive(isEnabled);
+
+			mesh = BinaryMeshConversion.ReadBinaryMesh(binaryMeshData, out int[] submeshIndices);
+			mesh.name = source;
+
+			container.AddComponent<DrawInstancedMeshes>().DrawInstancedMeshesOnPoints(mesh, sharedMaterial, instanceableMeshes);
+
+			return container;
+		}
+	}
+}
---
+++ 3DAmsterdam/Assets/3DAmsterdam/Scripts/TileSystem/DrawInstancedMeshes.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class DrawInstancedMeshes : MonoBehaviour
+{
+	private MaterialPropertyBlock block;
+	private Matrix4x4[] matrices;
+
+	[SerializeField]
+	private List<Matrix4x4[]> splitMatrices;
+	[SerializeField]
+	private List<Vector4[]> splitUvs;
+
+	int pointCount;
+
+	[SerializeField]
+	private int textureRows = 6;
+
+	private Mesh mesh;
+	private Material material;
+
+	private Mesh[] instanceableMeshes;
+
+	private CommandBuffer buffer;
+
+	private void Awake()
+	{
+		block = new MaterialPropertyBlock();
+	}
+
+	public void DrawInstancedMeshesOnPoints(Mesh pointsMesh,Material material, Mesh[] instanceableMeshes)
+	{
+		this.mesh = pointsMesh;
+		this.material = material;
+		this.instanceableMeshes = instanceableMeshes;
+
+		pointCount = pointsMesh.vertexCount;
+		Vector3[] vertices = pointsMesh.vertices;
+
+		//Filter to fake points based dataset
+		List<Vector3> filteredVertices = new List<Vector3>();
+		for (int i = 0; i < pointCount; i+=24)
+		{
+			filteredVertices.Add(vertices[i]);
+			filteredVertices.Add(vertices[i]);
+		}
+		vertices = filteredVertices.ToArray();
+		var mainScale = Vector3.one * 2;
+		var allMatrices = new Matrix4x4[vertices.Length];
+		var allUvOffsets = new Vector4[vertices.Length];
+
+		//For testing purposes skip about the length of a single obj test geometry
+		for (int i = 0; i < vertices.Length; i++)
+		{
+			var position = vertices[i] + this.transform.position;
+			var rotation = Quaternion.Euler(0, Random.Range(-180, 180), 0);
+			var scale = mainScale;
+
+			var matrix = Matrix4x4.TRS(position, rotation, scale);
+			allMatrices[i] = matrix;
+
+			//Fill out UV coordinates (based on type input)
+			var randomXSheet = (1.0f / textureRows) * Random.Range(0, textureRows);
+			var randomYSheet = (1.0f / textureRows) * Random.Range(0, textureRows);
+			allUvOffsets[i] = new Vector4(randomXSheet, randomYSheet,0,0);
+		}
+
+		//Limit of 1023 per DrawMeshInstanced call. So split array into limit. 
+		splitMatrices = allMatrices.Select((x, i) => new { Index = i, Value = x })
+			.GroupBy(x => x.Index / 1023)
+			.Select(x => x.Select(v => v.Value).ToArray())
+			.ToList();
+
+		splitUvs = allUvOffsets.Select((x, i) => new { Index = i, Value = x })
+			.GroupBy(x => x.Index / 1023)
+			.Select(x => x.Select(v => v.Value).ToArray())
+			.ToList();
+	}
+
+	private void Update()
+	{
+		for (int i = 0; i < splitMatrices.Count; i++)
+		{
+			var matrices = splitMatrices[i];
+			var uvs = splitUvs[i];
+
+			block.SetVectorArray("_Offsets", uvs);
+			Graphics.DrawMeshInstanced(instanceableMeshes[0], 0, material, matrices, matrices.Length, block);
+		}
+	}
+}
