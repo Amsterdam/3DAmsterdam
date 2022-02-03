75b97705f2985361c8ee16a374d4568b8c1b39a5
Sammey <samuelpetrus@gmail.com>
2022-02-03 13:01:43 +0100

    loading screen events added

--- 3DAmsterdam/Assets/Netherlands3D/Scripts/Interface/LoadingScreen.cs
+++ 3DAmsterdam/Assets/Netherlands3D/Scripts/Interface/LoadingScreen.cs
@@ -1,4 +1,6 @@
-﻿using Netherlands3D.Interface;
+﻿using Netherlands3D.Events;
+using Netherlands3D.Interface;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,14 +15,46 @@ namespace Netherlands3D.Interface
 
 		[SerializeField]
 		private ProgressBar progressBar;
-
 		public ProgressBar ProgressBar { get => progressBar; }
 
-		public static LoadingScreen Instance;
+		public static LoadingScreen Instance; //TODO: remove singleton, and make all users use events instead.
 
+		[Header("Listeners")]
+		[SerializeField]
+		private FloatEvent onProgressBarPercentage;
+		[SerializeField]
+		private StringEvent onProgressBarMessage;
+		[SerializeField]
+		private StringEvent onProgressBarDetailedMessage;
 		private void Awake()
 		{
 			Instance = this;
+
+			onProgressBarMessage.started.AddListener(SetProgressBarMessage);
+			onProgressBarPercentage.started.AddListener(SetProgressBarPercentage);
+			onProgressBarDetailedMessage.started.AddListener(SetProgressBarDetailedMessage);
+		}
+
+		private void SetProgressBarDetailedMessage(string message)
+		{
+			progressBar.SetMessage(message);
+		}
+
+		private void SetProgressBarMessage(string message)
+		{
+			textMessage.text = message;
+		}
+
+		public void SetProgressBarPercentage(float percentage)
+		{
+			progressBar.Percentage(percentage / 100.0f);
+			if (percentage < 100)
+			{
+				Show();
+			}
+			else{
+				Hide();
+			}
 		}
 
 		public void ShowMessage(string text)
@@ -32,6 +66,10 @@ namespace Netherlands3D.Interface
 		public void Hide()
 		{
 			gameObject.SetActive(false);
+		}
+		public void Show()
+		{
+			gameObject.SetActive(true);
 		}
 	}
 }
