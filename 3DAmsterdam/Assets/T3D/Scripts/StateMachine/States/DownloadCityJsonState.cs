413767890cd91f61c3da5636f09d96a52df581f3
Tom Simons <tom.v.simons@gmail.com>
2022-02-07 17:28:49 +0100

    Replaced download step UI with new submit request UI

--- 3DAmsterdam/Assets/T3D/Scripts/StateMachine/States/DownloadCityJsonState.cs
+++ 3DAmsterdam/Assets/T3D/Scripts/StateMachine/States/DownloadCityJsonState.cs
@@ -1,7 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DownloadCityJsonState : State
 {
+    private string userNameKey;
+    private SaveableString userName;
+    [SerializeField] private InputField nameInputField;
+
+    private string userMailKey;
+    private SaveableString userMail;
+    [SerializeField] private InputField mailInputField;
+
+    private string userCommentsKey;
+    private SaveableString userComments;
+    [SerializeField] private InputField commentsInputField;
+
+    private string userPermissionKey;
+    private SaveableBool userPermission;
+    [SerializeField] private Toggle permissionToggle;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        userNameKey = GetType().ToString() + ".userName";
+        userMailKey = GetType().ToString() + ".userMail";
+        userCommentsKey = GetType().ToString() + ".userComments";
+        userPermissionKey = GetType().ToString() + ".userPermission";
+
+        userName = new SaveableString(userNameKey);
+        userMail = new SaveableString(userMailKey);
+        userComments = new SaveableString(userCommentsKey);
+        userPermission = new SaveableBool(userPermissionKey);
+    }
+
+    public override void StateCompletedAction()
+    {
+        userName.SetValue(nameInputField.text);
+        userMail.SetValue(mailInputField.text);
+        userComments.SetValue(commentsInputField.text);
+        userPermission.SetValue(permissionToggle.isOn);
+
+        SessionSaver.ExportSavedData();
+    }
 }
