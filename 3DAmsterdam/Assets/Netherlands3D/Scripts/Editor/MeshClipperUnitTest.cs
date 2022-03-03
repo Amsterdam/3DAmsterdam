c730b60df2fabfacb3d0014784e1a2b6e8665a96
Sammey <samuelpetrus@gmail.com>
2022-03-03 14:06:48 +0100

    unit test commented out

--- 3DAmsterdam/Assets/Netherlands3D/Scripts/Editor/MeshClipperUnitTest.cs
+++ 3DAmsterdam/Assets/Netherlands3D/Scripts/Editor/MeshClipperUnitTest.cs
@@ -11,12 +11,13 @@ public class MeshClipperUnitTest
     [Test]
     public void TestPointsInTriangle()
     {
-        Vector2 p1 = new Vector2(0,0);
-        Vector2 p2 = new Vector2(0,1);
-        Vector2 p3 = new Vector2(1,0);
+        //meshclipper existing methods seem to use x and z for vertex input too
+        Vector3 p1 = new Vector3(0, 0, 0);
+        Vector3 p2 = new Vector3(0, 0, 1);
+        Vector3 p3 = new Vector3(1, 0, 0);
 
-        Vector2 outsidePoint = new Vector2(-0.5f,0.4f);
-        Vector2 insidePoint = new Vector2(0.2f,0.4f);
+        Vector3 outsidePoint = new Vector3(-0.5f,0 , 0.4f);
+        Vector3 insidePoint = new Vector3(0.2f,0, 0.4f);
 
         var pointOutside = MeshClipper.PointIsInTriangle(outsidePoint, p1, p2, p3);
         var pointInside = MeshClipper.PointIsInTriangle(insidePoint, p1, p2, p3);
