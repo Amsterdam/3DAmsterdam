a1f9d774b5ad7e45339cbad6f57fa1383bdb890d
Sammey <samuelpetrus@gmail.com>
2022-03-03 13:45:33 +0100

    added points in triangle method and added test. still problem with existing points-are-in-bounds method

---
+++ 3DAmsterdam/Assets/Netherlands3D/Scripts/Editor/MeshClipperUnitTest.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+using System.IO;
+using UnityEngine.TestTools;
+using Netherlands3D.WebGL;
+
+public class MeshClipperUnitTest 
+{
+    [Test]
+    public void TestPointsInTriangle()
+    {
+        Vector2 p1 = new Vector2(0,0);
+        Vector2 p2 = new Vector2(0,1);
+        Vector2 p3 = new Vector2(1,0);
+
+        Vector2 outsidePoint = new Vector2(-0.5f,0.4f);
+        Vector2 insidePoint = new Vector2(0.2f,0.4f);
+
+        var pointOutside = MeshClipper.PointIsInTriangle(outsidePoint, p1, p2, p3);
+        var pointInside = MeshClipper.PointIsInTriangle(insidePoint, p1, p2, p3);
+
+        Assert.AreEqual(false, pointOutside);
+        Assert.AreEqual(true, pointInside);
+    }
+
+    [Test]
+    public void TestTriangleOverlapBounds()
+    {
+        Vector2 p1 = new Vector2(0, 0);
+        Vector2 p2 = new Vector2(0, 1);
+        Vector2 p3 = new Vector2(1, 0);
+
+        var boundingBox = new MeshClipper.RDBoundingBox(0.2f, 0.2f, 10f, 10f);
+
+        MeshClipper.trianglePosition trianglePosition = MeshClipper.GetTrianglePosition(p1, p2, p3, boundingBox);
+        Assert.AreEqual(MeshClipper.trianglePosition.overlap, trianglePosition);
+    }
+}
--- 3DAmsterdam/Assets/Netherlands3D/Scripts/Utilities/MeshClipper.cs
+++ 3DAmsterdam/Assets/Netherlands3D/Scripts/Utilities/MeshClipper.cs
@@ -105,8 +105,16 @@ public class MeshClipper
         {
             return trianglePosition.overlap;
         }
+    }
 
+    public static bool PointIsInTriangle(Vector2 p, Vector2 p0, Vector2 p1, Vector2 p2)
+    {
+        var a = .5f * (-p1.y * p2.x + p0.y * (-p1.x + p2.x) + p0.x * (p1.y - p2.y) + p1.x * p2.y);
+        var sign = a < 0 ? -1 : 1;
+        var s = (p0.y * p2.x - p0.x * p2.y + (p2.y - p0.y) * p.x + (p0.x - p2.x) * p.y) * sign;
+        var t = (p0.x * p1.y - p0.y * p1.x + (p0.y - p1.y) * p.x + (p1.x - p0.x) * p.y) * sign;
 
+        return s > 0 && t > 0 && (s + t) < 2 * a * sign;
     }
 
     private bool PointISInsideArea(Vector3 vector, RDBoundingBox boundingBox)
