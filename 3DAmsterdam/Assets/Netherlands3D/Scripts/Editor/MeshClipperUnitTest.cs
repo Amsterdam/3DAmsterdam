6175fd5825a2a6cd40d33a30a9f1c42f3fb2400a
Sammey <samuelpetrus@gmail.com>
2022-03-04 10:32:17 +0100

    extra test for polygon clipping

--- 3DAmsterdam/Assets/Netherlands3D/Scripts/Editor/MeshClipperUnitTest.cs
+++ 3DAmsterdam/Assets/Netherlands3D/Scripts/Editor/MeshClipperUnitTest.cs
@@ -25,17 +25,64 @@ public class MeshClipperUnitTest
         Assert.AreEqual(false, pointOutside);
         Assert.AreEqual(true, pointInside);
     }
-    /*
+  
     [Test]
     public void TestTriangleOverlapBounds()
     {
-        Vector2 p1 = new Vector2(0, 0);
-        Vector2 p2 = new Vector2(0, 1);
-        Vector2 p3 = new Vector2(1, 0);
+        Vector3 p1Overlap = new Vector3(-0.5f, 0, 0);
+        Vector3 p2Overlap = new Vector3(-0.5f, 0, 1);
+        Vector3 p3Overlap = new Vector3(1.5f, 0, 0);
 
-        var boundingBox = new MeshClipper.RDBoundingBox(0.2f, 0.2f, 10f, 10f);
+        Vector3 p1Outside = new Vector3(-10, 0, 0);
+        Vector3 p2Outside = new Vector3(-10, 0, 1);
+        Vector3 p3Outside = new Vector3(-3, 0, 0);
 
-        MeshClipper.trianglePosition trianglePosition = MeshClipper.GetTrianglePosition(p1, p2, p3, boundingBox);
-        Assert.AreEqual(MeshClipper.trianglePosition.overlap, trianglePosition);
-    }*/
+        var boundingBox = new MeshClipper.RDBoundingBox(0, 0, 10f, 10f);
+
+        MeshClipper.trianglePosition trianglePositionOverlap = MeshClipper.GetTrianglePosition(p1Overlap, p2Overlap, p3Overlap, boundingBox);
+        MeshClipper.trianglePosition trianglePositionOutside = MeshClipper.GetTrianglePosition(p1Outside, p2Outside, p3Outside, boundingBox);
+
+        Assert.AreEqual(MeshClipper.trianglePosition.overlap, trianglePositionOverlap);
+        Assert.AreEqual(MeshClipper.trianglePosition.outside, trianglePositionOutside);
+    }
+
+    [Test]
+    public void TestPolygonClippping()
+    {
+        Vector3 p1Overlap = new Vector3(-0.5f, 0, -0.1f);
+        Vector3 p2Overlap = new Vector3(-0.5f, 0, 1);
+        Vector3 p3Overlap = new Vector3(1.5f, 0, -0.5f);
+
+        Vector3 p1Outside = new Vector3(-10, 0, 0);
+        Vector3 p2Outside = new Vector3(-10, 0, 1);
+        Vector3 p3Outside = new Vector3(-3, 0, 0);
+
+        List<Vector3> triangleA = new List<Vector3>();
+        triangleA.Add(p1Overlap);
+        triangleA.Add(p2Overlap);
+        triangleA.Add(p3Overlap);
+
+        List<Vector3> triangleB = new List<Vector3>();
+        triangleA.Add(p1Outside);
+        triangleA.Add(p2Outside);
+        triangleA.Add(p3Outside);
+
+        var boundingBox = new MeshClipper.RDBoundingBox(0, 0, 10f, 10f);
+
+        List<Vector3> clippingPolygon = MeshClipper.CreateClippingPolygon(boundingBox);
+
+        List<Vector3> clippedTriangleA = Netherlands3D.Utilities.TriangleClipping.SutherlandHodgman.ClipPolygon(triangleA, clippingPolygon);
+        List<Vector3> clippedTriangleB = Netherlands3D.Utilities.TriangleClipping.SutherlandHodgman.ClipPolygon(triangleB, clippingPolygon);
+
+        Assert.AreEqual(3, clippedTriangleA.Count);
+		for (int i = 0; i < clippedTriangleA.Count; i++)
+		{
+            Debug.Log($"X {clippedTriangleA[i].x}");
+            Debug.Log($"Y {clippedTriangleA[i].y}");
+            Debug.Log($"Z {clippedTriangleA[i].z}");
+            Debug.Log($"-------------------------");
+		}
+
+        //Assert.AreEqual(0, clippedTriangleB.Count);
+    }
 }
