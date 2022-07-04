ecdcdb872ed09d7490395594ba096b90d72422ba
Bart Burkhardt <bart.burkhardt@gmail.com>
2022-07-04 10:20:41 +0200

    hardcoded laden van json aangepast

--- 3DAmsterdam/Assets/T3D/Scripts/LoadData/MetadataLoader.cs
+++ 3DAmsterdam/Assets/T3D/Scripts/LoadData/MetadataLoader.cs
@@ -242,29 +242,29 @@ namespace Netherlands3D.T3D.Uitbouw
         {
             //var cityjson = File.ReadAllText(@"E:\cityjson\NL.IMBAG.Pand.0518100000226302.json");
             //var cityjson = File.ReadAllText(@"F:\T3D\Data\CityJson\kubus_met_gaten.json");
-            var cityjson = File.ReadAllText(@"F:\T3D\Data\CityJson\VCS\0518100000226302.json");
+            //var cityjson = File.ReadAllText(@"F:\T3D\Data\CityJson\VCS\0518100000226302.json");
 
-            
-            CityJsonBagReceived?.Invoke(cityjson);
-            yield return null;
 
-            //var url = $"https://tomcat.totaal3d.nl/happyflow-wfs/wfs?SERVICE=WFS&VERSION=2.0.0&REQUEST=GetFeature&TYPENAMES=bldg:Building&RESOURCEID=NL.IMBAG.Pand.{id}&OUTPUTFORMAT=application%2Fjson";
-            //var uwr = UnityWebRequest.Get(url);
+            //CityJsonBagReceived?.Invoke(cityjson);
+            //yield return null;
 
-            //using (uwr)
-            //{
-            //    yield return uwr.SendWebRequest();
-            //    if (uwr.result == UnityWebRequest.Result.ConnectionError || uwr.result == UnityWebRequest.Result.ProtocolError)
-            //    {
-            //    }
-            //    else
-            //    {
-            //        var cityjson = uwr.downloadHandler.text;
-            //        Debug.Log(cityjson);
-            //        CityJsonBagReceived?.Invoke(cityjson);
-            //    }
+            var url = $"https://tomcat.totaal3d.nl/happyflow-wfs/wfs?SERVICE=WFS&VERSION=2.0.0&REQUEST=GetFeature&TYPENAMES=bldg:Building&RESOURCEID=NL.IMBAG.Pand.{id}&OUTPUTFORMAT=application%2Fjson";
+            var uwr = UnityWebRequest.Get(url);
 
-            //}
+            using (uwr)
+            {
+                yield return uwr.SendWebRequest();
+                if (uwr.result == UnityWebRequest.Result.ConnectionError || uwr.result == UnityWebRequest.Result.ProtocolError)
+                {
+                }
+                else
+                {
+                    var cityjson = uwr.downloadHandler.text;
+                    Debug.Log(cityjson);
+                    CityJsonBagReceived?.Invoke(cityjson);
+                }
+
+            }
         }
 
         void ProcessPerceelData(JSONNode jsonData)
