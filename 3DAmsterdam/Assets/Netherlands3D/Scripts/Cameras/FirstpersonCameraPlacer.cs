9f9d463501d48d7c1fb0c7b86a311cf16ffc37c9
Sammey <samuelpetrus@gmail.com>
2022-08-08 14:40:45 +0200

    maps style first person (streetview) placement

--- 3DAmsterdam/Assets/Netherlands3D/Scripts/Cameras/FirstpersonCameraPlacer.cs
+++ 3DAmsterdam/Assets/Netherlands3D/Scripts/Cameras/FirstpersonCameraPlacer.cs
@@ -8,6 +8,7 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 namespace Netherlands3D.Interface
@@ -15,16 +16,37 @@ namespace Netherlands3D.Interface
     [RequireComponent(typeof(WorldPointFollower))]
     public class FirstpersonCameraPlacer : Interactable
     {
+        [SerializeField] private InputActionAsset actionAsset;
+        private InputActionMap releaseActionMap;
+        private InputAction releaseAction;
+
         [HideInInspector] public Quaternion cameraRotation = Quaternion.identity;
         [SerializeField] private TriggerEvent startPlacement;
+        [SerializeField] private bool pushCameraOnScreenBounds = true;
+        [SerializeField] private float panSpeed = 1.0f;
         private WorldPointFollower worldPointFollower;
-
+        private Vector3 startPosition;
         private bool placing = false;
 
         private void Awake()
         {
+            startPosition = this.transform.localPosition;
+
+            releaseActionMap = actionAsset.FindActionMap("UI");
+            releaseAction = releaseActionMap.FindAction("Click");
+
             worldPointFollower = GetComponent<WorldPointFollower>();
             startPlacement.started.AddListener(StartPlacement);
+        }
+
+        private void Placed()
+        {
+            placing = false;
+            cameraRotation = CameraModeChanger.Instance.ActiveCamera.transform.rotation;
+            CameraModeChanger.Instance.FirstPersonMode(worldPointFollower.WorldPosition, cameraRotation);
+            StopInteraction();
+
+            gameObject.SetActive(false);
         }
 
         public override void Escape()
@@ -35,8 +57,6 @@ namespace Netherlands3D.Interface
 
         private void StartPlacement()
         {
-            Selector.Instance.registeredClickInput.AddListener(Placed);
-
             gameObject.SetActive(true);
             HelpMessage.Show("<b>Klik</b> op het maaiveld om een camerastandpunt te plaatsen\n\nGebruik de <b>Escape</b> toets om te annuleren");
             placing = true;
@@ -49,7 +69,6 @@ namespace Netherlands3D.Interface
 
             if (placing)
             {
-                Selector.Instance.registeredClickInput.RemoveListener(Placed);
                 placing = false;
                 StopAllCoroutines();
             }
@@ -58,22 +77,37 @@ namespace Netherlands3D.Interface
         private IEnumerator FollowPointer()
         {
             yield return new WaitForEndOfFrame();
-
+            var rectTransform = this.GetComponent<RectTransform>();
             while (placing)
             {
                 yield return new WaitForEndOfFrame();
+                if (pushCameraOnScreenBounds && Vector3.Distance(rectTransform.localPosition, startPosition) > 0.10f)
+                {
+                    var margin = 0.47f; //based on center based origin, ranging from -50 to 50
+                    var normalisedPointerPosition = new Vector2(rectTransform.localPosition.x / Screen.width, rectTransform.localPosition.y / Screen.height);
+                    var cameraForward = Camera.main.transform.forward;
+                    cameraForward.y = 0;
+
+                    if (Mathf.Abs(normalisedPointerPosition.x) > margin)
+                    {
+                        Camera.main.transform.Translate(Vector3.right * normalisedPointerPosition.x * panSpeed * Mathf.Abs(Camera.main.transform.position.y), Space.Self);
+                    }
+                    else if (Mathf.Abs(normalisedPointerPosition.y) > margin)
+                    {
+                        Camera.main.transform.Translate(cameraForward.normalized * normalisedPointerPosition.y * panSpeed * Mathf.Abs(Camera.main.transform.position.y), Space.World);
+                    }
+                }
+
                 var targetLocation = (Selector.hits.Length > 0) ? Selector.hits[0].point : CameraModeChanger.Instance.CurrentCameraControls.GetPointerPositionInWorld();
                 worldPointFollower.AlignWithWorldPosition(targetLocation + Vector3.up * 1.8f);
+
+                if (Mouse.current.leftButton.wasReleasedThisFrame)
+                {
+                    placing = false;
+                    Placed();
+                }
             }
-        }
 
-        private void Placed()
-        {
-            cameraRotation = CameraModeChanger.Instance.ActiveCamera.transform.rotation;
-            CameraModeChanger.Instance.FirstPersonMode(worldPointFollower.WorldPosition, cameraRotation);
-            StopInteraction();
-
-            gameObject.SetActive(false);
         }
     }
 }
