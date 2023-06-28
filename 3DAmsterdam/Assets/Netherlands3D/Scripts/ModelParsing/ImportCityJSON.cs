4308a7b294608bea1dbdb0e84209bd6eca5ffcd7
Sam Baas <samuelpetrus@gmail.com>
2023-06-28 14:49:20 +0200

    streaming via json.net

--- 3DAmsterdam/Assets/Netherlands3D/Scripts/ModelParsing/ImportCityJSON.cs
+++ 3DAmsterdam/Assets/Netherlands3D/Scripts/ModelParsing/ImportCityJSON.cs
@@ -1,3 +1,4 @@
+using Netherlands3D.Coordinates;
 using Newtonsoft.Json;
 using System;
 using System.Collections;
@@ -6,187 +7,215 @@ using System.IO;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class ImportCityJSON : MonoBehaviour
+namespace Netherlands3D.CityJSON.Stream
 {
-    [SerializeField] private int maxParseSteps = 30000;
-    [SerializeField] private UnityEvent<GameObject> spawnImportedGameObject;
-
-    //Transform values
-    Vector3 scale = new Vector3();
-    Vector3 translate = new Vector3();
-
-    public void LoadCityJSONFromFile(string filepath)
+    public class ImportCityJSON : MonoBehaviour
     {
-        if(IsCityJSON(filepath))
-            StartCoroutine(Parse(filepath));
-    }
+        [SerializeField] private Material material;
+        [SerializeField] private int maxParseSteps = 30000;
+        [SerializeField] private UnityEvent<GameObject> spawnImportedGameObject;
 
-    /// <summary>
-    /// Streamread first part of json to check if it matches a CityJSON.
-    /// Streamreading saves memory and time for parsing possible big CityJSON files.
-    /// </summary>
-    /// <param name="filepath">Path to json file</param>
-    /// <returns>True if it is a CityJSON</returns>
-    private bool IsCityJSON(string filepath)
-    {
-        Debug.Log("Checking if json is CityJSON");
+        //Transform values
+        Vector3 scale = new Vector3();
+        Vector3 translate = new Vector3();
 
-        using (var fileStream = new FileStream(filepath, FileMode.Open))
-        using (var streamReader = new StreamReader(fileStream))
-        using (var jsonReader = new JsonTextReader(streamReader))
+        public void LoadCityJSONFromFile(string filepath)
         {
-            var jsonSerializer = new JsonSerializer();
-
-            // Load the first few tokens to check if it is a CityJSON file
-            var startTokens = new List<JsonToken>();
-            var propertyNameTokens = new List<string>();
-
-            // Read the first 5 tokens
-            for (int i = 0; i < 5; i++)
-            {
-                if (!jsonReader.Read())
-                {
-                    break;
-                }
-
-                startTokens.Add(jsonReader.TokenType);
-
-                if (jsonReader.TokenType == JsonToken.PropertyName)
-                {
-                    propertyNameTokens.Add(jsonReader.Value.ToString());
-                }
-            }
-
-            // Check if it matches the CityJSON structure
-            bool isCityJson = startTokens.Count >= 3 &&
-                              startTokens[0] == JsonToken.StartObject &&
-                              startTokens[1] == JsonToken.PropertyName &&
-                              propertyNameTokens.Contains("type") &&
-                              propertyNameTokens.Contains("version") &&
-                              propertyNameTokens.Contains("CityObjects");
-
-            return isCityJson;
+            if (IsCityJSON(filepath))
+                StartCoroutine(StreamParseCityJSON(filepath));
         }
-    }
 
-    private IEnumerator Parse(string filepath)
-    {
-        Debug.Log("Parsing CityJSON");
-
-        using (var fileStream = new FileStream(filepath, FileMode.Open))
-        using (var streamReader = new StreamReader(fileStream))
-        using (var jsonReader = new JsonTextReader(streamReader))
+        /// <summary>
+        /// Streamread first part of json to check if it matches a CityJSON.
+        /// Streamreading saves memory and time for parsing possible big CityJSON files.
+        /// </summary>
+        /// <param name="filepath">Path to json file</param>
+        /// <returns>True if it is a CityJSON</returns>
+        private bool IsCityJSON(string filepath)
         {
-            List<int> triangleIndices = new List<int>();
-            List<Vector3> vertices = new List<Vector3>();
-            
+            Debug.Log("Checking if json is CityJSON");
 
-            // Advance the reader until we find properties we need and parse it
-            while (jsonReader.Read())
+            using (var fileStream = new FileStream(filepath, FileMode.Open))
+            using (var streamReader = new StreamReader(fileStream))
+            using (var jsonReader = new JsonTextReader(streamReader))
             {
-                if (jsonReader.TokenType == JsonToken.PropertyName)
+                var jsonSerializer = new JsonSerializer();
+
+                // Load the first few tokens to check if it is a CityJSON file
+                var startTokens = new List<JsonToken>();
+                var propertyNameTokens = new List<string>();
+
+                // Read the first 5 tokens
+                for (int i = 0; i < 5; i++)
                 {
-                    yield return null;
-
-                    var propertyName = jsonReader.Value.ToString();
-
-                    if (propertyName == "boundaries")
+                    if (!jsonReader.Read())
                     {
-                        yield return ReadBoundaries(jsonReader, triangleIndices);
+                        break;
                     }
-                    else if (propertyName == "vertices")
+
+                    startTokens.Add(jsonReader.TokenType);
+
+                    if (jsonReader.TokenType == JsonToken.PropertyName)
                     {
-                        yield return ReadVertices(jsonReader, vertices);
-                    }
-                    else if (propertyName == "transform")
-                    {
-                        yield return ReadTransformValues(jsonReader);
+                        propertyNameTokens.Add(jsonReader.Value.ToString());
                     }
                 }
+
+                // Check if it matches the CityJSON structure
+                bool isCityJson = startTokens.Count >= 3 &&
+                                  startTokens[0] == JsonToken.StartObject &&
+                                  startTokens[1] == JsonToken.PropertyName &&
+                                  propertyNameTokens.Contains("type") &&
+                                  propertyNameTokens.Contains("version") &&
+                                  propertyNameTokens.Contains("CityObjects");
+
+                return isCityJson;
             }
         }
-    }
 
-    private IEnumerator ReadTransformValues(JsonTextReader jsonReader)
-    {
-        scale = new Vector3();
-        translate = new Vector3();
-
-        //Read transform properties untill we move out of transform object
-        while (jsonReader.Read() && jsonReader.TokenType == JsonToken.EndObject)
+        private IEnumerator StreamParseCityJSON(string filepath)
         {
-            if (jsonReader.TokenType == JsonToken.PropertyName && jsonReader.Value.ToString() == "scale")
-            {
-                int index = 0;
-                while (jsonReader.Read() && jsonReader.TokenType == JsonToken.Float)
-                {
-                    var number = Convert.ToDouble(jsonReader.Value);
-                    translate[index] = (float)number;
-                    index++;
-                }
-            }
-            else if (jsonReader.TokenType == JsonToken.PropertyName && jsonReader.Value.ToString() == "translate")
-            {
-                int index = 0;
-                while (jsonReader.Read() && jsonReader.TokenType == JsonToken.Float)
-                {
-                    var number = Convert.ToDouble(jsonReader.Value);
-                    translate[index] = (float)number;
-                    index++;
-                }
-            }
-        }
-        yield return null;
-    }
+            Debug.Log("Parsing CityJSON");
 
-    private IEnumerator ReadVertices(JsonTextReader jsonReader, List<Vector3> vertices)
-    {
-        // Ensure the next token is the start of an array
-        if (jsonReader.Read() && jsonReader.TokenType == JsonToken.StartArray)
-        {
-            while (jsonReader.Read() && jsonReader.TokenType == JsonToken.StartArray)
+            using (var fileStream = new FileStream(filepath, FileMode.Open))
+            using (var streamReader = new StreamReader(fileStream))
+            using (var jsonReader = new JsonTextReader(streamReader))
             {
-                //Vertices
-                int vertexPositionIndex = 0;
-                while (jsonReader.Read() && jsonReader.TokenType == JsonToken.Float)
-                {
-                    var number = Convert.ToInt32(jsonReader.Value);
-                    Vector3 vert = new Vector3();
-                    vertices.Add(vert);
-                }
-            }
-            yield return null;
-        }
-    }
+                List<int> triangleIndices = new List<int>();
+                List<Vector3> vertices = new List<Vector3>();
 
-    private IEnumerator ReadBoundaries(JsonTextReader jsonReader, List<int> triangleIndices)
-    {
-        // Ensure the next token is the start of an array
-        if (jsonReader.Read() && jsonReader.TokenType == JsonToken.StartArray)
-        {
-            var jsonSerializer = new JsonSerializer();
-
-            // Read the outermost array elements to get to the nested triangle arrays
-            // "boundaries":[[[2,1,0]],[[3,2,0]],[[6,5,4]]]
-            if (jsonReader.Read() && jsonReader.TokenType == JsonToken.StartArray)
-            {
-                if (jsonReader.Read() && jsonReader.TokenType == JsonToken.StartArray)
+                // Advance the reader until we find properties we need and parse it
+                while (jsonReader.Read())
                 {
-                    //Triangle indices
-                    while (jsonReader.Read() && jsonReader.TokenType != JsonToken.EndArray)
+                    if (jsonReader.TokenType == JsonToken.PropertyName)
                     {
-                        if (jsonReader.TokenType == JsonToken.Integer)
+                        yield return null;
+
+                        var propertyName = jsonReader.Value.ToString();
+
+                        if (propertyName == "boundaries")
                         {
-                            var number = Convert.ToInt32(jsonReader.Value);
-                            triangleIndices.Add(number);
+                            yield return ReadBoundaries(jsonReader, triangleIndices);
+                        }
+                        else if (propertyName == "vertices")
+                        {
+                            yield return ReadVertices(jsonReader, vertices);
+                        }
+                        else if (propertyName == "transform")
+                        {
+                            yield return ReadTransformValues(jsonReader);
                         }
                     }
                 }
 
+                //End of file
+                SpawnNewGameObject(triangleIndices, vertices);
+            }
+        }
+
+        private void SpawnNewGameObject(List<int> triangleIndices, List<Vector3> vertices)
+        {
+            var mesh = new Mesh();
+            mesh.vertices = vertices.ToArray();
+            mesh.triangles = triangleIndices.ToArray();
+
+            var gameObject = new GameObject();
+            gameObject.AddComponent<MeshFilter>().mesh = mesh;
+            gameObject.AddComponent<MeshRenderer>().material = new Material(material);
+
+            gameObject.transform.position = CoordinateConverter.RDtoUnity(translate.x,translate.y,translate.z);
+
+
+            spawnImportedGameObject.Invoke(gameObject);
+        }
+
+        private IEnumerator ReadTransformValues(JsonTextReader jsonReader)
+        {
+            scale = new Vector3();
+            translate = new Vector3();
+
+            //Read transform properties untill we move out of transform object
+            while (jsonReader.Read() && jsonReader.TokenType == JsonToken.EndObject)
+            {
+                if (jsonReader.TokenType == JsonToken.PropertyName && jsonReader.Value.ToString() == "scale")
+                {
+                    int index = 0;
+                    while (jsonReader.Read() && jsonReader.TokenType == JsonToken.Float)
+                    {
+                        var number = Convert.ToDouble(jsonReader.Value);
+                        translate[index] = (float)number;
+                        index++;
+                    }
+                }
+                else if (jsonReader.TokenType == JsonToken.PropertyName && jsonReader.Value.ToString() == "translate")
+                {
+                    int index = 0;
+                    while (jsonReader.Read() && jsonReader.TokenType == JsonToken.Float)
+                    {
+                        var number = Convert.ToDouble(jsonReader.Value);
+                        translate[index] = (float)number;
+                        index++;
+                    }
+                }
+            }
+            yield return null;
+        }
+
+        private IEnumerator ReadVertices(JsonTextReader jsonReader, List<Vector3> vertices)
+        {
+            // Ensure the next token is the start of an array
+            if (jsonReader.Read() && jsonReader.TokenType == JsonToken.StartArray)
+            {
+                //Read a vertex array
+                while (jsonReader.Read() && jsonReader.TokenType == JsonToken.StartArray)
+                {
+                    //Vertices
+                    Vector3 vert = new Vector3();
+                    int vertexPositionIndex = 0;
+                    while (jsonReader.Read() && jsonReader.TokenType == JsonToken.Float)
+                    {
+                        var coordinate = Convert.ToSingle(jsonReader.Value);
+                        vert[vertexPositionIndex] = coordinate;
+                        vertexPositionIndex++;
+                    }
+                    vertices.Add(vert);
+
+                    if ((vertices.Count % maxParseSteps) == 0) yield return null;
+
+                }
                 yield return null;
             }
+        }
 
-            yield return null;
+        private IEnumerator ReadBoundaries(JsonTextReader jsonReader, List<int> triangleIndices)
+        {
+            // Ensure the next token is the start of an array
+            if (jsonReader.Read() && jsonReader.TokenType == JsonToken.StartArray)
+            {
+                var jsonSerializer = new JsonSerializer();
+
+                // Read the outermost array elements to get to the nested triangle arrays
+                // "boundaries":[[[2,1,0]],[[3,2,0]],[[6,5,4]]]
+                if (jsonReader.Read() && jsonReader.TokenType == JsonToken.StartArray)
+                {
+                    if (jsonReader.Read() && jsonReader.TokenType == JsonToken.StartArray)
+                    {
+                        //Triangle indices
+                        while (jsonReader.Read() && jsonReader.TokenType != JsonToken.EndArray)
+                        {
+                            if (jsonReader.TokenType == JsonToken.Integer)
+                            {
+                                var number = Convert.ToInt32(jsonReader.Value);
+                                triangleIndices.Add(number);
+                            }
+                        }
+                    }
+
+                    yield return null;
+                }
+
+                yield return null;
+            }
         }
     }
 }
